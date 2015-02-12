5ac2acc4e534af2c83676ab090cf7f40816456d2
Tom Crowley <tom@tom-crowley.co.uk>
2015-02-12 21:29:02 +0000

    add not implemented assert to functions that have no code yet in the profile render.

--- Pulp/Core/XProfileRender.cpp
+++ Pulp/Core/XProfileRender.cpp
@@ -363,12 +363,12 @@ void XProfileSys::DrawLabel(float x, float y, const char* pStr, Color& col, Flag
 
 void XProfileSys::DrawRect(float x1, float y1, float x2, float y2, Color& col)
 {
-
+	X_ASSERT_NOT_IMPLEMENTED();
 }
 
 void XProfileSys::DrawRect(Vec4f& rec, Color& col)
 {
-
+	X_ASSERT_NOT_IMPLEMENTED()
 }
 
 X_NAMESPACE_END
