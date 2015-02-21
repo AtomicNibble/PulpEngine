73a52f8c632bc2c0b15d5fec7c4ee1003f1ca090
Tom Crowley <tom@tom-crowley.co.uk>
2015-02-21 14:59:41 +0000

    Move the TextureFile contructors into a cpp file, since the renderArena is not defined when the file is included in UT code.

---
+++ Pulp/RenderSystems/Common/Textures/XTextureFile.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "XTextureFile.h"
+
+X_NAMESPACE_BEGIN(texture)
+
+
+XTextureFile::XTextureFile() 
+{
+	core::zero_this(this);
+
+	format = Texturefmt::UNKNOWN;
+	type = TextureType::UNKNOWN;
+}
+XTextureFile::~XTextureFile() 
+{
+	for (int i = 0; i < 6; i++) {
+		X_ASSERT(!(i > numFaces && pFaces[i]), "memory set on face out of range")(numFaces);
+		if (!dontDelete())
+			X_DELETE_ARRAY(pFaces[i], g_rendererArena);
+	}
+}
+
+
+const bool XTextureFile::isValid(void) const
+{
+	// check every things been set.
+	return size.x > 0 && size.y > 0 &&
+		depth > 0 &&
+		numMips > 0 &&
+		datasize > 0 &&
+		numFaces > 0 &&
+		format != Texturefmt::UNKNOWN &&
+		type != TextureType::UNKNOWN;
+}
+
+X_NAMESPACE_END
--- Pulp/RenderSystems/Common/Textures/XTextureFile.h
+++ Pulp/RenderSystems/Common/Textures/XTextureFile.h
@@ -44,19 +44,8 @@ uint8_t				numFaces;  // Cube maps aka 6 faces.
 
 struct XTextureFile
 {
-	XTextureFile() {
-		core::zero_this(this);
-
-		format = Texturefmt::UNKNOWN;
-		type = TextureType::UNKNOWN;
-	}
-	~XTextureFile() {
-		for (int i = 0; i < 6; i++) {
-			X_ASSERT(!(i > numFaces && pFaces[i]), "memory set on face out of range")(numFaces);
-			if (!dontDelete())
-				X_DELETE_ARRAY( pFaces[i], g_rendererArena);
-		}
-	}
+	XTextureFile();
+	~XTextureFile();
 
 	friend class DDS::XTexLoaderDDS;
 	friend class JPG::XTexLoaderJPG;
@@ -72,27 +61,18 @@ public:
 	X_INLINE const char* getName(void) const { return pName; }
 #endif // !X_DEBUG
 
-	X_INLINE const Vec2<uint16_t> getSize() const { return size; }
-	X_INLINE const int getWidth() const { return size.x; }
-	X_INLINE const int getHeight() const { return size.y; }
-	X_INLINE const int getNumFaces() const { return numFaces; }
-	X_INLINE const int getDepth() const { return depth; }
-	X_INLINE const int getNumMips() const { return numMips; }
-	X_INLINE const int getDataSize() const { return datasize; }
-	X_INLINE TextureFlags getFlags() const { return flags; }
-	X_INLINE Texturefmt::Enum getFormat() const { return format; }
-	X_INLINE TextureType::Enum getType() const { return type; }
+	X_INLINE const Vec2<uint16_t> getSize(void) const { return size; }
+	X_INLINE const int getWidth(void) const { return size.x; }
+	X_INLINE const int getHeight(void) const { return size.y; }
+	X_INLINE const int getNumFaces(void) const { return numFaces; }
+	X_INLINE const int getDepth(void) const { return depth; }
+	X_INLINE const int getNumMips(void) const { return numMips; }
+	X_INLINE const int getDataSize(void) const { return datasize; }
+	X_INLINE TextureFlags getFlags(void) const { return flags; }
+	X_INLINE Texturefmt::Enum getFormat(void) const { return format; }
+	X_INLINE TextureType::Enum getType(void) const { return type; }
 
-	X_INLINE const bool isValid() const {
-		// check every things been set.
-		return size.x > 0 && size.y > 0 &&
-			depth > 0 &&
-			numMips > 0 &&
-			datasize > 0 &&
-			numFaces > 0 &&
-			format != Texturefmt::UNKNOWN &&
-			type != TextureType::UNKNOWN;
-	}
+	const bool isValid(void) const;
 
 	struct MipInfo
 	{
