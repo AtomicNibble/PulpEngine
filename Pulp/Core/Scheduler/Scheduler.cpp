1b9b295c32376b38eb58046d74292b9f70c6e52c
Tom Crowley <tom@tom-crowley.co.uk>
2015-03-18 07:24:15 +0000

    Added some notes about how i'm going to implement the Scheduler. Created a bare bone implementation also.

--- Pulp/Core/Scheduler/Scheduler.cpp
+++ Pulp/Core/Scheduler/Scheduler.cpp
@@ -2,9 +2,55 @@
 #include "Scheduler.h"
 
 
+#include "Cpu.h"
+#include "Core.h"
+
 X_NAMESPACE_BEGIN(core)
 
 
+Scheduler::Scheduler() :
+numThreads_(0)
+{
+
+}
+
+
+Scheduler::~Scheduler()
+{
+
+}
+
+
+
+void Scheduler::StartThreads(void)
+{
+	X_ASSERT_NOT_NULL(gEnv);
+	X_ASSERT_NOT_NULL(gEnv->pCore);
+
+	// get the num HW threads
+	XCore* pCore = (XCore*)gEnv->pCore;
+	CpuInfo* pCpu = pCore->GetCPUInfo();
+
+	uint32_t numCores = pCpu->GetLogicalProcessorCount();
+	numCores = core::Max(core::Min(HW_THREAD_MAX, numCores - HW_THREAD_NUM_DELTA),1u);
+
+	X_LOG0("Scheduler", "Creating %i threads", numThreads_);
+
+
+
+
+}
+
+void Scheduler::ShutDown(void)
+{
+
+}
+
+void Scheduler::addJob(SJob& job)
+{
+
+
+}
 
 
 
--- Pulp/Core/Scheduler/Scheduler.h
+++ Pulp/Core/Scheduler/Scheduler.h
@@ -3,11 +3,71 @@
 #ifndef X_SCHEDULER_H_
 #define X_SCHEDULER_H_
 
+#include "Threading\Thread.h"
 
 X_NAMESPACE_BEGIN(core)
 
+/*
+
+Ok this is going to be the threading job system.
+it will take jobs that are a function and also take data.
+
+maybe i will make it class based also.
+
+the later is more the c++ way.
 
 
+It should be possible to add loads of jobs then sync them.
+So that any cost of adding the jobs can be done after all the required jobs have been added.
+
+A job should be able to add other jobs.
+And also wait for them to complte before it continues.
+
+
+this is why fibers is probs a good solution since i can have a job running in a thread.
+that adds jobs to the schedular then waits for them to finish before carrying on.
+becuases of fibres the job that is waiting is not blocking any thead.
+
+that thread is free todo other things in the meantime, preventing threads from becoming idle.
+
+Which means i will need a pool of fibers that can be run within x number of threads.
+i also need todo like load balancing so threads don't get starved.
+
+how can i take my jobs and place them in fibres.
+guess a fibere will have to get the job then run it in the context of the fibre.
+
+
+*/
+
+class SJob
+{
+public:
+
+
+private:
+
+};
+
+
+class Scheduler
+{
+	static const uint32_t HW_THREAD_MAX = 6; // max even if hardware supports more.
+	static const uint32_t HW_THREAD_NUM_DELTA = 1; // num = Min(max,hw_num-delta);
+
+public:
+	Scheduler();
+	~Scheduler();
+
+	void StartThreads(void);
+	void ShutDown(void);
+
+	void addJob(SJob& job);
+
+private:
+	uint32_t numThreads_; // num created.
+
+	Thread threads_[HW_THREAD_MAX];
+};
 
 
 X_NAMESPACE_END
