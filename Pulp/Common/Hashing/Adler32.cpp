76a3efa10f762f6179886068d6a476345467002e
Tom Crowley <tom@tom-crowley.co.uk>
2015-03-21 20:52:38 +0000

    Updated Adler32 code.

--- Pulp/Common/Hashing/Adler32.cpp
+++ Pulp/Common/Hashing/Adler32.cpp
@@ -5,41 +5,58 @@ X_NAMESPACE_BEGIN(core)
 
 namespace Hash
 {
+#define BASE 65521      
+#define NMAX 5552
 
-#define BASE 65521UL    // largest prime smaller than 65536 
-#define NMAX 5552		// NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 
-
-#define DO1(buf,i)  {s1 += buf[i]; s2 += s1;}
+#define DO1(buf,i)  {adler += (buf)[i]; sum2 += adler;}
 #define DO2(buf,i)  DO1(buf,i); DO1(buf,i+1);
 #define DO4(buf,i)  DO2(buf,i); DO2(buf,i+2);
 #define DO8(buf,i)  DO4(buf,i); DO4(buf,i+4);
 #define DO16(buf)   DO8(buf,0); DO8(buf,8);
 
+/* use NO_DIVIDE if your processor does not do division in hardware --
+try it both ways to see which is faster */
 #ifdef NO_DIVIDE
+	/* note that this assumes BASE is 65521, where 65536 % 65521 == 15
+	(thank you to John Reiser for pointing this out) */
+#  define CHOP(a) \
+	do {
+	\
+	unsigned long tmp = a >> 16; \
+	a &= 0xffffUL; \
+	a += (tmp << 4) - tmp; \
+	} while (0)
+#  define MOD28(a) \
+	do {
+	\
+	CHOP(a); \
+	if (a >= BASE) a -= BASE; \
+	} while (0)
 #  define MOD(a) \
-		do {
-		\
-		if (a >= (BASE << 16)) a -= (BASE << 16); \
-		if (a >= (BASE << 15)) a -= (BASE << 15); \
-		if (a >= (BASE << 14)) a -= (BASE << 14); \
-		if (a >= (BASE << 13)) a -= (BASE << 13); \
-		if (a >= (BASE << 12)) a -= (BASE << 12); \
-		if (a >= (BASE << 11)) a -= (BASE << 11); \
-		if (a >= (BASE << 10)) a -= (BASE << 10); \
-		if (a >= (BASE << 9)) a -= (BASE << 9); \
-		if (a >= (BASE << 8)) a -= (BASE << 8); \
-		if (a >= (BASE << 7)) a -= (BASE << 7); \
-		if (a >= (BASE << 6)) a -= (BASE << 6); \
-		if (a >= (BASE << 5)) a -= (BASE << 5); \
-		if (a >= (BASE << 4)) a -= (BASE << 4); \
-		if (a >= (BASE << 3)) a -= (BASE << 3); \
-		if (a >= (BASE << 2)) a -= (BASE << 2); \
-		if (a >= (BASE << 1)) a -= (BASE << 1); \
-		if (a >= BASE) a -= BASE; \
-		} while (0)
+	do {
+	\
+	CHOP(a); \
+	MOD28(a); \
+	} while (0)
+#  define MOD63(a) \
+	do { /* this assumes a is not negative */ \
+	z_off64_t tmp = a >> 32; \
+	a &= 0xffffffffL; \
+	a += (tmp << 8) - (tmp << 5) + tmp; \
+	tmp = a >> 16; \
+	a &= 0xffffL; \
+	a += (tmp << 4) - tmp; \
+	tmp = a >> 16; \
+	a &= 0xffffL; \
+	a += (tmp << 4) - tmp; \
+	if (a >= BASE) a -= BASE; \
+	} while (0)
 #else
 #  define MOD(a) a %= BASE
+#  define MOD28(a) a %= BASE
+#  define MOD63(a) a %= BASE
 #endif
+
 
 
 	Adler32Val Adler32(const char* str)
@@ -55,39 +72,69 @@ namespace Hash
 
 	Adler32Val Adler32(const void* buf, size_t length)
 	{
-		Adler32Val val = 0;
+		Adler32Val val = 1u;
 		return Adler32(val, buf, length);
 	}
 
-	Adler32Val Adler32(Adler32Val& adler, const void* bufVoid, size_t length)
+	Adler32Val Adler32(Adler32Val& adler, const void* bufV, size_t len)
 	{
-		uint32_t s1 = adler & 0xffff;
-		uint32_t s2 = (adler >> 16) & 0xffff;
-		const uint8_t* buf = reinterpret_cast<const uint8_t*>(bufVoid);
-		int k;
+		const uint8_t* buf = reinterpret_cast<const uint8_t*>(bufV);
+		Adler32Val sum2;
+		Adler32Val n;
 
-		if (length == 0)
-			return 1L;
+		sum2 = (adler >> 16) & 0xffff;
+		adler &= 0xffff;
 
-		while (length > 0)
-		{
-			k = length < NMAX ? (int)length : NMAX;
-			length -= k;
-			while (k >= 16) {
-				DO16(buf);
-				buf += 16;
-				k -= 16;
-			}
-			if (k != 0) do {
-				s1 += *buf++;
-				s2 += s1;
-			} while (--k);
-			MOD(s1);
-			MOD(s2);
+		if (len == 1) {
+			adler += buf[0];
+			if (adler >= BASE)
+				adler -= BASE;
+			sum2 += adler;
+			if (sum2 >= BASE)
+				sum2 -= BASE;
+			return adler | (sum2 << 16);
 		}
 
-		adler = (s2 << 16) | s1;
-		return adler;
+		if (buf == nullptr)
+			return 1L;
+
+		if (len < 16) {
+			while (len--) {
+				adler += *buf++;
+				sum2 += adler;
+			}
+			if (adler >= BASE)
+				adler -= BASE;
+			MOD28(sum2);            
+			return adler | (sum2 << 16);
+		}
+
+		while (len >= NMAX) {
+			len -= NMAX;
+			n = NMAX / 16;         
+			do {
+				DO16(buf);          
+				buf += 16;
+			} while (--n);
+			MOD(adler);
+			MOD(sum2);
+		}
+
+		if (len) {                  
+			while (len >= 16) {
+				len -= 16;
+				DO16(buf);
+				buf += 16;
+			}
+			while (len--) {
+				adler += *buf++;
+				sum2 += adler;
+			}
+			MOD(adler);
+			MOD(sum2);
+		}
+
+		return adler | (sum2 << 16);
 	}
 
 
