a26e170fb363f13b06a8a37d3572ec7f2238034e
Tom Crowley <tom@tom-crowley.co.uk>
2015-03-24 23:05:04 +0000

    Added some initial code to Scheduler UT just to check everything is working.

--- Pulp/UnitTests/UT_Scheduler.cpp
+++ Pulp/UnitTests/UT_Scheduler.cpp
@@ -7,10 +7,33 @@
 X_USING_NAMESPACE;
 
 
+void TestJob(void* pParam, uint32_t batchOffset, uint32_t batchNum, uint32_t workerIdx)
+{
+	uint32_t idx = reinterpret_cast<uint32_t>(pParam);
+
+	X_LOG0("TestJob", "job idx: %i Worker: %i", idx, workerIdx);
+	Sleep(150);
+}
+
+
 TEST(Threading, Scheduler)
 {
 	core::Scheduler jobSys;
+	core::JobList jobs;
+
+	jobSys.StartThreads();
+
+	{
+		for (size_t i = 0; i < 100; i++) {
+			jobs.AddJob(TestJob, (void*)i);
+		}
+
+		jobSys.SubmitJobList(&jobs);
 
 
+		jobs.Wait();
+	}
 
+	::Sleep(60000);
+	jobSys.ShutDown();
 }
