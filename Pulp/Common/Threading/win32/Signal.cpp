d8cd0dceb93f228a7387463447077ead16d180ab
Tom Crowley <tom@tom-crowley.co.uk>
2015-03-25 21:47:32 +0000

    Add a signal class into threading util. UT not yet added.

---
+++ Pulp/Common/Threading/win32/Signal.cpp
@@ -0,0 +1,53 @@
+#include <EngineCommon.h>
+#include "Signal.h"
+
+
+
+X_NAMESPACE_BEGIN(core)
+
+Signal::Signal() : hHandle_(0)
+{
+	hHandle_ = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (hHandle_ == NULL) {
+		core::lastError::Description Dsc;
+		X_ERROR("Signal", "failed to create event. Error: %s", core::lastError::ToString(Dsc));
+	}
+}
+
+Signal::~Signal()
+{
+	CloseHandle(hHandle_);
+}
+
+
+void Signal::raise(void)
+{
+	SetEvent(hHandle_);
+}
+
+void Signal::clear(void)
+{
+	ResetEvent(hHandle_);
+}
+
+bool Signal::wait(uint32_t timeout)
+{
+	DWORD result = WaitForSingleObject(hHandle_, timeout == Signal::WAIT_INFINITE ? INFINITE : timeout);
+
+	if (result != WAIT_OBJECT_0)
+	{
+		core::lastError::Description Dsc;
+		if (timeout == Signal::WAIT_INFINITE) {
+			X_ERROR("Signal", "WaitForObject infinite error: %s", core::lastError::ToString(Dsc));
+		}
+		else if (result != WAIT_TIMEOUT) {
+			X_ERROR("Signal", "WaitForObject none-infinite error: %s", core::lastError::ToString(Dsc));
+		}
+	}
+
+	return result == WAIT_OBJECT_0;
+}
+
+
+
+X_NAMESPACE_END
---
+++ Pulp/Common/Threading/win32/Signal.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#ifndef X_THREADING_SIGNAL_H_
+#define X_THREADING_SIGNAL_H_
+
+X_NAMESPACE_BEGIN(core)
+
+class Signal
+{
+public:
+	static const uint32_t WAIT_INFINITE = -1;
+
+public:
+	Signal();
+	~Signal();
+
+	void raise(void);
+	void clear(void);
+	bool wait(uint32_t timeout = WAIT_INFINITE);
+
+
+private:
+	X_NO_ASSIGN(Signal);
+	X_NO_COPY(Signal);
+
+	HANDLE hHandle_;
+};
+
+X_NAMESPACE_END
+
+#endif // !X_THREADING_SIGNAL_H_
