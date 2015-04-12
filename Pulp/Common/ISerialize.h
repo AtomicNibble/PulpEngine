4306545f4a0a2ead485ccb322eed34a7d47a2b7f
Tom Crowley <tom@tom-crowley.co.uk>
2015-04-12 14:20:07 +0100

    Add a ISerialize interface.

---
+++ Pulp/Common/ISerialize.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef X_I_SERIALIZE_H_
+#define X_I_SERIALIZE_H_
+
+X_NAMESPACE_BEGIN(core)
+
+struct XFile;
+
+struct ISerialize
+{
+	virtual bool SSave(XFile* pFile) X_ABSTRACT;
+	virtual bool SLoad(XFile* pFile) X_ABSTRACT;
+
+protected:
+	virtual ~ISerialize() {}
+};
+
+X_NAMESPACE_END
+
+#endif // !X_I_SERIALIZE_H_
