72dd96779bb6ad492380bbf07b45164adb6b3e08
Tom Crowley <tom@tom-crowley.co.uk>
2015-04-17 23:17:59 +0100

    More StackString UT's

--- Pulp/UnitTests/UT_StackString.cpp
+++ Pulp/UnitTests/UT_StackString.cpp
@@ -35,5 +35,53 @@ TEST(StackString, Construct)
 	EXPECT_STREQ("123456789123456454", str32_u64.c_str());
 	EXPECT_STREQ("hello bob", str32_startEnd.c_str());
 	EXPECT_STREQ("hello bob", str32_range.c_str());
+}
+
+
+TEST(StackString, Set)
+{
+	core::StackString<1024> str("goat man");
+
+	str.set("Lorem Ipsum is simply dummy text of the printing and"
+		" typesetting industry.Lorem Ipsum has been the industry's"
+		" standard dummy text ever since the 1500");
+
+	EXPECT_STREQ("Lorem Ipsum is simply dummy text of the printing and"
+		" typesetting industry.Lorem Ipsum has been the industry's"
+		" standard dummy text ever since the 1500", str.c_str());
+
+	const char testTxt[38] = { "slap a goat in a moat while in a boat" };
+
+	str.set(testTxt, testTxt + 38);
+
+	EXPECT_STREQ(testTxt, str.c_str());
+}
+
+
+TEST(StackString, Append)
+{
+	core::StackString<1024> str("camel man ");
+
+	str.append('a', 5);
+	EXPECT_STREQ("camel man aaaaa", str.c_str());
+
+	str.append(" goat");
+	EXPECT_STREQ("camel man aaaaa goat", str.c_str());
+
+	str.append(" cat", 3);
+	EXPECT_STREQ("camel man aaaaa goat ca", str.c_str());
+
+	const char testTxt[10] = { "hookersss" };
+	str.append(testTxt, testTxt + 10);
+	EXPECT_STREQ("camel man aaaaa goat cahookersss", str.c_str());
+}
+
+TEST(StackString, AppendFmt)
+{
+	core::StackString<1024> str(" ");
+
+	str.appendFmt("%i%s_%u", 1337, "shit", 0x4096);
+
+	EXPECT_STREQ(" 1337shit_16534", str.c_str());
 
 }
