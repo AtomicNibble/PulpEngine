0bc5f5cc8178f2c58eb8195cb8c1c5a545dc7261
Tom Crowley <tom@tom-crowley.co.uk>
2015-05-12 08:51:30 +0100

    Remove BSPCompiler.cpp file. Add BSPFace.cpp

---
+++ tools/LevelBuilder/BSPFace.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "LevelBuilder.h"
+
+
+void LvlBuilder::BuildBSPFace(LvlEntity& ent)
+{
+	bspTree& root = ent.bspTree;
+	root.bounds.clear();
+	root.headnode = X_NEW(bspNode, g_arena, "BspNode");
+	
+	// add the windows to the bounds.
+	LvlEntity::BspFaceArr::ConstIterator it = ent.bspFaces.begin();
+	LvlEntity::BspFaceArr::ConstIterator end = ent.bspFaces.end();
+	for (; it != end; ++it)
+	{
+		const bspFace& face = *it;
+		const XWinding& winding = *face.w;
+
+		for (int32_t i = 0; i < winding.GetNumPoints(); i++)
+		{
+			root.bounds.add(winding[i].asVec3());
+		}
+	}
+
+
+}
