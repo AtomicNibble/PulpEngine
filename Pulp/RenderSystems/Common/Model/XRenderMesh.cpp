c41ec86d00a65bf5cad53045048d837e5e6e929f
Tom Crowley <tom@tom-crowley.co.uk>
2015-06-18 20:26:04 +0100

    Update the temp rendermesh code to also apply normal map.
    These will be made redundant once I've wrote the proper system for adding stuff to draw list.

--- Pulp/RenderSystems/Common/Model/XRenderMesh.cpp
+++ Pulp/RenderSystems/Common/Model/XRenderMesh.cpp
@@ -186,6 +186,12 @@ bool XRenderMesh::render(void)
 				texture::TexID id = pTextRes->pITex->getTexID();
 				texture::XTexture::applyFromId(0, id, 0);
 			}
+			pTextRes = shaderItem.pResources_->getTexture(shader::ShaderTextureIdx::BUMP);
+			if (pTextRes)
+			{
+				texture::TexID id = pTextRes->pITex->getTexID();
+				texture::XTexture::applyFromId(1, id, 0);
+			}
 		}
 
 		g_Dx11D3D.FX_DrawIndexPrimitive(
