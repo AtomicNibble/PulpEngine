0e1e3c7bd634fdf0e84d1591ab7480fdc00aec6a
Tom Crowley <tom@tom-crowley.co.uk>
2015-08-04 22:56:40 +0100

    Add base files to project / filters for SHA512.

---
+++ Pulp/Common/Hashing/SHA512.cpp
@@ -0,0 +1,15 @@
+#include "EngineCommon.h"
+#include "SHA512.h"
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Hash
+{
+
+
+
+
+} // namespace Hash
+
+X_NAMESPACE_END
+
---
+++ Pulp/Common/Hashing/SHA512.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#ifndef X_HASH_SHA512_H_
+#define X_HASH_SHA512_H_
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Hash
+{
+
+
+
+
+} // namespace Hash
+
+X_NAMESPACE_END
+
+#endif // X_HASH_SHA512_H_
