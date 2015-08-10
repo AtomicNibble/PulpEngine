a175331aa8445283d45f07d939dfb64f940de4e6
Tom Crowley <tom@tom-crowley.co.uk>
2015-08-10 18:12:13 +0100

    Respect the ends of the strings in IsEqualCaseInsen.
    Previously it return a match even if you specify a end value before the end of the string.

--- Pulp/Common/String/StringUtil.cpp
+++ Pulp/Common/String/StringUtil.cpp
@@ -345,7 +345,15 @@ namespace strUtil
 		size_t Len = endExclusiveS1 - startInclusiveS1;
 
 		if (Len == (endExclusiveS2 - startInclusiveS2))
-			return _stricmp(startInclusiveS1, startInclusiveS2) == 0;
+		{
+			// need to respect length values.
+			while (Len && (::tolower(*startInclusiveS1) == ::tolower(*startInclusiveS2))) {
+				Len--;
+				startInclusiveS1++;
+				startInclusiveS2++;
+			}
+			return Len == 0;
+		}
 
 		return false;
 	}
