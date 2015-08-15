68fe13cdc397e18f32395031a904871f838e8396
Tom Crowley <tom@tom-crowley.co.uk>
2015-08-15 17:57:01 +0100

    Move Semaphore to platform folder it's win32 specific code

--- Pulp/Common/Threading/Semaphore.h
+++ Pulp/Common/Threading/Semaphore.h
@@ -1,27 +1,3 @@
 #pragma once
 
-#ifndef _X_THREADING_SEMAPHORE_H_
-#define _X_THREADING_SEMAPHORE_H_
-
-X_NAMESPACE_BEGIN(core)
-
-class Semaphore
-{
-public:
-	X_INLINE Semaphore(int initialValue, int maximumValue);
-	X_INLINE ~Semaphore(void);
-
-	X_INLINE void Signal(void);
-	X_INLINE void Wait(void);
-	X_INLINE bool TryWait(void);
-
-private:
-	HANDLE sema_;
-};
-
-#include "Semaphore.inl"
-
-X_NAMESPACE_END
-
-
-#endif // !_X_THREADING_SEMAPHORE_H_
+#include X_INCLUDE(Threading/X_PLATFORM/Semaphore.h)
