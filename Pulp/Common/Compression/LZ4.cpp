72be690b1faba8b6dbfb177462b3bbe96b1dfd78
Tom Crowley <tom@tom-crowley.co.uk>
2015-09-13 14:08:15 +0100

    Add Lz4 header / source file (empty currently)

---
+++ Pulp/Common/Compression/LZ4.cpp
@@ -0,0 +1,15 @@
+#include <EngineCommon.h>
+#include "LZ4.h"
+
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Compression
+{
+
+
+
+
+} // namespace Compression
+
+X_NAMESPACE_END
---
+++ Pulp/Common/Compression/LZ4.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#ifndef X_COMPRESSION_LZ4_H_
+#define X_COMPRESSION_LZ4_H_
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Compression
+{
+
+
+
+
+
+} // namespace Compression
+
+X_NAMESPACE_END
+
+#endif // !X_COMPRESSION_LZ4_H_
