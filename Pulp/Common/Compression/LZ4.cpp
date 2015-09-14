73feaf2185c4a2de8a19754a6db2f768e9e20d70
Tom Crowley <tom@tom-crowley.co.uk>
2015-09-14 22:38:12 +0100

    Add LZ4 Wrapper.

--- Pulp/Common/Compression/LZ4.cpp
+++ Pulp/Common/Compression/LZ4.cpp
@@ -1,14 +1,83 @@
 #include <EngineCommon.h>
 #include "LZ4.h"
 
+#include <../../3rdparty/source/lz4-r131/lz4.h>
 
 X_NAMESPACE_BEGIN(core)
 
 namespace Compression
 {
+	namespace
+	{
+		int compressLevelToAcceleration(LZ4::CompressLevel::Enum lvl)
+		{
+			if (lvl == LZ4::CompressLevel::LOW) {
+				return 8;
+			}
+			if (lvl == LZ4::CompressLevel::NORMAL) {
+				return 3;
+			}
+			if (lvl == LZ4::CompressLevel::HIGH) {
+				return 1;
+			}
 
+			X_ASSERT_UNREACHABLE();
+			return 1;
+		}
+	}
 
+	size_t LZ4::maxSourceSize(void)
+	{
+		return static_cast<size_t>(LZ4_MAX_INPUT_SIZE);
+	}
 
+	size_t LZ4::requiredDeflateDestBuf(size_t sourceLen)
+	{
+		return static_cast<size_t>(LZ4_compressBound(safe_static_cast<int,size_t>(sourceLen)));
+	}
+
+	bool LZ4::deflate(const void* pSrcBuf, size_t srcBufLen,
+		void* pDstBuf, size_t destBufLen, size_t& destLenOut, CompressLevel::Enum lvl)
+	{
+		const char* pSrc = reinterpret_cast<const char*>(pSrcBuf);
+		char* pDst = reinterpret_cast<char*>(pDstBuf);
+
+		int srcSize = safe_static_cast<int, size_t>(srcBufLen);
+		int detSize = safe_static_cast<int, size_t>(destBufLen);
+
+		int res = LZ4_compress_fast(pSrc, pDst, srcSize, detSize, 
+			compressLevelToAcceleration(lvl));
+
+		if (res <= 0) {
+			X_ERROR("LZ4", "Failed to compress buffer: %i", res);
+			destLenOut = 0;
+			return false;
+		}
+
+		destLenOut = res;
+		return true;
+	}
+
+	bool LZ4::inflate(void* pSrcBuf, size_t srcBufLen,
+		void* pDstBuf, size_t destBufLen)
+	{
+		X_UNUSED(srcBufLen);
+
+		const char* pSrc = reinterpret_cast<const char*>(pSrcBuf);
+		char* pDst = reinterpret_cast<char*>(pDstBuf);
+
+		int size = safe_static_cast<int, size_t>(destBufLen);
+
+		int res = LZ4_decompress_fast(pSrc, pDst, size);
+
+		if (res <= 0) {
+			X_ERROR("LZ4", "Failed to decompress buffer: %i", res);
+			return false;
+		}
+		return true;
+	}
+
+	
 
 } // namespace Compression
 
--- Pulp/Common/Compression/LZ4.h
+++ Pulp/Common/Compression/LZ4.h
@@ -10,6 +10,38 @@ namespace Compression
 {
 
 
+	class LZ4
+	{
+	public:
+		X_DECLARE_ENUM(CompressLevel)(
+			LOW, // speed
+			NORMAL, // normal
+			HIGH // best
+		);
+
+
+	protected:
+	//	virtual ~LZ4();
+
+	public:
+		// max source buffer size.
+		static size_t maxSourceSize(void);
+		// buffer than source is garanted to fit into.
+		static size_t requiredDeflateDestBuf(size_t sourceLen);
+
+		// none buffed single step inflate / deflate.
+		static bool deflate(const void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen, size_t& destLenOut, CompressLevel::Enum lvl = CompressLevel::NORMAL);
+
+		static bool inflate(void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen);
+
+
+	private:
+		X_NO_CREATE(LZ4);
+		X_NO_COPY(LZ4);
+		X_NO_ASSIGN(LZ4);
+	};
 
 
 
