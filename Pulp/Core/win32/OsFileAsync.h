aef5c8733625e00a2e2bf68c6e0202c51a57f0d0
Tom Crowley <tom@tom-crowley.co.uk>
2015-09-27 17:02:49 +0100

    Remove unneeded qualification of class in member declaration.

--- Pulp/Core/win32/OsFileAsync.h
+++ Pulp/Core/win32/OsFileAsync.h
@@ -14,8 +14,8 @@ struct OsFileAsync
 	OsFileAsync(const wchar_t* path, IFileSys::fileModeFlags mode);
 	~OsFileAsync(void);
 
-	XOsFileAsyncOperation OsFileAsync::readAsync(void* pBuffer, uint32_t length, size_t position);
-	XOsFileAsyncOperation OsFileAsync::writeAsync(const void* pBuffer, uint32_t length, size_t position);
+	XOsFileAsyncOperation readAsync(void* pBuffer, uint32_t length, size_t position);
+	XOsFileAsyncOperation writeAsync(const void* pBuffer, uint32_t length, size_t position);
 
 	size_t tell(void) const;
 	size_t remainingBytes(void) const;
