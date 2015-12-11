59dc6f08cfe599536336c3447a1c3afd4ae00f14
Tom Crowley <tom@tom-crowley.co.uk>
2015-12-11 22:43:12 +0000

    Add FiberScheduler source files. (empty)

---
+++ Pulp/Common/Threading/win32/FiberScheduler.cpp
@@ -0,0 +1,16 @@
+#include <EngineCommon.h>
+#include "FiberScheduler.h"
+
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Fiber
+{
+
+
+
+
+} // namespadce Fiber
+
+
+X_NAMESPACE_END
---
+++ Pulp/Common/Threading/win32/FiberScheduler.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Fiber
+{
+	
+
+
+
+} // namespadce Fiber
+
+
+X_NAMESPACE_END
