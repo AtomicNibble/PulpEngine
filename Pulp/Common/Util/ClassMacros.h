ca2c73374a44ae4d497c90fe34c758d130baa0c5
Tom Crowley <tom@tom-crowley.co.uk>
2015-12-12 18:21:48 +0000

    Add a class marco to disable volatile assignment operator. Also update the code to use delete keyword.

--- Pulp/Common/Util/ClassMacros.h
+++ Pulp/Common/Util/ClassMacros.h
@@ -5,9 +5,10 @@
 
 // genral shit for classes
 
-#define X_NO_CREATE(className)			private: className(void); ~className(void)
-#define X_NO_COPY(className)			private: className(const className&)
-#define X_NO_ASSIGN(className)			private: className& operator=(const className&)
+#define X_NO_CREATE(className)				private: className(void); ~className(void)
+#define X_NO_COPY(className)				className(const className&) = delete
+#define X_NO_ASSIGN(className)				className& operator=(const className&) = delete
+#define X_NO_ASSIGN_VOLATILE(className)		className& operator=(const className&) volatile = delete
 
 
 #endif // !X_CLASSMACROS_H_
