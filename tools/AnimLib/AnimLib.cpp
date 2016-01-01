30acf8887225a8b86a831c4fbf9cd822d8df6f38
Tom Crowley <tom@tom-crowley.co.uk>
2016-01-01 01:47:49 +0000

    Expose create / delete interface methods. Define a arena for AnimLib.

--- tools/AnimLib/AnimLib.cpp
+++ tools/AnimLib/AnimLib.cpp
@@ -1,3 +1,35 @@
 #include "stdafx.h"
 
+#include <IAnimation.h>
+#include <IEngineModule.h>
 
+#include <ModuleExports.h>
+#include <Extension\XExtensionMacros.h>
+
+#include "XAnimLib.h"
+
+
+namespace 
+{
+
+	core::MallocFreeAllocator g_AnimLibAlloc;
+
+
+} // namespace
+
+AnimLibrena* g_AnimLibArena = nullptr;
+
+extern "C" DLL_EXPORT X_NAMESPACE_NAME::anim::IAnimLib* CreateInterface(ICore *pCore)
+{
+	g_AnimLibArena = X_NEW(AnimLibrena, gEnv->pArena, "AnimLibArena")(&g_AnimLibAlloc, "AnimLibArena");
+	
+	LinkModule(pCore, "AnimLib");
+
+	return X_NEW(anim::XAnimLib, g_AnimLibArena, "AnimLib")();
+}
+
+extern "C" DLL_EXPORT void FreeInterface(X_NAMESPACE_NAME::anim::IAnimLib* pAnimLib)
+{
+	X_DELETE(pAnimLib, g_AnimLibArena);
+	X_DELETE(g_AnimLibArena, gEnv->pArena);
+}
