c61f79b668e57a3a926ed791803f02456aaad6c9
Tom Crowley <tom@tom-crowley.co.uk>
2016-01-01 19:13:08 +0000

    Add Converter tool project under tools.

---
+++ tools/Converter/Converter.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Converter.h"
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+    return 0;
+}
+
