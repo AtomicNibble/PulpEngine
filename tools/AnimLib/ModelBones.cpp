b7d9dff1e383953505f7e808258d0220cc152e46
Tom Crowley <tom@tom-crowley.co.uk>
2016-01-01 14:21:14 +0000

    Add source file for a model bone loader.
    Will load just the bones out of a model file for compiling animations.

---
+++ tools/AnimLib/ModelBones.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "ModelBones.h"
+
+X_NAMESPACE_BEGIN(model)
+
+
+
+X_NAMESPACE_END
---
+++ tools/AnimLib/ModelBones.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+X_NAMESPACE_BEGIN(model)
+
+
+
+
+
+X_NAMESPACE_END
