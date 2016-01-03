5ff4f76e7e5379700cfd77da37eef144113fd8a0
Tom Crowley <tom@tom-crowley.co.uk>
2016-01-03 10:40:09 +0000

    Add code for loading inter anim bones into converter, then dropping any that are not in the model file.

--- tools/AnimLib/AnimCompiler.cpp
+++ tools/AnimLib/AnimCompiler.cpp
@@ -4,9 +4,11 @@
 X_NAMESPACE_BEGIN(anim)
 
 
-AnimCompiler::AnimCompiler(const InterAnim& inter, const model::ModelSkeleton& skelton) :
+AnimCompiler::AnimCompiler(core::MemoryArenaBase* arena, const InterAnim& inter, const model::ModelSkeleton& skelton) :
 	inter_(inter),
-	skelton_(skelton)
+	skelton_(skelton),
+
+	bones_(arena)
 {
 
 }
@@ -17,5 +19,71 @@ AnimCompiler::~AnimCompiler()
 
 }
 
+bool AnimCompiler::compile(void)
+{
+	// got any bones in the inter?
+	if (inter_.getNumBones() < 1) {
+		X_WARNING("Anim", "skipping compile of anim, source inter anim has no bones");
+		return false; 
+	}
+	// any anims in the model skeleton?
+	if (skelton_.getNumBones() < 1) {
+		X_WARNING("Anim", "skipping compile of anim, source model skeleton has no bones");
+		return false;
+	}
+
+
+	loadInterBoneNames();
+	dropMissingBones();
+
+	if (bones_.isEmpty()) {
+		X_WARNING("Anim", "skipping compile of anim, inter anim and model skelton have bones in common");
+		return true;
+	}
+
+
+
+	return false;
+}
+
+
+void AnimCompiler::loadInterBoneNames(void)
+{
+	// make a copy of all the bones names in anim file.
+	bones_.resize(inter_.getNumBones());
+
+	for (size_t i = 0; i < inter_.getNumBones(); i++)
+	{
+		const anim::Bone& interBone = inter_.getBone(i);
+		bones_[i].name = interBone.name;
+	}
+}
+
+void AnimCompiler::dropMissingBones(void)
+{
+	// drop any bones that are not in the model file.
+	size_t i;
+	for (i = 0; i < bones_.size(); i++)
+	{
+		const core::string& name = bones_[i].name;
+
+		// check if this bone in model file.
+		size_t x, numModelBones = skelton_.getNumBones();
+		for (x = 0; x < numModelBones; x++)
+		{
+			const char* pName = skelton_.getBoneName(x);
+			if (name == pName)
+			{
+				break;
+			}
+		}
+
+		if (x == numModelBones)
+		{
+			// remove it.
+			bones_.removeIndex(i);
+		}
+	}
+}
 
 X_NAMESPACE_END
--- tools/AnimLib/AnimCompiler.h
+++ tools/AnimLib/AnimCompiler.h
@@ -5,18 +5,32 @@
 
 X_NAMESPACE_BEGIN(anim)
 
-
 class AnimCompiler
 {
+	struct Bone
+	{
+		core::string name;
+	};
+
+	typedef core::Array<Bone> BoneArr;
 
 public:
-	AnimCompiler(const InterAnim& inter, const model::ModelSkeleton& skelton);
+	AnimCompiler(core::MemoryArenaBase* arena, const InterAnim& inter, const model::ModelSkeleton& skelton);
 	~AnimCompiler();
 
+	bool compile(void);
+
+private:
+
+	void loadInterBoneNames(void);
+	void dropMissingBones(void);
 
 private:
 	const InterAnim& inter_;
 	const model::ModelSkeleton& skelton_;
+private:
+
+	BoneArr bones_;
 };
 
 
