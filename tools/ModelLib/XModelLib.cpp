f2c7dcb95ec6b88c2baf11a94ac99994458d867a
Tom Crowley <tom@tom-crowley.co.uk>
2016-01-21 23:09:34 +0000

    Add base empty implementation of Model lib.

---
+++ tools/ModelLib/XModelLib.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "XModelLib.h"
+
+X_NAMESPACE_BEGIN(model)
+
+XModelLib::XModelLib()
+{
+
+}
+
+XModelLib::~XModelLib()
+{
+
+
+}
+
+
+
+X_NAMESPACE_END
---
+++ tools/ModelLib/XModelLib.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include <IModel.h>
+
+X_NAMESPACE_BEGIN(model)
+
+class XModelLib : public IModelLib
+{
+public:
+	XModelLib();
+	~XModelLib() X_OVERRIDE;
+
+
+
+private:
+};
+
+
+X_NAMESPACE_END
