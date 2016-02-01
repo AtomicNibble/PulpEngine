c3e8ce5db3cea79500b39e2c94a52d874a32e37c
Tom Crowley <tom@tom-crowley.co.uk>
2016-02-01 23:17:22 +0000

    Don't include sqlite3ext.h

--- tools/SqLite/SqlLib.h
+++ tools/SqLite/SqlLib.h
@@ -2,5 +2,5 @@
 
 
 #include <../../3rdparty/source/sqlite-3.10.1/sqlite3.h>
-#include <../../3rdparty/source/sqlite-3.10.1/sqlite3ext.h>
+// #include <../../3rdparty/source/sqlite-3.10.1/sqlite3ext.h>
 
