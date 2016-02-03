f0e2b73fe2366b581ec2edcbb54a8a1c6f62ab5e
Tom Crowley <tom@tom-crowley.co.uk>
2016-02-03 22:05:44 +0000

    Create a simple table for storing assets. Add method to drop tables.

--- tools/AssetDB/AssetDB.cpp
+++ tools/AssetDB/AssetDB.cpp
@@ -30,8 +30,28 @@ void AssetDB::CloseDB(void)
 
 bool AssetDB::CreateTables(void)
 {
+	// we want to store a list of the assets.
+	// so create a table which has every asset in it.
+
+	db_.execute("CREATE TABLE IF NOT EXISTS file_ids ("
+		" file_id INTEGER PRIMARY KEY,"
+		"name TEXT COLLATE NOCASE,"
+		"path TEXT,"
+		"type INTEGER,"
+		"write_time INTEGER,"
+		");");
+
+
 
 	return false;
+}
+
+bool AssetDB::DropTables(void)
+{
+
+	db_.execute("DROP TABLE IF EXISTS gdt_files;");
+
+	return true;
 }
 
 
--- tools/AssetDB/AssetDB.h
+++ tools/AssetDB/AssetDB.h
@@ -19,6 +19,7 @@ public:
 private:
 
 	bool CreateTables(void);
+	bool DropTables(void);
 
 private:
 	sql::SqlLiteDb db_;
