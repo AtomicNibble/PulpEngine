7e7f6622bb4d8464ca05351b271805d6d8b708bb
Tom Crowley <tom@tom-crowley.co.uk>
2016-02-07 21:57:19 +0000

    Add some logic for setting mod and adding a asset.

--- tools/AssetDB/AssetDB.cpp
+++ tools/AssetDB/AssetDB.cpp
@@ -8,7 +8,8 @@ X_NAMESPACE_BEGIN(assetDb)
 const char* AssetDB::DB_NAME = X_ENGINE_NAME"_asset.db";
 
 
-AssetDB::AssetDB()
+AssetDB::AssetDB() :
+	modId_(-1)
 {
 
 }
@@ -16,6 +17,72 @@ AssetDB::AssetDB()
 AssetDB::~AssetDB()
 {
 
+}
+
+bool AssetDB::AddAsset(AssetType::Enum type, const core::string& name)
+{
+	if (!isModSet()) {
+		X_ERROR("AssetDB", "Mod must be set before calling AddAsset!");
+		return false;
+	}
+
+	sql::SqlLiteTransaction trans(db_);
+	sql::SqlLiteCmd cmd(db_, "INSERT INTO file_ids (name, type, mod_id) VALUES(?,?,?)");
+	cmd.bind(1, name.c_str());
+	cmd.bind(2, type);
+	cmd.bind(3, modId_);
+
+	int32_t res = cmd.execute();
+	if (res != 0) {
+		return false;
+	}
+
+	trans.commit();
+	return true;
+}
+
+
+bool AssetDB::SetMod(const core::string& name)
+{
+	// does the mod exsist?
+	{
+		sql::SqlLiteTransaction trans(db_, true);
+		sql::SqlLiteQuery qry(db_, "SELECT mod_id, name FROM mods WHERE name = ?");
+		qry.bind(0, name.c_str());
+
+
+		sql::SqlLiteQuery::iterator it = qry.begin();
+
+		if (it != qry.end())
+		{
+			modId_ = (*it).get<int32_t>(0);
+
+			X_LOG0("AssetDB", "Mod set: \"%s\" id: %i", name.c_str(), modId_);
+			return true;
+		}
+	}
+
+	{
+		sql::SqlLiteTransaction trans(db_);
+		sql::SqlLiteCmd cmd(db_, "INSERT INTO mods (name) VALUES(?)");
+		cmd.bind(1, name.c_str());
+
+		int32_t res = cmd.execute();
+		if (res != 0) {
+			return false;
+		}
+
+		trans.commit();
+	}
+
+	modId_ = safe_static_cast<int32_t, sql::SqlLiteDb::RowId>(db_.lastInsertRowid());
+	X_LOG0("AssetDB", "Mod set: \"%s\" id: %i", name.c_str(), modId_);
+	return true;
+}
+
+bool AssetDB::isModSet(void) const
+{
+	return modId_ >= 0;
 }
 
 bool AssetDB::OpenDB(void)
@@ -30,28 +97,38 @@ void AssetDB::CloseDB(void)
 
 bool AssetDB::CreateTables(void)
 {
-	// we want to store a list of the assets.
-	// so create a table which has every asset in it.
+	if (!db_.execute("CREATE TABLE IF NOT EXISTS mods ("
+		" mod_id INTEGER PRIMARY KEY,"
+		"name TEXT COLLATE NOCASE"
+		");")) {
+		return false;
+	}
 
 	if (!db_.execute("CREATE TABLE IF NOT EXISTS file_ids ("
 		" file_id INTEGER PRIMARY KEY,"
 		"name TEXT COLLATE NOCASE,"
 		"path TEXT,"
 		"type INTEGER,"
-		"write_time INTEGER"
+		"add_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+		"lastUpdateTime TIMESTAMP,"
+		"mod_id INTEGER,"
+		"FOREIGN KEY(mod_id) REFERENCES mods(mod_id)"
 		");")) {
 		return false;
 	}
 
 
-
-	return false;
+	return true;
 }
 
 bool AssetDB::DropTables(void)
 {
 
 	if (!db_.execute("DROP TABLE IF EXISTS gdt_files;")) {
+		return false;
+	}
+
+	if (!db_.execute("DROP TABLE IF EXISTS mods;")) {
 		return false;
 	}
 
--- tools/AssetDB/AssetDB.h
+++ tools/AssetDB/AssetDB.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include "../SqLite/SqlLib.h"
 
 X_NAMESPACE_BEGIN(assetDb)
-
 
 
 class AssetDB
@@ -10,18 +10,26 @@ class AssetDB
 	static const char* DB_NAME;
 
 public:
+	X_DECLARE_ENUM(AssetType)(MODEL, ANIM);
+
+public:
 	AssetDB();
 	~AssetDB();
 
+	bool AddAsset(AssetType::Enum type, const core::string& name);
+	bool SetMod(const core::string& name);
+
 	bool OpenDB(void);
 	void CloseDB(void);
-
-private:
-
 	bool CreateTables(void);
 	bool DropTables(void);
 
 private:
+	bool isModSet(void) const;
+
+private:
+	int32_t modId_;
+
 	sql::SqlLiteDb db_;
 };
 
