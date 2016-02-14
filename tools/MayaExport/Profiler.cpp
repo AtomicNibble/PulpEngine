48b4e3164f5d453be5ca1a0baf4cc7924b457988
Tom Crowley <tom@tom-crowley.co.uk>
2016-02-14 22:08:58 +0000

    Update profile to use the stopwatch class and print the resutls via MayaUtil.

---
+++ tools/MayaExport/Profiler.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Profiler.h"
+
+#include "MayaUtil.h"
+
+MayaProfiler::MayaProfiler(const char* name) :
+	name_(name)
+{
+
+}
+
+MayaProfiler::~MayaProfiler()
+{
+	float ms = timer_.GetMilliSeconds();
+
+	core::StackString512 msg;
+
+	if (name_ != nullptr) {
+		msg.append(name_);
+	}
+
+	msg.appendFmt(" (%gms)", ms);
+
+	MayaUtil::MayaPrintMsg(msg.c_str());
+}
--- tools/MayaExport/Profiler.h
+++ tools/MayaExport/Profiler.h
@@ -1,47 +1,16 @@
 #pragma once
 
-
+#include <Time\StopWatch.h>
 
 class MayaProfiler
 {
 public:
-	MayaProfiler(const char* name) : name_(name) {
-		start_ = getTime();
-	}
-	~MayaProfiler() {
-		int64_t elapsed = (getTime() - start_);
-
-		double ms = convertToMs(elapsed);
-
-		if (name_ != nullptr)
-			std::cout << name_;
-		std::cout << " (" << ms << "ms)\n";
-	}
+	MayaProfiler(const char* name);
+	~MayaProfiler();
 
 private:
 	const char* name_;
-	int64_t start_;
-
-	static double s_frequency;     
-
-public:
-	static double convertToMs(int64_t elapsed) {
-		if (s_frequency == 0.f)
-		{
-			LARGE_INTEGER freq;
-			QueryPerformanceFrequency(&freq);
-			s_frequency = 1.0 / double(freq.QuadPart);
-			s_frequency *= 1000.0;
-		}
-
-		return elapsed * s_frequency;
-	}
-
-	static int64_t getTime() {
-		LARGE_INTEGER start;
-		QueryPerformanceCounter(&start);
-		return  start.QuadPart;
-	}
+	core::StopWatch timer_;
 };
 
 #define PROFILE_MAYA(name) \
