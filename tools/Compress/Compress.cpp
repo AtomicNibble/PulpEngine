0c6ea7cf4664d167d6c30fecada9299870afdcf8
Tom Crowley <tom@tom-crowley.co.uk>
2016-04-14 09:41:47 +0100

    Start up engine app then parse args. Add in logging for elapsed time etc.

--- tools/Compress/Compress.cpp
+++ tools/Compress/Compress.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Compress.h"
+#include "EngineApp.h"
 
 #define _LAUNCHER
 #include <ModuleExports.h>
@@ -16,6 +17,14 @@
 #include <iostream>
 #include <fstream>
 
+#ifdef X_LIB
+
+struct XRegFactoryNode* g_pHeadToRegFactories = 0;
+
+X_LINK_LIB("engine_Core")
+
+#endif // !X_LIB
+
 namespace
 {
 
@@ -27,6 +36,7 @@ namespace
 		core::SimpleMemoryTagging
 	> CompressorArena;
 
+	X_DECLARE_ENUM(Algo)(LZ4, LZMA, ZLIB);
 
 	struct FileInfo
 	{
@@ -35,6 +45,7 @@ namespace
 		}
 		uint64_t deflatedSize;
 		uint64_t inflatedSize;
+		Algo::Enum algo;
 	};
 
 
@@ -87,9 +98,8 @@ namespace
 
 	};
 
-	X_DECLARE_ENUM(Algo)(LZ4, LZMA, ZLIB);
 
-	bool ReadFileToBuf(const std::string& filePath, core::Array<uint8_t>& bufOut, FileInfo* pFinfo = nullptr)
+	bool ReadFileToBuf(const std::wstring& filePath, core::Array<uint8_t>& bufOut, FileInfo* pFinfo = nullptr)
 	{
 		std::ifstream file(filePath, std::ios::binary | std::ios::ate);
 		std::streamsize size = file.tellg();
@@ -112,7 +122,7 @@ namespace
 		return false;
 	}
 
-	bool WriteFileToBuf(const std::string& filePath, const core::Array<uint8_t>& buf, FileInfo* pInfo)
+	bool WriteFileToBuf(const std::wstring& filePath, const core::Array<uint8_t>& buf, FileInfo* pInfo)
 	{
 		std::ofstream file(filePath, std::ios::binary | std::ios::out);
 
@@ -129,9 +139,21 @@ namespace
 		return false;
 	}
 
+
+	void PrintArgs(void)
+	{
+		X_LOG0("Compressor", "Args:");
+		X_LOG0("Compressor", "^6-if^7		(input file) ^1required");
+		X_LOG0("Compressor", "^6-of^7		(output file, default: file + algo) ^9not-required");
+		X_LOG0("Compressor", "^6-a^7		(algo 1:lz4 2:lzma 3:zlib, default: lza) ^1not-required");
+		X_LOG0("Compressor", "^6-d^7		(deflate 1/0, default: 1) ^9not-required");
+		X_LOG0("Compressor", "^6-lvl^7		(lvl 1-9, default: 5) ^9not-required");
+	}
+
 } // namespace 
 
 core::MemoryArenaBase* g_arena = nullptr;
+HINSTANCE g_hInstance = 0;
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -139,9 +161,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
+	g_hInstance = hInstance;
+
 	core::Console Console(L"Potato - Compressor");
 	Console.RedirectSTD();
-	Console.SetSize(60, 40, 2000);
+	Console.SetSize(100, 40, 2000);
 	Console.MoveTo(10, 10);
 
 
@@ -160,48 +184,114 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	// algo: lz4, lzma, zlib
 	// lvl: 1-9
 
-	std::string inFile = R"(C:\Users\WinCat\Documents\code\WinCat\engine\potatoengine\game_folder\mod\models\mech_body.model_raw.lz4)";
-	std::string outFile = R"(C:\Users\WinCat\Documents\code\WinCat\engine\potatoengine\game_folder\mod\models\mech_body.model_raw)";
+	EngineApp app;
 
-	bool defalte = false;
-	int32_t lvl = 5;
+	if (!app.Init(lpCmdLine, Console)) {
+		return -1;
+	}
+
+	PrintArgs();
+
+	std::wstring inFile, outFile;
 
 	Algo::Enum algo = Algo::LZ4;
+	bool defalte = true;
+	int32_t lvl = 5;
+
+	// args
+	{
+		const wchar_t* pInFile = gEnv->pCore->GetCommandLineArgForVarW(L"if");
+		if (!pInFile) {
+			X_ERROR("Compress", "Missing required arg -if");
+			return -1;
+		}
+
+		inFile = pInFile;
+
+		const wchar_t* pOutFile = gEnv->pCore->GetCommandLineArgForVarW(L"of");
+		if (pOutFile) {
+			outFile = pOutFile;
+		}
+
+		const wchar_t* pAlgo = gEnv->pCore->GetCommandLineArgForVarW(L"a");
+		if (pAlgo) {
+			int32_t iAlgo = core::strUtil::StringToInt<int32_t>(pAlgo);
+			iAlgo = constrain<int32_t>(iAlgo, 1, 3);
+
+			if (iAlgo == 1) {
+				algo = Algo::LZ4;
+			}
+			else if (iAlgo == 2) {
+				algo = Algo::LZMA;
+			}
+			else if (iAlgo == 3) {
+				algo = Algo::ZLIB;
+			}
+			else {
+				X_ASSERT_UNREACHABLE();
+				return -1;
+			}
+		}
+
+		const wchar_t* pLvl = gEnv->pCore->GetCommandLineArgForVarW(L"lvl");
+		if (pLvl) {
+			int32_t lvlArg = core::strUtil::StringToInt<int32_t>(pLvl);
+
+			lvl = constrain<int32_t>(lvlArg, 1, 9);		
+		}
+	}
+
+	core::Array<uint8_t> inFileData(g_arena);
+	core::Array<uint8_t> outfileData(g_arena);
+
+	core::StopWatch timer;
+	FileInfo info;
+
+	if (!ReadFileToBuf(inFile, inFileData, defalte ? nullptr : &info)) {
+		X_ERROR("Compress", "Failed to read input file");
+		return -1;
+	}
+	
+	const float loadTime = timer.GetMilliSeconds();
+	X_LOG0("Compress", "loadTime: ^2%fms", loadTime);
+
+	// if infalting get algo from file.
+	if (!defalte) {
+		algo = info.algo;
+	}
+
 	ICompressor* pCompressor = nullptr;
 
 	if (algo == Algo::LZ4) {
 		pCompressor = X_NEW(Compressor<core::Compression::LZ4>, g_arena, "LZ4");
-		if (defalte) {
-			outFile += ".lz4";
+		if (defalte && outFile.empty()) {
+			outFile = inFile + L".lz4";
+			info.algo = Algo::LZ4;
 		}
 	}
 	else if (algo == Algo::LZMA) {
 		pCompressor = X_NEW(Compressor<core::Compression::LZMA>, g_arena, "LZMA");
-		if (defalte) {
-			outFile += ".lzma";
+		if (defalte && outFile.empty()) {
+			outFile = inFile + L".lzma";
+			info.algo = Algo::LZ4;
 		}
 	}
 	else if (algo == Algo::ZLIB) {
 		pCompressor = X_NEW(Compressor<core::Compression::Zlib>, g_arena, "ZLIB");
-		if (defalte) {
-			outFile += ".zlib";
+		if (defalte && outFile.empty()) {
+			outFile = inFile + L".zlib";
+			info.algo = Algo::LZ4;
 		}
 	}
 	else {
+		X_ERROR("Compress","unknown compression algo: %i", algo);
 		return -1;
 	}
  
-	core::Array<uint8_t> inFileData(g_arena);
-	core::Array<uint8_t> outfileData(g_arena);
-
-	FileInfo info;
-	if (!ReadFileToBuf(inFile, inFileData, defalte ? nullptr : &info)) {
-		return -1;
-	}
 
 	bool res = false;
 
-	core::StopWatch timer;
+	timer.Start();
 
 	if (defalte) {
 		pCompressor->setLvl(lvl);
@@ -211,15 +301,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		res = pCompressor->inflate(g_arena, inFileData, info, outfileData);
 	}
 
-	float elapsedMS = timer.GetMilliSeconds();
+	const float compressTime = timer.GetMilliSeconds();
+	X_LOG0("Compress", "compressTime: ^2%fms", compressTime);
 
 	if (!res) {
+		X_ERROR("Compress", "%s failed.", defalte ? "deflation" : "inflation");
 		return -1;
 	}
 
+	timer.Start();
+
 	if (!WriteFileToBuf(outFile, outfileData, defalte ? &info : nullptr)) {
+		X_ERROR("Compress", "Failed to write output file");
 		return -1;
 	}
+
+	const float writeTime = timer.GetMilliSeconds();
+	X_LOG0("Compress", "writeTime: ^2%fms", writeTime);
 
 
 	X_DELETE(pCompressor, g_arena);
