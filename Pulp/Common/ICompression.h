027f7192045f5ddf8df8496f4115a20d712df08d
Tom Crowley <tom@tom-crowley.co.uk>
2016-04-17 16:36:44 +0100

    Add compression header, that has interface for wrapping all the supported compression algo's I writes the algorithm used and original size at start of deflated buffers, storing the information in a uniformed way for all the algorithms.

---
+++ Pulp/Common/ICompression.h
@@ -0,0 +1,165 @@
+#pragma once
+
+
+#ifndef _X_COMPRESSION_I_H_
+#define _X_COMPRESSION_I_H_
+
+
+X_NAMESPACE_BEGIN(core)
+
+namespace Compression
+{
+
+	X_DECLARE_ENUM8(Algo)(LZ4, LZMA, ZLIB);
+	X_DECLARE_ENUM(CompressLevel)(
+		LOW, // speed
+		NORMAL, // normal
+		HIGH // best
+	);
+
+	// placed at start of defalted buffers.
+	// that are made via Compressor<T>
+	struct BufferHdr
+	{
+		static const uint32_t MAGIC = 0x37BD98;
+
+		BufferHdr() {
+			core::zero_this(this);
+		}
+
+		X_INLINE bool IsMagicValid(void) const {
+			uint32_t val = (magic[0] << 16 | magic[1] << 8 | magic[2]);
+			return val == MAGIC;
+		}
+
+		Algo::Enum algo;
+		uint8_t magic[3];
+		// we don't allow hugh single deflated blocks.
+		// i see no use case.
+		uint32_t deflatedSize;
+		uint32_t inflatedSize;
+	};
+
+	X_ENSURE_SIZE(BufferHdr, 12);
+	
+	struct ICompressor
+	{
+		virtual ~ICompressor() {}
+
+		static Algo::Enum getAlgo(const core::Array<uint8_t>& data) {
+			X_ASSERT(data.size() >= sizeof(BufferHdr), "Buffer is too small to contain BufferHdr")(sizeof(BufferHdr), data.size());
+			const BufferHdr* pHdr = union_cast<const BufferHdr*, const uint8_t*>(data.ptr());
+			X_ASSERT(pHdr->IsMagicValid(), "Compressed buffer header is not valid")();
+			return pHdr->algo;
+		}
+
+		// max source buffer size.
+		virtual size_t maxSourceSize(void) const X_ABSTRACT;
+		// buffer than source is garanted to fit into.
+		virtual size_t requiredDeflateDestBuf(size_t sourceLen) const X_ABSTRACT;
+
+		template<typename T>
+		bool deflate(core::MemoryArenaBase* arena, const core::Array<T>& data,
+			core::Array<uint8_t>& compressed, CompressLevel::Enum lvl = CompressLevel::NORMAL);
+
+		template<typename T>
+		bool inflate(core::MemoryArenaBase* arena, const core::Array<uint8_t>& data,
+			core::Array<T>& inflated);
+
+	private:
+		virtual bool deflate_int(core::MemoryArenaBase* arena, const void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen, size_t& destLenOut,
+			CompressLevel::Enum lvl = CompressLevel::NORMAL) X_ABSTRACT;
+
+		virtual bool inflate_int(core::MemoryArenaBase* arena, const void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen) X_ABSTRACT;
+
+	};
+	
+	template<typename T>
+	X_INLINE bool ICompressor::deflate(core::MemoryArenaBase* arena, const core::Array<T>& data,
+		core::Array<uint8_t>& compressed, CompressLevel::Enum lvl)
+	{
+		static_assert(compileTime::IsPOD<T>::Value, "T must be a POD type.");
+
+		size_t compressedSize = 0;
+		size_t bufSize = requiredDeflateDestBuf(data.size() * sizeof(T));
+
+		compressed.resize(bufSize);
+
+		bool res = deflate_int(arena, data.ptr(), data.size() * sizeof(T),
+			compressed.ptr(), compressed.size(), compressedSize, lvl);
+
+		compressed.resize(compressedSize);
+		return res;
+	}
+
+	template<typename T>
+	X_INLINE bool ICompressor::inflate(core::MemoryArenaBase* arena, const core::Array<uint8_t>& data, core::Array<T>& inflated)
+	{
+		static_assert(compileTime::IsPOD<T>::Value, "T must be a POD type.");
+
+		BufferHdr* pHdr = union_cast<BufferHdr*, const uint8_t*>(data.ptr());
+		inflated.resize(pHdr->inflatedSize);
+
+		return inflate_int(arena, 
+			pHdr + 1, data.size() - sizeof(BufferHdr), 
+			inflated.ptr(), inflated.size() * sizeof(T));
+	}
+
+
+	template<typename T>
+	struct Compressor : public ICompressor
+	{
+		using ICompressor::deflate;
+		using ICompressor::inflate;
+
+		virtual size_t maxSourceSize(void) const X_FINAL
+		{
+			return T::maxSourceSize();
+		}
+
+		// buffer than source is garanted to fit into.
+		virtual size_t requiredDeflateDestBuf(size_t sourceLen) const X_FINAL
+		{
+			return T::requiredDeflateDestBuf(sourceLen) + sizeof(BufferHdr);
+		}
+
+	private:
+		virtual bool deflate_int(core::MemoryArenaBase* arena, const void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen, size_t& destLenOut,
+			CompressLevel::Enum lvl) X_FINAL
+		{
+			uint8_t* pDestu8 = reinterpret_cast<uint8_t*>(pDstBuf);
+
+			bool res =  T::deflate(arena, pSrcBuf, srcBufLen, 
+				pDestu8 + sizeof(BufferHdr), destBufLen - sizeof(BufferHdr), destLenOut, lvl);
+
+			if (res) {
+				BufferHdr* pHdr = union_cast<BufferHdr*, void*>(pDstBuf);
+				pHdr->inflatedSize = safe_static_cast<uint32_t, size_t>(srcBufLen);
+				pHdr->deflatedSize = safe_static_cast<uint32_t, size_t>(destLenOut);
+				pHdr->algo = T::getAlgo();
+				pHdr->magic[0] = ((BufferHdr::MAGIC & 0xff0000) >> 16) & 0xff;
+				pHdr->magic[1] = ((BufferHdr::MAGIC & 0xff00) >> 8) & 0xff;
+				pHdr->magic[2] = (BufferHdr::MAGIC & 0xff) & 0xff;
+
+				// inc hdr in dest len
+				destLenOut += sizeof(BufferHdr);
+			}
+
+			return res;
+		}
+
+		virtual bool inflate_int(core::MemoryArenaBase* arena, const void* pSrcBuf, size_t srcBufLen,
+			void* pDstBuf, size_t destBufLen) X_FINAL
+		{
+			return T::inflate(arena, pSrcBuf, srcBufLen, pDstBuf, destBufLen);
+		}
+	};
+
+} // namespace Compression
+
+X_NAMESPACE_END
+
+#endif // !_X_COMPRESSION_I_H_
