b43454a8012a2f1e57b22b62cc54cd95d29f8308
Tom Crowley <tom@tom-crowley.co.uk>
2016-04-23 19:01:32 +0100

    Add asset manager project.

---
+++ tools/AssetManager/AssetManager.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "AssetManager.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+   
+
+	return 0;
+}
+
+
