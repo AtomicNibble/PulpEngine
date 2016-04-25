61f2ff820d89f4e1595fcc9ef46b7613e73bbf2d
Tom Crowley <tom@tom-crowley.co.uk>
2016-04-25 19:22:33 +0100

    Add appname and version.

--- tools/AssetManager/main.cpp
+++ tools/AssetManager/main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationDisplayName("AssetManager");
+    a.setApplicationName("AssetManager");
+    a.setApplicationVersion("0.0.0.1");
+
     AssetManager w;
     w.show();
 
