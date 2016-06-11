d5e121aa36f8318241f71fde6feffdbf384e8103
Tom Crowley <tom@tom-crowley.co.uk>
2016-06-11 18:27:53 +0100

    Add some frame flags to FrameData, currently just set if game was in focus at start of frame.

--- Pulp/Common/IFrameData.h
+++ Pulp/Common/IFrameData.h
@@ -5,11 +5,20 @@
 
 X_NAMESPACE_BEGIN(core)
 
+X_DECLARE_FLAGS(FrameFlag)(
+	HAS_FOCUS
+
+);
+
+
 struct FrameData
 {
+	typedef Flags<FrameFlag> FrameFlags;
+
 	TimeVal startTime;
 	float delta;
 
+	FrameFlags flags;
 
 };
 
