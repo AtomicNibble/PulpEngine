70c9cdfbea6699942299eaf5b5747e6075a638c8
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-02 12:12:28 +0100

    Add 32radix sort.

--- Pulp/Common/Sorting/RadixSort.h
+++ Pulp/Common/Sorting/RadixSort.h
@@ -92,7 +92,60 @@ namespace Sorting
 	}
 
 
+	template<typename IndexType>
+	X_INLINE void radix_sort_uint32_buf(core::Array<uint32_t>& in,
+		core::Array<IndexType>& sortedIndexes, core::MemoryArenaBase* tempIndexArena)
+	{
+		const IndexType num = safe_static_cast<IndexType, size_t>(in.size());
+		size_t count[0x100] = {};
+		IndexType* pBuckets[0x100];
 
+		core::Array<IndexType> indexTemp(tempIndexArena);
+
+		sortedIndexes.resize(num);
+		indexTemp.resize(num);
+
+		for (IndexType i = 0; i < num; i++) {
+			count[in[i] & 0xFF]++;
+		}
+
+		IndexType* pBucketDest = indexTemp.ptr();
+
+		for (uint16_t i = 0; i < 0x100; pBucketDest += count[i++]) {
+			pBuckets[i] = pBucketDest;
+		}
+
+		for (IndexType i = 0; i < num; i++) {
+			*pBuckets[in[i] & 0xFF]++ = i;
+		}
+
+		pBucketDest = sortedIndexes.ptr();
+		IndexType* pSrc = indexTemp.ptr();
+
+		for (size_t shift = 8; shift < 32; shift += 8)
+		{
+			core::zero_object(count);
+
+			for (IndexType i = 0; i < num; i++) {
+				count[(in[i] >> shift) & 0xFF]++;
+			}
+
+			IndexType* pBucketDestStart = pBucketDest;
+			for (uint16_t i = 0; i < 0x100; pBucketDest += count[i++]) {
+				pBuckets[i] = pBucketDest;
+			}
+			pBucketDest = pBucketDestStart;
+
+
+			for (IndexType i = 0; i < num; i++) {
+				const IndexType sortedIdx = pSrc[i];
+				*pBuckets[(in[sortedIdx] >> shift) & 0xFF]++ = sortedIdx;
+			}
+
+
+			core::Swap(pSrc, pBucketDest);
+		}
+	}
 
 } // namespace Sorting
 
