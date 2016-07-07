7883841c02fb8176103614d12aedec5035bed061
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-07 18:08:28 +0100

    Remove commented out code for XCamera, it was moved into base class XFrustum.

--- Pulp/Common/Math/XCamera.h
+++ Pulp/Common/Math/XCamera.h
@@ -76,6 +76,9 @@ public:
 		float32_t farpane = DEFAULT_FAR, float32_t pixelAspectRatio = 1.0f);
 
 
+
+
+
 protected:
 
 };
@@ -88,85 +91,5 @@ X_INLINE void XCamera::SetFrustum(uint32_t nWidth, uint32_t nHeight, float32_t F
 		nearplane, farpane, fPixelAspectRatio);
 }
 
-#if 0
-// Frustum diagram: http://winpic.co/152d7037
-// goat: http://winpic.co/153f6eb6
-// --------------------------------------------
-X_INLINE void XCamera::SetFrustum(uint32_t nWidth, uint32_t nHeight, float32_t FOV,
-		float32_t nearplane, float32_t farpane, float32_t fPixelAspectRatio)
-{
-	X_ASSERT(nearplane > 0.001f, "near plane not valid")(nearplane);
-	X_ASSERT(farpane > 0.1f, "far plane not valid")(farpane);
-	X_ASSERT(farpane > nearplane, "near plane is less than far plane")(farpane, nearplane);
-
-	width_ = nWidth;
-	height_ = nHeight; 
-
-	fov_ = FOV;
-
-	float32_t fWidth = (((float32_t)nWidth) / fPixelAspectRatio);
-	float32_t fHeight = (float32_t)nHeight;
-
-	projectionRatio_ = fWidth / fHeight;    // projection ratio (1.0 for square pixels)
-	pixelAspectRatio_ = fPixelAspectRatio;
-
-	float32_t projLeftTopX = -fWidth*0.5f;
-	float32_t projLeftTopY = (float32_t)((1.0f / math<float32_t>::tan(fov_ * 0.5f)) * (fHeight * 0.5f));
-	float32_t projLeftTopZ = fHeight * 0.5f;
-
-
-	edge_plt_.x = projLeftTopX;
-	edge_plt_.y = projLeftTopY;
-	edge_plt_.z = projLeftTopZ;
-//	assert(fabs(acos_tpl(Vec3r(0, m_edge_plt.y, m_edge_plt.z).GetNormalized().y) * 2 - m_fov)<0.001);
-
-	float invProjLeftTopY = 1.0f / projLeftTopY;
-	edge_nlt_.x = nearplane * projLeftTopX * invProjLeftTopY;
-	edge_nlt_.y = nearplane;
-	edge_nlt_.z = nearplane * projLeftTopZ * invProjLeftTopY;
-
-	//calculate the left/upper edge of the far-plane (=not rotated) 
-	edge_flt_.x = projLeftTopX  * (farpane * invProjLeftTopY);
-	edge_flt_.y = farpane;
-	edge_flt_.z = projLeftTopZ  * (farpane * invProjLeftTopY);
-
-	UpdateFrustum();
-}
-
-X_INLINE void XCamera::UpdateFrustum()
-{
-	//-------------------------------------------------------------------
-	//--- calculate frustum-edges of projection-plane in CAMERA-SPACE ---
-	//-------------------------------------------------------------------
-	Matrix33f m33 = matrix_.subMatrix33(0,0);
-
-	cltp_ = m33*Vec3f(+edge_plt_.x, +edge_plt_.y, +edge_plt_.z);
-	crtp_ = m33*Vec3f(-edge_plt_.x, +edge_plt_.y, +edge_plt_.z);
-	clbp_ = m33*Vec3f(+edge_plt_.x, +edge_plt_.y, -edge_plt_.z);
-	crbp_ = m33*Vec3f(-edge_plt_.x, +edge_plt_.y, -edge_plt_.z);
-
-	cltn_ = m33*Vec3f(+edge_nlt_.x, +edge_nlt_.y, +edge_nlt_.z);
-	crtn_ = m33*Vec3f(-edge_nlt_.x, +edge_nlt_.y, +edge_nlt_.z);
-	clbn_ = m33*Vec3f(+edge_nlt_.x, +edge_nlt_.y, -edge_nlt_.z);
-	crbn_ = m33*Vec3f(-edge_nlt_.x, +edge_nlt_.y, -edge_nlt_.z);
-
-	cltf_ = m33*Vec3f(+edge_flt_.x, +edge_flt_.y, +edge_flt_.z);
-	crtf_ = m33*Vec3f(-edge_flt_.x, +edge_flt_.y, +edge_flt_.z);
-	clbf_ = m33*Vec3f(+edge_flt_.x, +edge_flt_.y, -edge_flt_.z);
-	crbf_ = m33*Vec3f(-edge_flt_.x, +edge_flt_.y, -edge_flt_.z);
-
-	const Vec3f& position = GetPosition();
-
-	// set the 3 points for each plane.
-	fp_[FrustumPlanes::NEAR].set(crtn_ + position, cltn_ + position, crbn_ + position);
-	fp_[FrustumPlanes::RIGHT].set(crbf_ + position, crtf_ + position, position);
-	fp_[FrustumPlanes::LEFT].set(cltf_ + position, clbf_ + position, position);
-	fp_[FrustumPlanes::TOP].set(crtf_ + position, cltf_ + position, position);
-	fp_[FrustumPlanes::BOTTOM].set(clbf_ + position, crbf_ + position, position);
-	fp_[FrustumPlanes::FAR].set(crtf_ + position, crbf_ + position, cltf_ + position);  //clip-plane
-
-
-}
-#endif
 
 #endif // !_X_MATH_CAMERA_H_
