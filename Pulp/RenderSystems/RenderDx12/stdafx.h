1dab8e778ad121e6adaf6433a2c0daa5d3fc124f
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-10 23:01:36 +0100

    Include directx headers and link the dx12 lib.

--- Pulp/RenderSystems/RenderDx12/stdafx.h
+++ Pulp/RenderSystems/RenderDx12/stdafx.h
@@ -5,3 +5,19 @@
 #define IPRENDER_EXPORTS
 
 #include <IRender.h>
+
+
+
+
+
+X_DISABLE_WARNING(4005)
+
+
+#include <../../3rdparty/source/directx/D3D12.h>
+#include <../../3rdparty/source/directx/D3D11.h>
+#include <../../3rdparty/source/directx/d3dx10.h>
+
+X_ENABLE_WARNING(4005)
+
+
+X_LINK_LIB("D3D12.lib")
