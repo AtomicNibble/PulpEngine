d744d1021451b20b629a390e0efbebc22b2d60a2
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-14 10:28:29 +0100

    Change the virtual address null / unkown to size_t.

--- Pulp/RenderSystems/RenderDx12/GpuResource.h
+++ Pulp/RenderSystems/RenderDx12/GpuResource.h
@@ -3,8 +3,8 @@
 
 X_NAMESPACE_BEGIN(render)
 
-static const D3D12_GPU_VIRTUAL_ADDRESS D3D12_GPU_VIRTUAL_ADDRESS_NULL = static_cast<D3D12_GPU_VIRTUAL_ADDRESS>(0);
-static const D3D12_GPU_VIRTUAL_ADDRESS D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN = static_cast<D3D12_GPU_VIRTUAL_ADDRESS>(-1);
+static const size_t D3D12_GPU_VIRTUAL_ADDRESS_NULL = static_cast<size_t>(0);
+static const size_t D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN = static_cast<size_t>(-1);
 
 class GpuResource
 {
