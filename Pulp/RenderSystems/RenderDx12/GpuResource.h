73b4fba6bce3f85941aa6c11fd2095fba4b19a4c
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-15 01:41:21 +0100

    Add a constants file for some global directx constants.

--- Pulp/RenderSystems/RenderDx12/GpuResource.h
+++ Pulp/RenderSystems/RenderDx12/GpuResource.h
@@ -3,8 +3,6 @@
 
 X_NAMESPACE_BEGIN(render)
 
-static const size_t D3D12_GPU_VIRTUAL_ADDRESS_NULL = static_cast<size_t>(0);
-static const size_t D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN = static_cast<size_t>(-1);
 
 class GpuResource
 {
