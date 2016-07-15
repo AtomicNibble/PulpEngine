4f9a6b4ece0a327177ffa888db2cb6c3da2aceec
Tom Crowley <tom@tom-crowley.co.uk>
2016-07-15 11:57:56 +0100

    Move some inlines to the inl file.

--- Pulp/RenderSystems/RenderDx12/SamplerDesc.h
+++ Pulp/RenderSystems/RenderDx12/SamplerDesc.h
@@ -8,41 +8,11 @@ class DescriptorAllocator;
 class SamplerDesc : public D3D12_SAMPLER_DESC
 {
 public:
-	SamplerDesc()
-	{
-		Filter = D3D12_FILTER_ANISOTROPIC;
-		AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-		AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-		AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-		MipLODBias = 0.0f;
-		MaxAnisotropy = 16;
-		ComparisonFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-		BorderColor[0] = 1.0f;
-		BorderColor[1] = 1.0f;
-		BorderColor[2] = 1.0f;
-		BorderColor[3] = 1.0f;
-		MinLOD = 0.0f;
-		MaxLOD = D3D12_FLOAT32_MAX;
-	}
+	X_INLINE SamplerDesc();
 
-	void setTextureAddressMode(D3D12_TEXTURE_ADDRESS_MODE addressMode)
-	{
-		AddressU = addressMode;
-		AddressV = addressMode;
-		AddressW = addressMode;
-	}
-
-	void setBorderColor(Color8u border)
-	{
-		setBorderColor(Colorf(border));
-	}
-	void setBorderColor(const Colorf& border)
-	{
-		BorderColor[0] = border.r;
-		BorderColor[1] = border.g;
-		BorderColor[2] = border.b;
-		BorderColor[3] = border.a;
-	}
+	X_INLINE void setTextureAddressMode(D3D12_TEXTURE_ADDRESS_MODE addressMode);
+	X_INLINE void setBorderColor(Color8u border);
+	X_INLINE void setBorderColor(const Colorf& border);
 };
 
 
