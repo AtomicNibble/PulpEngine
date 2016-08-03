5b44229893e8f8eaa128193e41befae113e77d2f
Tom Crowley <tom@tom-crowley.co.uk>
2016-08-03 18:21:14 +0100

    Needed to make gtest compile after /std:latest turned on. I think it's cus tuple is no longer in std::tr1::

--- Pulp/UnitTests/stdafx.h
+++ Pulp/UnitTests/stdafx.h
@@ -4,6 +4,9 @@
 
 #include <EngineCommon.h>
 
+#define GTEST_HAS_TR1_TUPLE 0
+// if need tuple define this, fucking vs std/latest breaking shit.
+// #define GTEST_USE_OWN_TR1_TUPLE 1
 #include <gtest\gtest.h>
 
 #include "UT_AssertChecker.h"
