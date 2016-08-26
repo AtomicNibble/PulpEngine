194e261038b448434a6c53949876f5e35f3ac64c
Tom Crowley <tom@tom-crowley.co.uk>
2016-08-26 20:36:36 +0100

    Add some more asset types.

--- Pulp/Common/IAssetDb.h
+++ Pulp/Common/IAssetDb.h
@@ -5,6 +5,29 @@
 X_NAMESPACE_BEGIN(assetDb)
 
 
-X_DECLARE_ENUM(AssetType)(MODEL, ANIM, MATERIAL, IMG);
+X_DECLARE_ENUM(AssetType)(
+	MODEL, 
+	ANIM,
+	MATERIAL, 
+	IMG,
+	WEAPON, // do we want diffent weapon types, or will the asset contain the sub type?
+	TURRET,
+	// will allow for data driven lights, that can then be placed in editor rather than having fixed light types.
+	LIGHT,
+	FX,
+	// cam shake
+	RUMBLE,
+
+	SHELLSHOCK,
+	// pre defined collection of models and settings to make up a char.
+	CHARACTER,
+	// beep beep, be a long time before this type of asset gets implemented...
+	VEHICLE,
+	// these will be pre defined camera paths
+	// that can be played.
+	// option to loop, hude hud, trigger note tracks..
+	// basically a anim for the camera
+	CAMERA
+);
 
 X_NAMESPACE_END
