a7e3f3c1a09b1586962c32aec4b204ea93ca9cda
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-04 22:07:36 +0100

    Move the Converter types to a seperate file, so it can be shared without having to include the Converter def.

--- tools/ImgLib/Converter/Converter.h
+++ tools/ImgLib/Converter/Converter.h
@@ -3,63 +3,17 @@
 
 #include <ITexture.h>
 #include "TextureFile.h"
+#include "Types.h"
 
 X_NAMESPACE_DECLARE(core,
 	class LinearAllocator;
 )
 
-
 X_NAMESPACE_BEGIN(texture)
 
 namespace Converter
 {
-	X_DECLARE_FLAGS(CompileFlag)(
-		NOMIPS,
-		PREMULTIPLY_ALPHA,
-		ALLOW_NONE_POW2,
-
-		STREAMABLE,			// can be streamed
-		HI_MIP_STREAMING,	// only high mips can be streamed.
-		FORCE_STREAM,		// force stream even if only one mip
-
-		NO_COMPRESSION		// leave as raw.
-	);
-
-
-	// we support producing a base mip smaller than original.
-	// so you can provide a 4096x4096 source apply 1/4 scaling and get a 1024x1024 base mip.
-	X_DECLARE_ENUM(ScaleFactor) (
-		ORIGINAL,
-		HALF,
-		QUARTER,
-		FOURTH,
-		EIGHTH
-	);
-
-	X_DECLARE_ENUM(ImgFileFormat) (
-		CI,
-		DDS,
-		PNG,
-		JPG,
-		PSD,
-		TGA,
-
-		UNKNOWN
-	);
-
-	X_DECLARE_ENUM(MipFilter) (
-		DEFAULT
-	);
-
-	X_DECLARE_ENUM(WrapMode) (
-		Mirror,
-		Repeat,
-		Clamp
-	);
-
-	typedef Flags<CompileFlag> CompileFlags;
-
-
+	
 	class ImgConveter
 	{
 		static const size_t MAX_FMT_CLASS_SIZE;
---
+++ tools/ImgLib/Converter/Types.h
@@ -0,0 +1,59 @@
+#pragma once
+
+
+
+X_NAMESPACE_BEGIN(texture)
+
+namespace Converter
+{
+
+	X_DECLARE_FLAGS(CompileFlag)(
+		NOMIPS,
+		PREMULTIPLY_ALPHA,
+		ALLOW_NONE_POW2,
+
+		STREAMABLE,			// can be streamed
+		HI_MIP_STREAMING,	// only high mips can be streamed.
+		FORCE_STREAM,		// force stream even if only one mip
+
+		NO_COMPRESSION		// leave as raw.
+	);
+
+
+	// we support producing a base mip smaller than original.
+	// so you can provide a 4096x4096 source apply 1/4 scaling and get a 1024x1024 base mip.
+	X_DECLARE_ENUM(ScaleFactor) (
+		ORIGINAL,
+		HALF,
+		QUARTER,
+		FOURTH,
+		EIGHTH
+	);
+
+	X_DECLARE_ENUM(ImgFileFormat) (
+		CI,
+		DDS,
+		PNG,
+		JPG,
+		PSD,
+		TGA,
+
+		UNKNOWN
+	);
+
+	X_DECLARE_ENUM(MipFilter) (
+		DEFAULT
+	);
+
+	X_DECLARE_ENUM(WrapMode) (
+		Mirror,
+		Repeat,
+		Clamp
+	);
+
+	typedef Flags<CompileFlag> CompileFlags;
+
+
+} // namespace Converter
+
+X_NAMESPACE_END
