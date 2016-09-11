f1e83eeeb97a9684a02e57f4d44cb9b8cf30f2aa
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-11 18:24:26 +0100

    Style updates.

--- tools/AssetManager/assetdbmodel.h
+++ tools/AssetManager/assetdbmodel.h
@@ -36,28 +36,22 @@ public:
     void setStartupProject(ProjectNode *projectNode);
 
 
-    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
-    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    QModelIndex index(int32_t row, int32_t column, const QModelIndex& parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex& index) const override;
+    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    bool setData(const QModelIndex& index, const QVariant &value, int role) override;
 
-    int rowCount(const QModelIndex & parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex & parent = QModelIndex()) const override;
-    bool hasChildren(const QModelIndex & parent = QModelIndex()) const override;
+    int32_t rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int32_t columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    bool hasChildren(const QModelIndex& parent = QModelIndex()) const override;
 
 
-
-    void added(FolderNode* folderNode, const QList<Node*> &newNodeList);
-    void removed(FolderNode* parentNode, const QList<Node*> &newNodeList);
-    void removeFromCache(QList<FolderNode *> list);
-    void changedSortKey(FolderNode *folderNode, Node *node);
-
-    bool canFetchMore(const QModelIndex & parent) const override;
-    void fetchMore(const QModelIndex & parent) override;
+    bool canFetchMore(const QModelIndex& parent) const override;
+    void fetchMore(const QModelIndex& parent) override;
     void fetchMore(FolderNode *foldernode) const;
 
-    Node *nodeForIndex(const QModelIndex &index) const;
+    Node *nodeForIndex(const QModelIndex& index) const;
     QModelIndex indexForNode(const Node *node);
 
     void recursiveAddFolderNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
@@ -65,7 +59,7 @@ public:
     void recursiveAddFileNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
     QList<Node*> childNodes(FolderNode *parentNode) const;
 
-    FolderNode *visibleFolderNode(FolderNode *node) const;
+    FolderNode* visibleFolderNode(FolderNode *node) const;
     bool filter(Node *node) const;
 
 private:
