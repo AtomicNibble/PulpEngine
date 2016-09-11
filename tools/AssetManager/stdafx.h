d3d77e3d8c23aab9010fe38ff596b54e8b9d5f54
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-11 23:16:39 +0100

    Fix the release builds.

--- tools/AssetManager/stdafx.h
+++ tools/AssetManager/stdafx.h
@@ -2,7 +2,16 @@
 #define PCH_H
 
 
+#ifdef NDEBUG 
+#define X_DEBUG 0
+#define X_RELEASE 1
+#define X_SUPER 0
+#else
 #define X_DEBUG 1
+#define X_RELEASE 0
+#define X_SUPER 0
+#endif // _DEBUG 
+
 #include <Pulp/Common/EngineCommon.h>
 #include <Pulp/Common/IAssetDb.h>
 
