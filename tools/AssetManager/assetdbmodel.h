8047f23089c1915931fc3ba8c971a484fda3f12a
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-13 21:57:55 +0100

    Fix spacing yo.

--- tools/AssetManager/assetdbmodel.h
+++ tools/AssetManager/assetdbmodel.h
@@ -13,82 +13,82 @@ X_NAMESPACE_BEGIN(assman)
 namespace AssetExplorer
 {
 
-class Node;
-class FileNode;
-class FolderNode;
-class ProjectNode;
-class SessionNode;
+	class Node;
+	class FileNode;
+	class FolderNode;
+	class ProjectNode;
+	class SessionNode;
 
 
-class AssetDBModel : public QAbstractItemModel
-{
-    Q_OBJECT
+	class AssetDBModel : public QAbstractItemModel
+	{
+		Q_OBJECT
 
-public:
-	typedef assetDb::AssetDB AssetDB;
+	public:
+		typedef assetDb::AssetDB AssetDB;
 
-public:
-    AssetDBModel(SessionNode *rootNode, AssetDB& db, QObject *parent = nullptr);
-    ~AssetDBModel();
+	public:
+		AssetDBModel(SessionNode *rootNode, AssetDB& db, QObject *parent = nullptr);
+		~AssetDBModel();
 
-    void setTreeView(QTreeView* pTree);
+		void setTreeView(QTreeView* pTree);
 
-    void reset();
-    void setStartupProject(ProjectNode *projectNode);
-
-
-    QModelIndex index(int32_t row, int32_t column, const QModelIndex& parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex& index) const override;
-    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
-    bool setData(const QModelIndex& index, const QVariant &value, int role) override;
-
-    int32_t rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    int32_t columnCount(const QModelIndex& parent = QModelIndex()) const override;
-    bool hasChildren(const QModelIndex& parent = QModelIndex()) const override;
+		void reset();
+		void setStartupProject(ProjectNode *projectNode);
 
 
-    bool canFetchMore(const QModelIndex& parent) const override;
-    void fetchMore(const QModelIndex& parent) override;
-    void fetchMore(FolderNode *foldernode) const;
+		QModelIndex index(int32_t row, int32_t column, const QModelIndex& parent = QModelIndex()) const override;
+		QModelIndex parent(const QModelIndex& index) const override;
+		QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const override;
+		Qt::ItemFlags flags(const QModelIndex& index) const override;
+		bool setData(const QModelIndex& index, const QVariant &value, int role) override;
 
-    Node *nodeForIndex(const QModelIndex& index) const;
-    QModelIndex indexForNode(const Node *node);
-
-	void added(FolderNode* pParentNode, const QList<Node*>& newNodeList);
-	void removed(FolderNode* pParentNode, const QList<Node*>& newNodeList);
-	void removeFromCache(QList<FolderNode*> list);
-	void changedSortKey(FolderNode* pFolderNode, Node* pNode);
-
-    void recursiveAddFolderNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
-    void recursiveAddFolderNodesImpl(FolderNode* pStartNode, QList<Node*>* pList) const;
-    void recursiveAddFileNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
-    QList<Node*> childNodes(FolderNode *parentNode) const;
-
-    FolderNode* visibleFolderNode(FolderNode *node) const;
-    bool filter(Node *node) const;
+		int32_t rowCount(const QModelIndex& parent = QModelIndex()) const override;
+		int32_t columnCount(const QModelIndex& parent = QModelIndex()) const override;
+		bool hasChildren(const QModelIndex& parent = QModelIndex()) const override;
 
 
-private slots:
+		bool canFetchMore(const QModelIndex& parent) const override;
+		void fetchMore(const QModelIndex& parent) override;
+		void fetchMore(FolderNode *foldernode) const;
 
-	void foldersAboutToBeAdded(FolderNode* pParentFolder, const QList<FolderNode*>& newFolders);
-	void foldersAdded(void);
+		Node *nodeForIndex(const QModelIndex& index) const;
+		QModelIndex indexForNode(const Node *node);
 
-	// files
-	void filesAboutToBeAdded(FolderNode* pFolder, const QList<FileNode*>& newFiles);
-	void filesAdded(void);
+		void added(FolderNode* pParentNode, const QList<Node*>& newNodeList);
+		void removed(FolderNode* pParentNode, const QList<Node*>& newNodeList);
+		void removeFromCache(QList<FolderNode*> list);
+		void changedSortKey(FolderNode* pFolderNode, Node* pNode);
+
+		void recursiveAddFolderNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
+		void recursiveAddFolderNodesImpl(FolderNode* pStartNode, QList<Node*>* pList) const;
+		void recursiveAddFileNodes(FolderNode* pStartNode, QList<Node*>* pList) const;
+		QList<Node*> childNodes(FolderNode *parentNode) const;
+
+		FolderNode* visibleFolderNode(FolderNode *node) const;
+		bool filter(Node *node) const;
 
 
-private:
-   QTreeView* treeview_;
-   ProjectNode* startupProject_;
-   SessionNode* rootNode_;
-   mutable QHash<FolderNode*, QList<Node*> > childNodes_;
+		private slots:
 
-   FolderNode* pParentFolderForChange_;
+		void foldersAboutToBeAdded(FolderNode* pParentFolder, const QList<FolderNode*>& newFolders);
+		void foldersAdded(void);
 
-   AssetDB& db_;
-};
+		// files
+		void filesAboutToBeAdded(FolderNode* pFolder, const QList<FileNode*>& newFiles);
+		void filesAdded(void);
+
+
+	private:
+		QTreeView* treeview_;
+		ProjectNode* startupProject_;
+		SessionNode* rootNode_;
+		mutable QHash<FolderNode*, QList<Node*> > childNodes_;
+
+		FolderNode* pParentFolderForChange_;
+
+		AssetDB& db_;
+	};
 
 } // namespace AssetExplorer
 
