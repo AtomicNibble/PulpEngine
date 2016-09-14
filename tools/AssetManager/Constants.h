f015706ba648c718e4fe559adb374cf0187ff6ac
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-15 00:26:55 +0100

    Additional constants.

--- tools/AssetManager/Constants.h
+++ tools/AssetManager/Constants.h
@@ -18,6 +18,58 @@ namespace Constants
 	const char G_DEFAULT_THREE[] = "Group.Default.Three";
 
 
+	const char C_GLOBAL[] = "AssMan Context";
+
+	// Menubar
+	const char MENU_BAR[] = "MenuBar";
+
+	// Menus
+	const char M_FILE[] = "Menu.File";
+	const char M_FILE_NEW[] = "Menu.File.New";
+	const char M_FILE_OPEN[] = "Menu.File.Open";
+	const char M_FILE_RECENTFILES[] = "Menu.File.RecentFiles";
+	const char M_FILE_RECENTPROJECTS[] = "Menu.File.RecentProjects";
+	const char M_EDIT[] = "Menu.Edit";
+	const char M_EDIT_FIND[] = "Menu.Edit.Find";
+	const char M_VIEW[] = "Menu.View";
+	const char M_VIEW_TOOLBAR[] = "Menu.View.ToolBar";
+	const char M_DEBUG[] = "Menu.Debug";
+	const char M_TOOLS[] = "Menu.Tools";
+	const char M_WINDOW[] = "Menu.Window";
+	const char M_WINDOW_PANES[] = "Menu.Window.Panes";
+	const char M_WINDOW_WINDOWS[] = "Menu.Window.Windows";
+	const char M_HELP[] = "Menu.Help";
+
+
+	// Main menu bar groups
+	const char G_FILE[] = "Group.File";
+	const char G_EDIT[] = "Group.Edit";
+	const char G_VIEW[] = "Group.View";
+	const char G_DEBUG[] = "Group.Debug";
+	const char G_TOOLS[] = "Group.Tools";
+	const char G_WINDOW[] = "Group.Window";
+	const char G_HELP[] = "Group.Help";
+
+
+	// File menu groups
+	const char G_FILE_NEW[] = "Group.File.New";
+	const char G_FILE_OPEN[] = "Group.File.Open";
+	const char G_FILE_SAVE[] = "Group.File.Save";
+	const char G_FILE_RECENT[] = "Group.File.Recent";
+	const char G_FILE_CLOSE[] = "Group.File.Close";
+
+	// Help menu groups
+	const char G_HELP_HELP[] = "Group.Help.Help";
+	const char G_HELP_ABOUT[] = "Group.Help.About";
+
+
+	// Actions
+	const char EXIT[] = "Exit";
+
+
+	// Help Actions
+	const char SHOW_ABOUT[] = "Help.About";
+
 
 } // namespace 
 
