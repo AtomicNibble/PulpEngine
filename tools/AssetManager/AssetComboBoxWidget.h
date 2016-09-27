018d661440a6a4f62816d7b4c42464bb9f8b52e9
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-27 18:04:08 +0100

    Support combo value prop value overrides, so that the property is set to a value diffrent to the display text.
    Also seperate out the logic for parsing the values string so it can be re-used.

--- tools/AssetManager/AssetComboBoxWidget.h
+++ tools/AssetManager/AssetComboBoxWidget.h
@@ -12,14 +12,41 @@ class AssetComboBoxWidget : public QComboBox
 	Q_OBJECT
 
 public:
-	AssetComboBoxWidget(QWidget *parent, const std::string& value, bool editable = false);
+	struct ComboEntry
+	{
+		X_INLINE bool hasOverRide(void) const;
+
+		QString title;
+		QString valueOverride;
+	};
+
+	typedef QList<ComboEntry> ComboEntryArr;
+
+
+public:
+	AssetComboBoxWidget(QWidget *parent, const std::string& value, const std::string& valuesStr, bool editable = false);
 	~AssetComboBoxWidget();
+
+
+public:
+	static bool splitValues(const std::string& values, ComboEntryArr& valuesOut, QList<int32_t>* pSeperators = nullptr);
+
+private:
+	void setValue(const std::string& value);
 
 private slots:
 	void currentIndexChanged(int index);
 	void returnPressed(void);
 
+private:
+	ComboEntryArr values_;
+	bool editable_;
 };
+
+X_INLINE bool AssetComboBoxWidget::ComboEntry::hasOverRide(void) const
+{
+	return !valueOverride.isEmpty();
+}
 
 
 X_NAMESPACE_END
