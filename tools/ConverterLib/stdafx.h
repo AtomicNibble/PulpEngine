7d886c838e2dd18d215869687fe845684c0c4321
Tom Crowley <tom@tom-crowley.co.uk>
2016-09-29 19:36:32 +0100

    Link the libs we may use in converterLib when none dynamic build.

--- tools/ConverterLib/stdafx.h
+++ tools/ConverterLib/stdafx.h
@@ -15,3 +15,16 @@
 #endif // X_LIB
 
 
+#ifdef X_LIB
+
+X_LINK_LIB("engine_ModelLib")
+X_LINK_LIB("engine_AnimLib")
+X_LINK_LIB("engine_ImgLib")
+X_LINK_LIB("engine_MaterialLib")
+
+X_FORCE_SYMBOL_LINK("?factory__@XFactory@XConverterLib_Model@@0V12@A")
+X_FORCE_SYMBOL_LINK("?factory__@XFactory@XConverterLib_Anim@@0V12@A")
+X_FORCE_SYMBOL_LINK("?factory__@XFactory@XConverterLib_Img@@0V12@A")
+X_FORCE_SYMBOL_LINK("?factory__@XFactory@XConverterLib_Material@@0V12@A")
+
+#endif // !X_LIB
