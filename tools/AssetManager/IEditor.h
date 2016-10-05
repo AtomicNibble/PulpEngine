4c9a6c9495ecc4a46daaabe7958203c21190ba9c
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-05 23:29:32 +0100

    Remove extra semi colon.

--- tools/AssetManager/IEditor.h
+++ tools/AssetManager/IEditor.h
@@ -18,7 +18,7 @@ public:
 
 	virtual bool open(QString* pErrorString, const QString& assetName, assetDb::AssetType::Enum type) X_ABSTRACT;
 	virtual IAssetEntry* assetEntry(void) X_ABSTRACT;
-	virtual Id id(void) const X_ABSTRACT;;
+	virtual Id id(void) const X_ABSTRACT;
 
 	X_INLINE virtual bool duplicateSupported(void) const;
 	X_INLINE virtual IEditor* duplicate(void);
