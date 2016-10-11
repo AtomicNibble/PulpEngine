1c58b4366e738e23e558d76a5c0f0e126da2ed1c
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-11 22:49:42 +0100

    Add simpleFormatPolicy that removes color codes.

--- Pulp/Common/Logging/FormatPolicies/LoggerSimpleFormatPolicy.cpp
+++ Pulp/Common/Logging/FormatPolicies/LoggerSimpleFormatPolicy.cpp
@@ -26,7 +26,7 @@ uint32_t LoggerSimpleFormatPolicy::Format(LoggerBase::Line& line, const char* in
 	size_t verbosity, const char* format, va_list args
 	)
 {
-	int bytesWritten; 
+	int32_t bytesWritten;
 
 	X_UNUSED(type);
 	X_UNUSED(sourceInfo);
@@ -36,9 +36,47 @@ uint32_t LoggerSimpleFormatPolicy::Format(LoggerBase::Line& line, const char* in
 	bytesWritten += vsnprintf_s(&line[bytesWritten], sizeof(LoggerBase::Line) - bytesWritten, _TRUNCATE, format, args);
 	bytesWritten += _snprintf_s(&line[bytesWritten], sizeof(LoggerBase::Line) - bytesWritten, _TRUNCATE, "\n");
 
-	return safe_static_cast<uint32_t,int>( bytesWritten );
+	return safe_static_cast<uint32_t, int32_t>( bytesWritten );
 }
 
- 
+ // ------------------------------------------------
+
+
+void LoggerSimpleFormatPolicyStripColors::Init(void)
+{
+
+}
+
+void LoggerSimpleFormatPolicyStripColors::Exit(void)
+{
+
+}
+
+uint32_t LoggerSimpleFormatPolicyStripColors::Format(LoggerBase::Line& line, const char* indentation,
+	const char* type, const SourceInfo& sourceInfo, const char* channel,
+	size_t verbosity, const char* format, va_list args
+)
+{
+	int32_t bytesWritten = LoggerSimpleFormatPolicy::Format(line, indentation, type, sourceInfo, channel,
+																verbosity, format, args);
+
+	// this not so bad as the buffer should already be in the cache.
+	int32_t len = bytesWritten;
+	for (int32_t i = 0; i < len; i++)
+	{
+		if (line[i] == '^' && (i + 1) < len && core::strUtil::IsDigit(line[i + 1]))
+		{
+			// we remove both ^ and col.
+			const int32_t bytesToCopy = bytesWritten - i;
+			std::memmove(&line[i], &line[i + 2], bytesToCopy);
+
+			len -= 2;
+			bytesWritten -= 2;
+		}
+	}
+
+	return safe_static_cast<uint32_t, int32_t>(bytesWritten);
+}
+
 
 X_NAMESPACE_END
--- Pulp/Common/Logging/FormatPolicies/LoggerSimpleFormatPolicy.h
+++ Pulp/Common/Logging/FormatPolicies/LoggerSimpleFormatPolicy.h
@@ -36,6 +36,22 @@ public:
 		const char* format, va_list args);
 };
 
+
+class LoggerSimpleFormatPolicyStripColors : public LoggerSimpleFormatPolicy
+{
+public:
+	/// Empty implementation.
+	void Init(void);
+
+	/// Empty implementation.
+	void Exit(void);
+
+	/// Formats the given message.
+	uint32_t Format(LoggerBase::Line& line, const char* indentation, const char* type,
+		const SourceInfo& sourceInfo, const char* channel, size_t verbosity,
+		const char* format, va_list args);
+};
+
 X_NAMESPACE_END
 
 
