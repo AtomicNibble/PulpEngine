8404fc01fb318a59d64c166254a95e6dfe4c19ee
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-13 22:42:13 +0100

    Add widget for Anim.

---
+++ tools/AssetManager/AssetAnimWidget.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <QObject>
+
+#include "RawFileLoader.h"
+
+X_NAMESPACE_BEGIN(assman)
+
+class IAssetEntry;
+
+class AssetAnimWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	AssetAnimWidget(QWidget *parent, IAssetEntry* pAssEntry, const std::string& value);
+	~AssetAnimWidget();
+
+	void setPromptDialogTitle(const QString& title);
+	QString promptDialogTitle(void) const;
+
+	void setPromptDialogFilter(const QString& filter);
+	QString promptDialogFilter(void) const;
+
+	QString path(void) const;
+
+private:
+	void setPath(const QString& filePath);
+	void setErrorTip(const QString& tip);
+	void removeErrorTip(void);
+	bool loadRawModel(void);
+
+private slots:
+	void setValue(const std::string& value);
+	void editingFinished(void);
+	void validatePath(void);
+	void browseClicked(void);
+
+	void setProgress(int32_t pro);
+	void setProgressLabel(const QString& label, int32_t pro);
+	void rawFileLoaded(void);
+
+signals:
+	void valueChanged(const std::string& value);
+
+protected:
+	bool eventFilter(QObject* pObject, QEvent* pEvent) X_OVERRIDE;
+	void dragEnterEvent(QDragEnterEvent* pEvent) X_OVERRIDE;
+	void dropEvent(QDropEvent* pEvent) X_OVERRIDE;
+
+private:
+	QString makeDialogTitle(const QString& title);
+	static bool fileExtensionValid(const QString& path);
+
+
+private:
+	IAssetEntry* pAssEntry_;
+
+private:
+	RawFileLoader loader_;
+	QProgressDialog* pProgress_;
+
+private:
+	QLineEdit* pEditPath_;
+	QAction* pWarningAction_;
+
+	QString curPath_;
+	QString dialogFilter_;
+	QString dialogTitleOverride_;
+	QString initialBrowsePathOverride_;
+};
+
+X_NAMESPACE_END
