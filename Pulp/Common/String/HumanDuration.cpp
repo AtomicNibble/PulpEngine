2fdfc9dd2d130d338c7df2b4c5b58c8367a53ceb
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-15 13:28:05 +0100

    Add str util for turning ms into sensible text representations.

---
+++ Pulp/Common/String/HumanDuration.cpp
@@ -0,0 +1,43 @@
+#include "EngineCommon.h"
+#include "HumanDuration.h"
+
+
+X_NAMESPACE_BEGIN(core)
+
+namespace HumanDuration
+{
+
+	const char* toString(Str& str, float ms)
+	{
+		str.clear();
+
+		if (ms <= 10000.f)
+		{
+			str.appendFmt("%g ms", ms);
+		}
+		else if (ms <= 300000)
+		{
+			const float sec = (ms / 1000);
+			str.appendFmt("%.2f sec", sec);
+		}
+		else if (ms <= 16777216)
+		{
+			const float sec = (ms / 1000);
+			const float min = (sec / 60);
+
+			str.appendFmt("%.2f min", min);
+		}
+		else
+		{
+			const float sec = (ms / 1000);
+			const float min = (sec / 60);
+
+			str.appendFmt("%.2f min", min);
+		}
+
+		return str.c_str();
+	}
+
+} // namespace HumanDuration
+
+X_NAMESPACE_END;
---
+++ Pulp/Common/String/HumanDuration.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+#ifndef X_CORE_STRUTIL_HUMAN_DURATION_H_
+#define X_CORE_STRUTIL_HUMAN_DURATION_H_
+
+#include "StackString.h"
+
+X_NAMESPACE_BEGIN(core)
+
+/*
+Rounds up ms into a sensible representation.
+*/
+namespace HumanDuration
+{
+	typedef StackString<64> Str; 
+
+	const char* toString(Str& str, float ms);
+
+} // namespace HumanDuration
+
+X_NAMESPACE_END
+
+#endif // X_CORE_STRUTIL_HUMAN_DURATION_H_
