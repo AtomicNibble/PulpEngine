56a133ba6b80b03558a3b834f0a6879ba8bb769b
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-16 12:38:10 +0100

    tidy up of defines.

--- Pulp/Common/Hashing/Fnva1Hash.cpp
+++ Pulp/Common/Hashing/Fnva1Hash.cpp
@@ -9,21 +9,17 @@ namespace Hash
 	/*
 	 * 32 bit magic FNV-1a prime
 	 */
-	#define FNV_32_PRIME ((uint32_t)0x01000193)
-	#define FNV1_32_INIT ((uint32_t)0x811c9dc5)
-	#define FNV1_32A_INIT FNV1_32_INIT
-
-	#define FNV_64_PRIME   ((uint64_t)0x100000001B3)
-	#define FNV1_64_INIT  ((uint64_t)0x14650FB0739D0383) 
-	#define FNV1_64A_INIT FNV1_64_INIT
+	static const uint32_t FNV_32_PRIME = 0x01000193;
+	static const uint32_t FNV1_32_INIT = 0x811c9dc5;
+	static const uint32_t FNV1_32A_INIT = FNV1_32_INIT;
 
 	unsigned int Fnv1aHash(const void* key, size_t length)
 	{
-		unsigned int i;
-		unsigned int hash = FNV1_32_INIT;
-		unsigned char *s = (unsigned char *)key;
+		uint32_t i;
+		uint32_t hash = FNV1_32_INIT;
+		unsigned char *s = (unsigned char*)key;
 
-		for ( i = 0; i < length; ++i ) {
+		for (i = 0; i < length; ++i ) {
 			hash ^= (uint32_t)*s++;
 			hash *= FNV_32_PRIME;
 		}
@@ -34,11 +30,15 @@ namespace Hash
 
 	namespace Int64
 	{
+		static const uint64_t FNV_64_PRIME = 0x100000001B3;
+		static const uint64_t FNV1_64_INIT = 0x14650FB0739D0383;
+		static const uint64_t FNV1_64A_INIT = FNV1_64_INIT;
+
 		uint64_t Fnv1aHash(const void* key, size_t length)
 		{
 			size_t i;
 			uint64_t hash = FNV1_64_INIT;
-			unsigned char *s = (unsigned char *)key;
+			unsigned char* s = (unsigned char *)key;
 
 			for ( i = 0; i < length; ++i ) {
 				hash ^= (uint64_t)*s++;
