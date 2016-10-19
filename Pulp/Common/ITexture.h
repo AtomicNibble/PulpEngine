f360c89717b779ef7b48c450fd21d8738f7ea30c
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-19 22:18:40 +0100

    Updated default image names. Currently i'm including asset name which is not actually part of the name.
    But the loader currently don't handle that.

--- Pulp/Common/ITexture.h
+++ Pulp/Common/ITexture.h
@@ -19,8 +19,8 @@ struct XFile;
 X_NAMESPACE_BEGIN(texture)
 
 // defaults.
-static const char* TEX_DEFAULT_DIFFUSE = "Textures/default";
-static const char* TEX_DEFAULT_BUMP = "Textures/$identitynormal";
+static const char* TEX_DEFAULT_DIFFUSE = "imgs/default/default_col";
+static const char* TEX_DEFAULT_BUMP = "imgs/default/identitynormal";
 
 static const uint32_t	 TEX_MAX_DIMENSIONS = 4096;
 static const uint32_t	 TEX_MAX_MIPS = 13;
