acb00a47d93c30b566bcbc09be88e1c6e2238a91
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-23 00:09:07 +0100

    Add string prepro helper for widen.
    Also don't redfine logic of Stringize, just use the helper.

--- Pulp/Common/Prepro/PreproString.h
+++ Pulp/Common/Prepro/PreproString.h
@@ -1,9 +1,13 @@
 #pragma once
+
 #ifndef X_PREPRO_STRING_H
 #define X_PREPRO_STRING_H
 
+#include "PreproStringize.h"
+#include "PreproWiden.h"
 
-#define X_STRINGIZE_HELPER(token)					#token
-#define X_STRINGIZE(str)							X_STRINGIZE_HELPER(str)
+
+#define X_STRINGIZE(str)							X_PP_STRINGIZE_HELPER(str)
+#define X_WIDEN(str)								X_PP_WIDEN(str)
 
 #endif // X_PREPRO_STRING_H
---
+++ Pulp/Common/Prepro/PreproWiden.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef X_PREPROCESSORWIDEN_H
+#define X_PREPROCESSORWIDEN_H
+
+
+#define X_PP_WIDEN_HELPER(token)				L ## token
+
+#define X_PP_WIDEN(str)							X_PP_WIDEN_HELPER(str)
+
+#endif // X_PREPROCESSORWIDEN_H
