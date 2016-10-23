53c3e98b5e48499d8e6e57e0c7b7de30ca70acdc
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-23 16:12:30 +0100

    Add some str to material enums helpers.
    Some of these are simular to what's in 3DEngine, I suspect I end up removing the ones in 3DEngine and just linking this lib.
    Like I did for the ImgLib.

--- tools/MaterialLib/Util/MatUtil.cpp
+++ tools/MaterialLib/Util/MatUtil.cpp
@@ -1,11 +1,117 @@
 #include "stdafx.h"
 #include "MatUtil.h"
 
+#include <Hashing\Fnva1Hash.h>
+
 
 X_NAMESPACE_BEGIN(engine)
 
 namespace Util
 {
+
+	MaterialType::Enum MatTypeFromStr(const char* str)
+	{
+		using namespace core::Hash::Fnva1Literals;
+
+		switch (core::Hash::Fnv1aHash(str, core::strUtil::strlen(str)))
+		{
+			case "world"_fnv1a:
+				return MaterialType::WORLD;
+			case "ui"_fnv1a:
+				return MaterialType::UI;
+			case "model"_fnv1a:
+				return MaterialType::MODEL;
+			case "tool"_fnv1a:
+				return MaterialType::WORLD;
+
+			default:
+				X_ERROR("Mtl", "Unknown material type: '%s' (case-sen)", str);
+				return MaterialType::UNKNOWN;
+		}
+	}
+
+	MaterialUsage::Enum MatUsageFromStr(const char* str)
+	{
+		using namespace core::Hash::Fnva1Literals;
+
+		core::StackString<96, char> strUpper(str);
+		strUpper.toLower();
+
+		switch (core::Hash::Fnv1aHash(strUpper.c_str(), strUpper.length()))
+		{
+			case "none"_fnv1a:
+				return MaterialUsage::NONE;
+			case "door"_fnv1a:
+				return MaterialUsage::DOOR;
+			case "floor"_fnv1a:
+				return MaterialUsage::FLOOR;
+			case "celing"_fnv1a:
+				return MaterialUsage::CELING;
+			case "roof"_fnv1a:
+				return MaterialUsage::ROOF;
+			case "window"_fnv1a:
+				return MaterialUsage::WINDOW;
+			case "sky"_fnv1a:
+				return MaterialUsage::SKY;
+
+			default:
+				X_ERROR("Mtl", "Unknown material usage: '%s'", str);
+				return MaterialUsage::NONE;
+		}
+	}
+
+
+	MaterialSurType::Enum MatSurfaceTypeFromStr(const char* str)
+	{
+		using namespace core::Hash::Fnva1Literals;
+
+		core::StackString<96, char> strUpper(str);
+		strUpper.toLower();
+
+		switch (core::Hash::Fnv1aHash(strUpper.c_str(), strUpper.length()))
+		{
+			case "none"_fnv1a:
+				return MaterialSurType::NONE;
+			case "brick"_fnv1a:
+				return MaterialSurType::BRICK;
+			case "concrete"_fnv1a:
+				return MaterialSurType::CONCRETE;
+			case "cloth"_fnv1a:
+				return MaterialSurType::CLOTH;
+			case "flesh"_fnv1a:
+				return MaterialSurType::FLESH;
+			case "glass"_fnv1a:
+				return MaterialSurType::GLASS;
+			case "grass"_fnv1a:
+				return MaterialSurType::GRASS;
+			case "gravel"_fnv1a:
+				return MaterialSurType::GRAVEL;
+			case "ice"_fnv1a:
+				return MaterialSurType::ICE;
+			case "metal"_fnv1a:
+				return MaterialSurType::METAL;
+			case "mud"_fnv1a:
+				return MaterialSurType::MUD;
+			case "plastic"_fnv1a:
+				return MaterialSurType::PLASTIC;
+			case "paper"_fnv1a:
+				return MaterialSurType::PAPER;
+			case "rock"_fnv1a:
+				return MaterialSurType::ROCK;
+			case "snow"_fnv1a:
+				return MaterialSurType::SNOW;
+			case "sand"_fnv1a:
+				return MaterialSurType::SAND;
+			case "wood"_fnv1a:
+				return MaterialSurType::WOOD;
+			case "water"_fnv1a:
+				return MaterialSurType::WATER;
+
+			default:
+				X_ERROR("Mtl", "Unknown material surface type: '%s'", str);
+				return MaterialSurType::NONE;
+		}
+	}
 
 
 
--- tools/MaterialLib/Util/MatUtil.h
+++ tools/MaterialLib/Util/MatUtil.h
@@ -1,11 +1,16 @@
 #pragma once
 
 
+#include <IMaterial.h>
+
 X_NAMESPACE_BEGIN(engine)
 
 namespace Util
 {
 
+	MaterialType::Enum MatTypeFromStr(const char* str);
+	MaterialUsage::Enum MatUsageFromStr(const char* str);
+	MaterialSurType::Enum MatSurfaceTypeFromStr(const char* str);
 
 
 } // namespace Util
