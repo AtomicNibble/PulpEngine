0e1e701312ddcf7c660102dc19d846945ef5a0f4
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-23 14:16:39 +0100

    update mipmapdebug path.

--- Pulp/RenderSystems/RenderDx12/Texture/TextureManager.cpp
+++ Pulp/RenderSystems/RenderDx12/Texture/TextureManager.cpp
@@ -223,7 +223,7 @@ X_NAMESPACE_BEGIN(texture)
 			return false;
 		}
 
-		ptexMipMapDebug_ = forName("Textures/Debug/MipMapDebug", default_flags | TextureFlags::FILTER_BILINEAR);
+		ptexMipMapDebug_ = forName("imgs/debug/mipmaplevels", default_flags | TextureFlags::FILTER_BILINEAR);
 
 
 		return true;
