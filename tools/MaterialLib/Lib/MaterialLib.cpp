14b5cf7af59fbd157cb45877135c7b0a6e573180
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-23 18:59:10 +0100

    load the args into the material compiler.

--- tools/MaterialLib/Lib/MaterialLib.cpp
+++ tools/MaterialLib/Lib/MaterialLib.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "MaterialLib.h"
 
+#include "Compiler\Compiler.h"
 
 X_NAMESPACE_BEGIN(engine)
 
@@ -26,12 +27,15 @@ bool MaterialLib::Convert(IConverterHost& host, int32_t assetId, ConvertArgs& ar
 {
 	X_UNUSED(host);
 	X_ASSERT_NOT_NULL(gEnv);
-	// don't think i'll be using the job ystem for material conversion :D
-//	X_ASSERT_NOT_NULL(gEnv->pJobSys);
 
-	core::json::Document d;
-	d.Parse(args.c_str());
+	// we basically just take the json and compile it into a binary format.
 
+	MaterialCompiler compiler;
+
+	if (!compiler.loadFromJson(args)) {
+		X_ERROR("Mat", "Error parsing material args");
+		return false;
+	}
 
 
 
