bc367c0c3aa9fef99365f4eeb50336b58eb13d68
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-25 23:45:44 +0100

    Start to iron out compiler currently it's very dumb and just parses values and maps to binary representation

--- tools/MaterialLib/Compiler/Compiler.cpp
+++ tools/MaterialLib/Compiler/Compiler.cpp
@@ -1,9 +1,18 @@
 #include "stdafx.h"
 #include "Compiler.h"
 
+#include <IAssetDb.h>
+#include <IFileSys.h>
+
 #include "Util\MatUtil.h"
 
 X_NAMESPACE_BEGIN(engine)
+
+MaterialCompiler::Tex::Tex() :
+	filterType_(MaterialFilterType::LINEAR_MIP_LINEAR),
+	texRepeat_(MaterialTexRepeat::TILE_BOTH)
+{
+}
 
 
 bool MaterialCompiler::Tex::parse(core::json::Document& d, const char* pName)
@@ -39,6 +48,32 @@ bool MaterialCompiler::Tex::parse(core::json::Document& d, const char* pName)
 	return true;
 }
 
+
+bool MaterialCompiler::Tex::write(core::XFile* pFile) const
+{
+	MaterialTexture tex;
+	tex.filterType = filterType_;
+	tex.texRepeat = texRepeat_;
+	tex.nameLen = safe_static_cast<decltype(MaterialTexture::nameLen), size_t>(name.length());
+
+	if (pFile->writeObj(tex) != sizeof(tex)) {
+		return false;
+	}
+
+	return true;
+}
+
+bool MaterialCompiler::Tex::writeName(core::XFile* pFile) const
+{
+	if (name.isEmpty()) {
+		return true;
+	}
+
+	return pFile->writeString(name.c_str()) == name.length();
+}
+
+// --------------------------------------
+
 MaterialCompiler::MaterialCompiler()
 {
 
@@ -51,10 +86,12 @@ bool MaterialCompiler::loadFromJson(core::string& str)
 	d.Parse(str.c_str(), str.length());
 
 	// find all the things.
-	std::array<std::pair<const char*, core::json::Type>, 3> requiredValues = { {
+	std::array<std::pair<const char*, core::json::Type>, 5> requiredValues = { {
 			{ "cat", core::json::kStringType },
+			{ "usage", core::json::kStringType },
 			{ "surface_type", core::json::kStringType },
-			{ "usage", core::json::kStringType }
+			{ "polyOffset", core::json::kStringType },
+			{ "cullFace", core::json::kStringType }
 		}
 	};
 
@@ -72,20 +109,57 @@ bool MaterialCompiler::loadFromJson(core::string& str)
 		}
 	}
 
+	// shieezz
 	const char* pCat = d["cat"].GetString();
 	const char* pUsage = d["usage"].GetString();
 	const char* pSurfaceType = d["surface_type"].GetString();
 	const char* pPolyOffset = d["polyOffset"].GetString();
 	const char* pCullFace = d["cullFace"].GetString();
 
-	type_ = Util::MatTypeFromStr(pCat);
+	cat_ = Util::MatCatFromStr(pCat);
 	usage_ = Util::MatUsageFromStr(pUsage);
 	surType_ = Util::MatSurfaceTypeFromStr(pSurfaceType);
 	polyOffset_ = Util::MatPolyOffsetFromStr(pPolyOffset);
 	cullType_ = Util::MatCullTypeFromStr(pCullFace);
+	coverage_ = MaterialCoverage::OPAQUE;
 	
+	// blend ops.
+	const char* pSrcCol = d["srcBlendColor"].GetString();
+	const char* pDstCol = d["dstBlendColor"].GetString();
+	const char* pSrcAlpha = d["srcBlendAlpha"].GetString();
+	const char* pDstAlpha = d["dstBlendAlpha"].GetString();
+
+	srcBlendColor_ = Util::MatBlendTypeFromStr(pSrcCol);
+	dstBlendColor_ = Util::MatBlendTypeFromStr(pDstCol);
+	srcBlendAlpha_ = Util::MatBlendTypeFromStr(pSrcAlpha);
+	dstBlendAlpha_ = Util::MatBlendTypeFromStr(pDstAlpha);
+
+
+	// tilling shit.
+	// how many goats for a given N pickles
+	const auto tillingWidthTyep = d["tilingWidth"].GetType();
+	if (tillingWidthTyep != core::json::kNumberType) {
+		tiling_.x = -1;
+	}
+	else {
+		const int32_t tillingWidth = d["tilingWidth"].GetInt();
+		tiling_.x = safe_static_cast<int16_t>(tillingWidth);
+	}
+	
+	const auto tillingHeightType = d["tilingHeight"].GetType();
+	if (tillingHeightType != core::json::kNumberType) {
+		tiling_.y = -1;
+	}
+	else {
+		const int32_t tillingHeight = d["tilingHeight"].GetInt();
+		tiling_.y = safe_static_cast<int16_t>(tillingHeight);
+	}
+
+
+
 	// now we do some flag parsing.
 	flags_.Clear();
+	stateFlags_.Clear();
 
 	std::array<std::pair<const char*, MaterialFlag::Enum>, 15> flags = { {
 			{ "f_nodraw", MaterialFlag::NODRAW }, 
@@ -106,39 +180,22 @@ bool MaterialCompiler::loadFromJson(core::string& str)
 		}
 	};
 
-	// process all the flags.
-	for (size_t i = 0; i < flags.size(); i++)
-	{
-		const auto& flag = flags[i];
-
-		if (d.HasMember(flag.first))
-		{
-			const auto& val = d[flags[i].first];
-
-			switch (val.GetType())
-			{
-				case core::json::kFalseType:
-					// do nothing
-					break;
-				case core::json::kTrueType:
-					flags_.Set(flag.second);
-					break;
-				case core::json::kNumberType:
-					if (val.IsBool()) {
-						if (val.GetBool()) {
-							flags_.Set(flag.second);
-						}
-						break;
-					}
-					// fall through if not bool
-				default:
-					X_ERROR("Mat", "Flag \"%s\" has a value with a incorrect type: %" PRIi32, flag.first, val.GetType());
-					return false;
-			}
+	std::array<std::pair<const char*, MaterialStateFlag::Enum>, 2> stateFlags = { {
+			{ "depthWrite", MaterialStateFlag::DEPTHWRITE },
+			{ "wireFrame", MaterialStateFlag::WIREFRAME }
 		}
+	};
+
+	if (!processFlagGroup(d, flags_, flags)) {
+		X_ERROR("Mat", "Failed to parse flags");
+		return false;
+	}
+	if (!processFlagGroup(d, stateFlags_, stateFlags)) {
+		X_ERROR("Mat", "Failed to parse state flags");
+		return false;
 	}
 
-
+	
 	// col map.
 	if (!colMap_.parse(d, "Color")) {
 		X_ERROR("Mat", "Failed to parse texture info");
@@ -155,6 +212,105 @@ bool MaterialCompiler::loadFromJson(core::string& str)
 	if (!specColMap_.parse(d, "SpecCol")) {
 		X_ERROR("Mat", "Failed to parse texture info");
 		return false;
+	}
+
+	return true;
+}
+
+bool MaterialCompiler::writeToFile(core::XFile* pFile) const
+{
+	// lets check asset will fit.
+	// I don't do this in IMaterial just to save including IAssetDb.h in the header.
+	static_assert(assetDb::ASSET_NAME_MAX_LENGTH <= std::numeric_limits<decltype(MaterialTexture::nameLen)>::max(),
+		"Material only supports 255 max name len");
+
+	MaterialHeader hdr;
+	hdr.fourCC = MTL_B_FOURCC;
+	hdr.version = MTL_B_VERSION;
+
+	hdr.flags = flags_;
+	hdr.stateFlags = stateFlags_;
+	hdr.cat = cat_;
+	hdr.usage = usage_;
+	hdr.surfaceType = surType_;
+	hdr.polyOffsetType = polyOffset_;
+	hdr.cullType = cullType_;
+	hdr.coverage = coverage_;
+
+	hdr.depthTest = depthTest_;
+
+	hdr.srcBlendColor = srcBlendColor_;
+	hdr.dstBlendColor = dstBlendColor_;
+	hdr.srcBlendAlpha = srcBlendAlpha_;
+	hdr.dstBlendAlpha = dstBlendAlpha_;
+
+	// textures.
+	hdr.numTextures = 1;
+	
+	if (pFile->writeObj(hdr) != sizeof(hdr)) {
+		return false;
+	}
+
+	// i want to just write all the tex blocks regardless if they are set or not.
+	std::array<const Tex* const, 4> textures = {
+		&colMap_,
+		&normalMap_,
+		&detailNormalMap_,
+		&specColMap_
+	};
+
+	for (const auto& t : textures)
+	{
+		if (!t->write(pFile)) {
+			return false;
+		}
+	}
+
+	// now write the names.
+	for (const auto& t : textures)
+	{
+		if (!t->writeName(pFile)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+template<typename FlagClass, size_t Num>
+bool MaterialCompiler::processFlagGroup(core::json::Document& d, FlagClass& flags, 
+	const std::array<std::pair<const char*, typename FlagClass::Enum>, Num>& flagValues)
+{
+	// process all the flags.
+	for (size_t i = 0; i < flagValues.size(); i++)
+	{
+		const auto& flag = flagValues[i];
+
+		if (d.HasMember(flag.first))
+		{
+			const auto& val = d[flagValues[i].first];
+
+			switch (val.GetType())
+			{
+				case core::json::kFalseType:
+					// do nothing
+					break;
+				case core::json::kTrueType:
+					flags.Set(flag.second);
+					break;
+				case core::json::kNumberType:
+					if (val.IsBool()) {
+						if (val.GetBool()) {
+							flags.Set(flag.second);
+						}
+						break;
+					}
+					// fall through if not bool
+				default:
+					X_ERROR("Mat", "Flag \"%s\" has a value with a incorrect type: %" PRIi32, flag.first, val.GetType());
+					return false;
+			}
+		}
 	}
 
 	return true;
--- tools/MaterialLib/Compiler/Compiler.h
+++ tools/MaterialLib/Compiler/Compiler.h
@@ -2,14 +2,21 @@
 
 #include <IMaterial.h>
 
-X_NAMESPACE_BEGIN(engine)
+X_NAMESPACE_DECLARE(core,
+	struct XFile;
+);
 
+X_NAMESPACE_BEGIN(engine)
 
 class MaterialCompiler
 {
 	struct Tex
 	{
+		Tex();
+
 		bool parse(core::json::Document& doc, const char* pName);
+		bool write(core::XFile* pFile) const;
+		bool writeName(core::XFile* pFile) const;
 
 	public:
 		core::string name;
@@ -21,18 +28,34 @@ public:
 	MaterialCompiler();
 
 	bool loadFromJson(core::string& str);
+	bool writeToFile(core::XFile* pFile) const;
 
 private:
+	template<typename FlagClass, size_t Num>
+	bool processFlagGroup(core::json::Document& doc, FlagClass& flags,
+		const std::array<std::pair<const char*, typename FlagClass::Enum>, Num>& flagValues);
+
 
 private:
 	MaterialFlags flags_;
-	MaterialType::Enum type_;
+	MaterialStateFlags stateFlags_;
+	MaterialCat::Enum cat_;
 	MaterialUsage::Enum usage_;
 	MaterialSurType::Enum surType_;
-
 	MaterialPolygonOffset::Enum polyOffset_;
 	MaterialCullType::Enum cullType_;
+	MaterialCoverage::Enum coverage_;
 
+	Vec2<int16_t> tiling_;
+
+	// stencil
+	StencilFunc::Enum depthTest_;
+
+	// blend ops.
+	MaterialBlendType::Enum srcBlendColor_;
+	MaterialBlendType::Enum dstBlendColor_;
+	MaterialBlendType::Enum srcBlendAlpha_;
+	MaterialBlendType::Enum dstBlendAlpha_;
 
 	Tex colMap_;
 	Tex normalMap_;
