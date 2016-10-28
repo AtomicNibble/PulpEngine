cb0fe14f5a5675c38bc60e4fc43dede18ab36b7d
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-28 12:14:35 +0100

    Export the material helpers.

--- tools/MaterialLib/Util/MatUtil.h
+++ tools/MaterialLib/Util/MatUtil.h
@@ -8,21 +8,23 @@ X_NAMESPACE_BEGIN(engine)
 namespace Util
 {
 
-	MaterialMountType::Enum MatMountTypeFromStr(const char* str);
-	MaterialCat::Enum MatCatFromStr(const char* str);
-	MaterialUsage::Enum MatUsageFromStr(const char* str);
-	MaterialSurType::Enum MatSurfaceTypeFromStr(const char* str);
-	MaterialFilterType::Enum MatFilterTypeFromStr(const char* str);
-	MaterialTexRepeat::Enum MatTexRepeatFromStr(const char* str);
-	MaterialPolygonOffset::Enum MatPolyOffsetFromStr(const char* str);
-	MaterialCullType::Enum MatCullTypeFromStr(const char* str);
-	MaterialBlendType::Enum MatBlendTypeFromStr(const char* str);
+	MATLIB_EXPORT MaterialMountType::Enum MatMountTypeFromStr(const char* str);
+	MATLIB_EXPORT MaterialCat::Enum MatCatFromStr(const char* str);
+	MATLIB_EXPORT MaterialUsage::Enum MatUsageFromStr(const char* str);
+	MATLIB_EXPORT MaterialSurType::Enum MatSurfaceTypeFromStr(const char* str);
+	MATLIB_EXPORT MaterialFilterType::Enum MatFilterTypeFromStr(const char* str);
+	MATLIB_EXPORT MaterialTexRepeat::Enum MatTexRepeatFromStr(const char* str);
+	MATLIB_EXPORT MaterialPolygonOffset::Enum MatPolyOffsetFromStr(const char* str);
+	MATLIB_EXPORT MaterialCullType::Enum MatCullTypeFromStr(const char* str);
+	MATLIB_EXPORT MaterialBlendType::Enum MatBlendTypeFromStr(const char* str);
 
-	StencilOperation::Enum StencilOpFromStr(const char* str);
-	StencilFunc::Enum StencilFuncFromStr(const char* str);
+	MATLIB_EXPORT StencilOperation::Enum StencilOpFromStr(const char* str);
+	MATLIB_EXPORT StencilFunc::Enum StencilFuncFromStr(const char* str);
 
 	// engine::AUTO_TILING for auto.
-	int16_t TilingSizeFromStr(const char* str);
+	MATLIB_EXPORT int16_t TilingSizeFromStr(const char* str);
+
+
 
 
 } // namespace Util
