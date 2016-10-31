d1b759a6b019bae677fe29dadb1f4a4566d4faef
Tom Crowley <tom@tom-crowley.co.uk>
2016-10-31 00:17:22 +0000

    define sampler and texture states.

--- Pulp/Common/IRender.h
+++ Pulp/Common/IRender.h
@@ -180,6 +180,26 @@ X_DECLARE_ENUM8(TexRepeat)(
 	TILE_VERT
 );
 
+struct SamplerState
+{
+	FilterType::Enum filter;
+	TexRepeat::Enum repeat;
+};
+
+X_ENSURE_SIZE(SamplerState, 2);
+
+// do we want to explicitly group these?
+struct TextureState
+{
+	texture::ITexture* pTexture; // i will make this 16bit texture id making this whoel thing 4bytes.
+	SamplerState sampler;
+	// do i want to pass this?
+	// or try be fancy and pass arrays in TextureSlot index order.
+	TextureSlot::Enum slot; 
+	uint8_t _pad[5];
+};
+
+X_ENSURE_SIZE(TextureState, 16);
 
 X_DECLARE_FLAGS8(StateNewFlag)(
 	DEPTHWRITE,
