5b265135fddde1a12454df7dde51497bcf180c37
Tom Crowley <tom@tom-crowley.co.uk>
2016-11-02 20:19:00 +0000

    Only require a valid ILfmt if it's a vertex shader.

--- Pulp/RenderSystems/RenderDx12/Shader/ShaderBin.cpp
+++ Pulp/RenderSystems/RenderDx12/Shader/ShaderBin.cpp
@@ -84,7 +84,12 @@ bool ShaderBin::saveShader(const char* pPath, const XHWShader* pShader)
 	X_ASSERT_NOT_NULL(pPath);
 	X_ASSERT_NOT_NULL(pShader);
 
-	if (!pShader->isValid() || !pShader->isILFmtValid())
+	if (!pShader->isValid())
+	{
+		X_ERROR("Shader", "Failed to save compiled shader it's not valid.");
+		return false;
+	}
+	if (pShader->getType() == ShaderType::Vertex && !pShader->isILFmtValid())
 	{
 		X_ERROR("Shader", "Failed to save compiled shader it's not valid.");
 		return false;
