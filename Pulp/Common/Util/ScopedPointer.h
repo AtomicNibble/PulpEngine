c89b94ca25127933b0c5ddadd225791f121f8c6b
Tom Crowley <tom@tom-crowley.co.uk>
2016-11-06 23:20:00 +0000

    Updates to scoped pointer.

--- Pulp/Common/Util/ScopedPointer.h
+++ Pulp/Common/Util/ScopedPointer.h
@@ -14,7 +14,16 @@ class ScopedPointer
 {
 	ScopedPointer();
 public:
-	ScopedPointer(T* pInstance, core::MemoryArenaBase* arena) :
+	typedef typename T* pointer;
+
+	explicit ScopedPointer(core::MemoryArenaBase* arena, nullptr_t) :
+		pInstance_(pointer()),
+		arena_(arena)
+	{
+		X_ASSERT_NOT_NULL(arena);
+	}
+
+	ScopedPointer(core::MemoryArenaBase* arena, T* pInstance) :
 		pInstance_(pInstance),
 		arena_(arena)
 	{
@@ -22,26 +31,83 @@ public:
 		X_ASSERT_NOT_NULL(arena);
 	}
 
-	~ScopedPointer() {
-		X_DELETE(pInstance_, arena_);
+
+	ScopedPointer(ScopedPointer&& oth) :
+		pInstance_(oth.release()),
+		arena_(oth.arena_)
+	{
 	}
 
-	X_INLINE T& operator* ()
+	~ScopedPointer() {	
+		if (pInstance_ != pointer()) {
+			deleter(pInstance_);
+		}
+	}
+
+	ScopedPointer& operator=(nullptr_t)
+	{	
+		// assign a null pointer
+		reset();
+		return *this;
+	}
+
+	ScopedPointer& operator=(ScopedPointer&& rhs) 
+	{	
+		// assign by moving _Right
+		if (this != &rhs)
+		{	
+			// different, do the move
+			reset(rhs.release());
+			arena_ = oth.arena_;
+		}
+		return *this;
+	}
+
+
+	X_INLINE T& operator* () const
 	{
 		return *pInstance_;
 	}
 
-	X_INLINE T* operator-> ()
+	X_INLINE pointer operator-> () const
 	{
 		return pInstance_;
 	}
 
-	X_INLINE T* get(void) {
+	X_INLINE pointer get(void) const {
 		return pInstance_;
+	}
+
+	X_INLINE core::MemoryArenaBase* getArena(void) const {
+		return arena_;
+	}
+
+	pointer release(void)
+	{	
+		// yield ownership of pointer
+		pointer pAns = get();
+		pInstance_ = pointer();
+		return pAns;
+	}
+
+	X_INLINE void reset(pointer ptr = pointer())
+	{	
+		// establish new pointer
+		pointer pOld = get();
+		pInstance_ = ptr;
+		if (pOld != pointer()) {
+			deleter(pOld);
+		}
 	}
 
 	X_NO_ASSIGN(ScopedPointer);
 	X_NO_COPY(ScopedPointer);
+
+private:
+	void deleter(pointer ptr) {
+		X_DELETE(ptr, arena_);
+	}
+
 private:
 	T* pInstance_;
 	core::MemoryArenaBase* arena_;
@@ -53,7 +119,17 @@ class ScopedPointer<T[]>
 {
 	ScopedPointer();
 public:
-	ScopedPointer(T* pInstance, core::MemoryArenaBase* arena) :
+	typedef typename T* pointer;
+
+	explicit ScopedPointer(core::MemoryArenaBase* arena, nullptr_t) :
+		pInstance_(pointer()),
+		arena_(arena)
+	{
+		X_ASSERT_NOT_NULL(arena);
+	}
+
+
+	ScopedPointer(core::MemoryArenaBase* arena, T* pInstance) :
 		pInstance_(pInstance),
 		arena_(arena)
 	{
@@ -61,26 +137,84 @@ public:
 		X_ASSERT_NOT_NULL(arena);
 	}
 
-	~ScopedPointer() {
-		X_DELETE_ARRAY(pInstance_, arena_);
+	ScopedPointer(ScopedPointer&& oth) :
+		pInstance_(oth.release()),
+		arena_(oth.arena_)
+	{
 	}
 
-	X_INLINE T& operator* ()
+	~ScopedPointer() {
+		if (pInstance_) {
+			deleter(pInstance_);
+		}
+ 	}
+
+	ScopedPointer& operator=(nullptr_t)
+	{
+		// assign a null pointer
+		reset();
+		return *this;
+	}
+
+	ScopedPointer& operator=(ScopedPointer&& rhs)
+	{
+		// assign by moving _Right
+		if (this != &rhs)
+		{
+			// different, do the move
+			reset(rhs.release());
+			arena_ = oth.arena_;
+		}
+		return *this;
+	}
+
+	X_INLINE T& operator* () const
 	{
 		return *pInstance_;
 	}
 
-	X_INLINE T* operator-> ()
+	X_INLINE T* operator-> () const
 	{
 		return pInstance_;
 	}
 
-	X_INLINE T* get(void) {
+	X_INLINE T* get(void) const
+	{
 		return pInstance_;
 	}
 
+	X_INLINE core::MemoryArenaBase* getArena(void) const {
+		return arena_;
+	}
+
+	pointer release(void)
+	{
+		// yield ownership of pointer
+		pointer pAns = get();
+		pInstance_ = pointer();
+		return pAns;
+	}
+
+	X_INLINE void reset(pointer ptr = pointer())
+	{
+		// establish new pointer
+		pointer pOld = get();
+		pInstance_ = ptr;
+		if (pOld != pointer()) {
+			deleter(pOld);
+		}
+	}
+
+
 	X_NO_ASSIGN(ScopedPointer);
 	X_NO_COPY(ScopedPointer);
+
+private:
+	void deleter(pointer ptr) {
+		X_DELETE_ARRAY(ptr, arena_);
+	}
+
+
 private:
 	T* pInstance_;
 	core::MemoryArenaBase* arena_;
