34f18f4a3c4cc3c1a4d2bacb518b2e1a06568011
Tom Crowley <tom@tom-crowley.co.uk>
2016-11-21 00:10:49 +0000

    Add a prim shader, potentially only temp while I finish refectoring the primative system.

---
+++ game_folder/core_assets/shaders/Prim.hlsl
@@ -0,0 +1,49 @@
+
+
+cbuffer ObjectConstants  : register(b0)
+{
+	float4x4 worldToScreenMatrix;
+	float4x4 worldViewProjectionMatrix;
+};
+
+
+struct VS_INPUT
+{
+    float3 osPosition           	: POSITION;
+    float2 tex 			: TEXCOORD0; // not used, but added so vertex layout can be shared.
+    float4 color                	: COLOR0;
+};
+
+struct VS_OUTPUT
+{
+    float4 ssPosition           	: SV_POSITION;
+    float4 color                	: COLOR;
+};
+
+struct PS_INPUT
+{
+    float4 ssPosition           	: SV_POSITION;
+    float4 color                	: COLOR0;
+};
+
+struct PS_OUTPUT
+{
+    float4 color                : SV_TARGET0;
+};
+
+
+
+VS_OUTPUT PrimVS( VS_INPUT IN )
+{
+  VS_OUTPUT OUT;
+  OUT.ssPosition = mul( worldToScreenMatrix, float4(IN.osPosition, 1.0) );
+  OUT.color =  IN.color;
+  return OUT;
+}
+
+PS_OUTPUT PrimPS( PS_INPUT IN )
+{
+	PS_OUTPUT output;
+	output.color = IN.color;
+	return output;
+}
---
+++ game_folder/core_assets/shaders/Prim.shader
@@ -0,0 +1,12 @@
+shader
+{
+	source "Prim.hlsl"
+	techniques
+	{
+		{
+			name 			"Prim"
+			vertex_shader   	"PrimVS"
+			pixel_shader    	"PrimPS"
+		}
+	}
+}
