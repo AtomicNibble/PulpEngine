6dbb0ef42396005fece59872ab23628c71b50bed
Tom Crowley <tom@tom-crowley.co.uk>
2016-12-28 15:59:13 +0000

    Additional math helpers.

--- Pulp/Physics/MathHelpers.h
+++ Pulp/Physics/MathHelpers.h
@@ -19,5 +19,25 @@ X_INLINE physx::PxVec3 Px3FromVec3(const Vec3f& vec)
 	return physx::PxVec3(vec.x, vec.y, vec.z);
 }
 
+X_INLINE physx::PxQuat PxQuatFromQuat(const Quatf& quat)
+{
+	return physx::PxQuat(quat.v.x, quat.v.y, quat.v.z, quat.w);
+}
+
+X_INLINE physx::PxTransform PxTransFromQuatTrans(const QuatTransf& myTrans)
+{
+	physx::PxTransform trans;
+	trans.p = Px3FromVec3(myTrans.trans);
+	trans.q = PxQuatFromQuat(myTrans.quat);
+	return trans;
+}
+
+X_INLINE physx::PxBounds3 PxBounds3FromAABB(const AABB& aabb)
+{
+	physx::PxBounds3 bounds;
+	bounds.minimum = Px3FromVec3(aabb.min);
+	bounds.maximum = Px3FromVec3(aabb.max);
+	return bounds;
+}
 
 X_NAMESPACE_END
