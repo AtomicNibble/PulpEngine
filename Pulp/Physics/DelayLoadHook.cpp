77793566821fb66b08f5b626564669d3f3948215
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-01 19:06:24 +0000

    Add a delay load hook for the engine physics module, so we can at runtime change which version of physx we load.
    This means that I can have a setting that forces either: release, profile, checked, debug to be loaded no mater the config the engine was built as.
    This allows things like quickly switching to checked version of physics in a release build to debug error.
    Loading the release version in debug to increase speed.

--- Pulp/Physics/DelayLoadHook.cpp
+++ Pulp/Physics/DelayLoadHook.cpp
@@ -1,29 +1,182 @@
 #include "stdafx.h"
 #include "DelayLoadHook.h"
 
+
+#include <Platform\Module.h>
+
+#if X_PLATFORM_WIN32
+#include <delayimp.h>
+
+#pragma comment(lib, "delayimp")
+
+#endif //!X_PLATFORM_WIN32
+
 X_NAMESPACE_BEGIN(physics)
 
-SampleDelayLoadHook gDelayLoadHook;
+DelayLoadHook gDelayLoadHook;
 
-const char* SampleDelayLoadHook::getPhysXCommonDEBUGDllName(void) const
+DelayLoadHook::DelayLoadHook() :
+	config_(Config::Normal)
 {
-	return "PhysXCommonDEBUG.dll";
+
 }
 
-const char* SampleDelayLoadHook::getPhysXCommonCHECKEDDllName(void) const
+void DelayLoadHook::forceConfig(Config config)
 {
-	return "PhysXCommonCHECKED.dll";
+	config_ = config;
 }
 
-const char* SampleDelayLoadHook::getPhysXCommonPROFILEDllName(void) const
+const char* DelayLoadHook::getPhysXCommonDEBUGDllName(void) const
 {
-	return "PhysXCommonPROFILE.dll";
+	return getCommonDllName(Config::Debug);
 }
 
-const char* SampleDelayLoadHook::getPhysXCommonDllName(void) const
+const char* DelayLoadHook::getPhysXCommonCHECKEDDllName(void) const
 {
-	return "PhysXCommon.dll";
+	return getCommonDllName(Config::Checked);
 }
 
+const char* DelayLoadHook::getPhysXCommonPROFILEDllName(void) const
+{
+	return getCommonDllName(Config::Profile);
+}
+
+const char* DelayLoadHook::getPhysXCommonDllName(void) const
+{
+	return getCommonDllName(Config::Release);
+}
+
+const char* DelayLoadHook::getPhysxName(const char* pRequestedName)
+{
+	// we need to work out which dll it is.
+	// before we can override it.
+	// we will have thinks like:
+	// * Physx
+	// * PhysXCommon
+	// * PhysXCooking
+	// * PhysXCharacterKinematic
+
+	if (config_ == Config::Normal) {
+		return pRequestedName;
+	}
+
+	const char* pNameEnd = pRequestedName + core::strUtil::strlen(pRequestedName);
+
+	// the order of these is important.
+	// anything that is a substring of anotehr should come later.
+	if (core::strUtil::Find(pRequestedName, pNameEnd, "PhysXCharacterKinematic") == pRequestedName)
+	{
+		return createNameForConfig("PhysXCharacterKinematic", dllNames_[NameBuffer::PhysxCharacterKinematic]);
+	}
+	else if (core::strUtil::Find(pRequestedName, pNameEnd, "PhysXCooking") == pRequestedName)
+	{
+		return createNameForConfig("PhysXCooking", dllNames_[NameBuffer::PhysxCooking]);
+	}
+	else if (core::strUtil::Find(pRequestedName, pNameEnd, "PhysXCommon") == pRequestedName)
+	{
+		return createNameForConfig("PhysXCommon", dllNames_[NameBuffer::PhysxCommon]);
+	}
+	else if (core::strUtil::Find(pRequestedName, pNameEnd, "PhysX") == pRequestedName)
+	{
+		return createNameForConfig("PhysX", dllNames_[NameBuffer::Physx]);
+	}
+
+	// this should not happen tbh, so tell me about it when it does.
+	X_ASSERT_UNREACHABLE();
+	return pRequestedName;
+}
+
+
+const char* DelayLoadHook::createNameForConfig(const char* pPrefix, core::Path<char>& buffer)
+{
+	X_ASSERT(config_ != Config::Normal, "Create config name should only be called when overiding")();
+
+	buffer.clear();
+	buffer.append(pPrefix);
+
+	switch (config_)
+	{
+		case Config::Debug:
+			buffer.append("DEBUG");
+			break;
+		case Config::Checked:
+			buffer.append("CHECKED");
+			break;
+		case Config::Profile:
+			buffer.append("PROFILE");
+			break;
+		case Config::Release:
+			break;
+	}
+
+	buffer.setExtension("dll");
+	return buffer.c_str();
+}
+
+
+const char* DelayLoadHook::getCommonDllName(Config config) const
+{
+	switch (config)
+	{
+		case Config::Debug:
+			return "PhysXCommonDEBUG.dll";
+		case Config::Checked:
+			return "PhysXCommonCHECKED.dll";
+		case Config::Profile:
+			return "PhysXCommonPROFILE.dll";
+		case Config::Release:
+		default:
+			return "PhysXCommon.dll";
+	}
+}
+
+#if X_PLATFORM_WIN32
+
+FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli)
+{
+	switch (dliNotify) 
+	{
+		case dliStartProcessing:
+			break;
+
+		case dliNotePreLoadLibrary:
+		{
+			const char* pName = gDelayLoadHook.getPhysxName(pdli->szDll);
+			return static_cast<FARPROC>(core::Module::Load(pName));
+		}
+		break;
+
+		case dliNotePreGetProcAddress:
+			break;
+
+		case dliFailLoadLib:
+			break;
+
+		case dliFailGetProc:
+			break;
+
+		case dliNoteEndProcessing:
+			break;
+
+		default:
+			return nullptr;
+	}
+
+	return nullptr;
+}
+
+// extern c it so it don't have namespace name.
+
+extern "C"
+
+#if _MSC_FULL_VER >= 190024210 && !defined(DELAYIMP_INSECURE_WRITABLE_HOOKS)
+const 
+#endif // !_MSC_FULL_VER
+
+PfnDliHook __pfnDliNotifyHook2 = delayHook;
+
+
+
+#endif //!X_PLATFORM_WIN32
 
 X_NAMESPACE_END
--- Pulp/Physics/DelayLoadHook.h
+++ Pulp/Physics/DelayLoadHook.h
@@ -4,16 +4,48 @@
 
 X_NAMESPACE_BEGIN(physics)
 
-class SampleDelayLoadHook : public physx::PxDelayLoadHook
+class DelayLoadHook : public physx::PxDelayLoadHook
 {
 public:
+	enum class Config
+	{
+		Normal, // load same config this dll was built as.
+		Debug,
+		Checked,
+		Profile,
+		Release
+	};
+
+	X_DECLARE_ENUM(NameBuffer)(
+		Physx,
+		PhysxCommon,
+		PhysxCooking,
+		PhysxCharacterKinematic
+	);
+
+public:
+	DelayLoadHook();
+
+	void forceConfig(Config config);
 
 	virtual const char* getPhysXCommonDEBUGDllName(void) const X_FINAL;
 	virtual const char* getPhysXCommonCHECKEDDllName(void) const X_FINAL;
 	virtual const char* getPhysXCommonPROFILEDllName(void) const X_FINAL;
 	virtual const char* getPhysXCommonDllName(void) const X_FINAL;
+
+	const char* getPhysxName(const char* pRequestedName);
+	
+private:
+	const char* createNameForConfig(const char* pPrefix, core::Path<char>& buffer);
+	const char* getCommonDllName(Config config) const;
+
+private:
+	Config config_;
+
+	// these are just so we are thread safe for each delay load dll.
+	core::Path<char> dllNames_[NameBuffer::ENUM_COUNT];
 };
 
-extern SampleDelayLoadHook gDelayLoadHook;
+extern DelayLoadHook gDelayLoadHook;
 
 X_NAMESPACE_END
