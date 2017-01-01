15ce169e027fede91fdce686e515f1f971284ae7
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-01 14:23:23 +0000

    Update the physx debugRender to split the prims up if they don't all fit in single page.

--- Pulp/Physics/DebugRender.cpp
+++ Pulp/Physics/DebugRender.cpp
@@ -25,71 +25,99 @@ void DebugRender::update(const physx::PxRenderBuffer& debugRenderable)
 	// it's just if one of them is bigger than a single page we will crash due to access violation.
 	// the ideall way to handle this is for the primContex to tell use how many verts we got back that way we can spread 
 	// the prims across multiple pages if required.
+	// or what if we just know the answer and break it up ourself, since this is the only place that requires this logic.
+	// so might as well just write it here.
+
+	const uint32_t maxVertsPerBatch = safe_static_cast<uint32_t>(pPrimCon_->maxVertsPerPrim());
 
 	// Points
-	const uint32_t numPoints = debugRenderable.getNbPoints();
+	uint32_t numPoints = debugRenderable.getNbPoints();
 	if (numPoints)
 	{
 		const physx::PxDebugPoint* X_RESTRICT points = debugRenderable.getPoints();
 
-		engine::IPrimativeContext::PrimVertex* X_RESTRICT pPoints = pPrimCon_->addPrimative(numPoints,
-			engine::IPrimativeContext::PrimitiveType::POINTLIST);
-
-		for (uint32_t i = 0; i<numPoints; i++)
+		while (numPoints)
 		{
-			const physx::PxDebugPoint& point = points[i];
+			const uint32_t pointBatchSize = core::Min(maxVertsPerBatch, numPoints);
 
-			pPoints[i].pos = Vec3FromPhysx(point.pos);
-			pPoints[i].color = Color8u::hexA(point.color);
+			engine::IPrimativeContext::PrimVertex* X_RESTRICT pPoints = pPrimCon_->addPrimative(pointBatchSize,
+				engine::IPrimativeContext::PrimitiveType::POINTLIST);
+
+			for (uint32_t i = 0; i < pointBatchSize; i++)
+			{
+				const physx::PxDebugPoint& point = points[i];
+
+				pPoints[i].pos = Vec3FromPhysx(point.pos);
+				pPoints[i].color = Color8u::hexA(point.color);
+			}
+
+			points += pointBatchSize;
+			numPoints -= pointBatchSize;
 		}
 	}
 
 	// Lines
-	const uint32_t numLines = debugRenderable.getNbLines();
+	uint32_t numLines = debugRenderable.getNbLines();
 	if (numLines)
 	{
 		const physx::PxDebugLine* X_RESTRICT lines = debugRenderable.getLines();
 
 		// lets do the batching ourself, since we need to cast so we can't use the range submit.
-		engine::IPrimativeContext::PrimVertex* X_RESTRICT pLines = pPrimCon_->addPrimative(2 * numLines,
-			engine::IPrimativeContext::PrimitiveType::LINELIST);
-
-		for (uint32_t i = 0; i<numLines; i++)
+		while (numLines)
 		{
-			const physx::PxDebugLine& line = lines[i];
+			const uint32_t lineBatchSize = core::Min(maxVertsPerBatch, numLines * 2);
 
-			pLines[0].pos = Vec3FromPhysx(line.pos0);
-			pLines[0].color = Color8u::hexA(line.color0);
-			pLines[1].pos = Vec3FromPhysx(line.pos1);
-			pLines[1].color = Color8u::hexA(line.color1);
-			pLines += 2;
+			engine::IPrimativeContext::PrimVertex* X_RESTRICT pLines = pPrimCon_->addPrimative(lineBatchSize,
+				engine::IPrimativeContext::PrimitiveType::LINELIST);
+
+			for (uint32_t i = 0; i < lineBatchSize; i++)
+			{
+				const physx::PxDebugLine& line = lines[i];
+
+				pLines[0].pos = Vec3FromPhysx(line.pos0);
+				pLines[0].color = Color8u::hexA(line.color0);
+				pLines[1].pos = Vec3FromPhysx(line.pos1);
+				pLines[1].color = Color8u::hexA(line.color1);
+				pLines += 2;
+			}
+
+			lines += (lineBatchSize >> 1);
+			numLines -= (lineBatchSize >> 1);
 		}
 	}
 
 	// Triangles
-	const uint32_t numTriangles = debugRenderable.getNbTriangles();
+	uint32_t numTriangles = debugRenderable.getNbTriangles();
 	if (numTriangles)
 	{
 		const physx::PxDebugTriangle* X_RESTRICT triangles = debugRenderable.getTriangles();
 
-		engine::IPrimativeContext::PrimVertex* X_RESTRICT pVerts = pPrimCon_->addPrimative(3 * numTriangles,
-			engine::IPrimativeContext::PrimitiveType::TRIANGLELIST);
-
-		for (uint32_t i = 0; i<numTriangles; i++)
+		while (numTriangles)
 		{
-			const physx::PxDebugTriangle& triangle = triangles[i];
-			auto* pTri = pVerts;
+			const uint32_t triBatchSize = core::Min(maxVertsPerBatch, numTriangles * 3);
 
-			pTri[0].pos = Vec3FromPhysx(triangle.pos0);
-			pTri[0].color = Color8u::hexA(triangle.color0);
+			engine::IPrimativeContext::PrimVertex* X_RESTRICT pVerts = pPrimCon_->addPrimative(triBatchSize,
+				engine::IPrimativeContext::PrimitiveType::TRIANGLELIST);
 
-			pTri[1].pos = Vec3FromPhysx(triangle.pos1);
-			pTri[1].color = Color8u::hexA(triangle.color1);
+			for (uint32_t i = 0; i < triBatchSize; i++)
+			{
+				const physx::PxDebugTriangle& triangle = triangles[i];
+				auto* pTri = pVerts;
 
-			pTri[2].pos = Vec3FromPhysx(triangle.pos2);
-			pTri[2].color = Color8u::hexA(triangle.color2);
+				pTri[0].pos = Vec3FromPhysx(triangle.pos0);
+				pTri[0].color = Color8u::hexA(triangle.color0);
 
-			pVerts += 3;
+				pTri[1].pos = Vec3FromPhysx(triangle.pos1);
+				pTri[1].color = Color8u::hexA(triangle.color1);
+
+				pTri[2].pos = Vec3FromPhysx(triangle.pos2);
+				pTri[2].color = Color8u::hexA(triangle.color2);
+
+				pVerts += 3;
+			}
+
+			triangles += (triBatchSize / 3);
+			numTriangles -= (triBatchSize / 3);
 		}
 	}
 }
