23a27bb5c0ed7e645842a1b4472ba64973d5b549
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-04 12:02:14 +0000

    Update all the mathHelpers to just cast instead of constructing new objects in stack.
    Sincer we now check at compile time this is safe.

--- Pulp/Physics/MathHelpers.h
+++ Pulp/Physics/MathHelpers.h
@@ -6,64 +6,98 @@ X_NAMESPACE_BEGIN(physics)
 
 // from physx
 
-X_INLINE Vec3f Vec3FromPhysx(const physx::PxVec3& vec)
+
+X_INLINE const Vec3f& Vec3FromPx3(const physx::PxVec3& vec)
 {
-	return Vec3f(vec.x, vec.y, vec.z);
+	return reinterpret_cast<const Vec3f&>(vec);
 }
-
-X_INLINE Vec3f Vec3FromPx3(const physx::PxVec3& vec)
+X_INLINE Vec3f& Vec3FromPx3(physx::PxVec3& vec)
 {
-	return Vec3f(vec.x, vec.y, vec.z);
-}
-
-
-X_INLINE Vec3d Vec3FromPx3Ext(const physx::PxExtendedVec3& vec)
-{
-	return Vec3d(vec.x, vec.y, vec.z);
+	return reinterpret_cast<Vec3f&>(vec);
 }
 
 
-X_INLINE Quatf QuatFromPxQuat(const physx::PxQuat& quat)
+X_INLINE const Vec3d& Vec3FromPx3Ext(const physx::PxExtendedVec3& vec)
 {
-	return Quatf(quat.x, quat.y, quat.z, quat.w);
+	return reinterpret_cast<const Vec3d&>(vec);
+}
+X_INLINE Vec3d& Vec3FromPx3Ext(physx::PxExtendedVec3& vec)
+{
+	return reinterpret_cast<Vec3d&>(vec);
 }
 
-X_INLINE QuatTransf QuatTransFromPxTrans(const physx::PxTransform& trans)
+
+X_INLINE const Quatf& QuatFromPxQuat(const physx::PxQuat& quat)
 {
-	return QuatTransf(Vec3FromPx3(trans.p), QuatFromPxQuat(trans.q));
+	return reinterpret_cast<const Quatf&>(quat);
 }
+X_INLINE Quatf& QuatFromPxQuat(physx::PxQuat& quat)
+{
+	return reinterpret_cast<Quatf&>(quat);
+}
+
+
+X_INLINE const QuatTransf& QuatTransFromPxTrans(const physx::PxTransform& trans)
+{
+	return reinterpret_cast<const QuatTransf&>(trans);
+
+}
+X_INLINE QuatTransf& QuatTransFromPxTrans(physx::PxTransform& trans)
+{
+	return reinterpret_cast<QuatTransf&>(trans);
+}
+
+
 
 // to physx
-X_INLINE physx::PxVec3 Px3FromVec3(const Vec3f& vec)
+X_INLINE const physx::PxVec3& Px3FromVec3(const Vec3f& vec)
 {
-	return physx::PxVec3(vec.x, vec.y, vec.z);
+	return reinterpret_cast<const physx::PxVec3&>(vec);
+}
+X_INLINE physx::PxVec3& Px3FromVec3(Vec3f& vec)
+{
+	return reinterpret_cast<physx::PxVec3&>(vec);
 }
 
-X_INLINE physx::PxExtendedVec3 Px3ExtFromVec3(const Vec3d& vec)
+
+X_INLINE const physx::PxExtendedVec3& Px3ExtFromVec3(const Vec3d& vec)
 {
-	return physx::PxExtendedVec3(vec.x, vec.y, vec.z);
+	return reinterpret_cast<const physx::PxExtendedVec3&>(vec);
+}
+X_INLINE physx::PxExtendedVec3& Px3ExtFromVec3(Vec3d& vec)
+{
+	return reinterpret_cast<physx::PxExtendedVec3&>(vec);
 }
 
 
-X_INLINE physx::PxQuat PxQuatFromQuat(const Quatf& quat)
+X_INLINE const physx::PxQuat& PxQuatFromQuat(const Quatf& quat)
 {
-	return physx::PxQuat(quat.v.x, quat.v.y, quat.v.z, quat.w);
+	return reinterpret_cast<const physx::PxQuat&>(quat);
+}
+X_INLINE physx::PxQuat& PxQuatFromQuat(Quatf& quat)
+{
+	return reinterpret_cast<physx::PxQuat&>(quat);
 }
 
-X_INLINE physx::PxTransform PxTransFromQuatTrans(const QuatTransf& myTrans)
+
+X_INLINE const physx::PxTransform& PxTransFromQuatTrans(const QuatTransf& myTrans)
 {
-	physx::PxTransform trans;
-	trans.p = Px3FromVec3(myTrans.trans);
-	trans.q = PxQuatFromQuat(myTrans.quat);
-	return trans;
+	return reinterpret_cast<const physx::PxTransform&>(myTrans);
+}
+X_INLINE physx::PxTransform& PxTransFromQuatTrans(QuatTransf& myTrans)
+{
+	return reinterpret_cast<physx::PxTransform&>(myTrans);
 }
 
-X_INLINE physx::PxBounds3 PxBounds3FromAABB(const AABB& aabb)
+
+X_INLINE const physx::PxBounds3& PxBounds3FromAABB(const AABB& aabb)
 {
-	physx::PxBounds3 bounds;
-	bounds.minimum = Px3FromVec3(aabb.min);
-	bounds.maximum = Px3FromVec3(aabb.max);
-	return bounds;
+	return reinterpret_cast<const physx::PxBounds3&>(aabb);
 }
+X_INLINE physx::PxBounds3& PxBounds3FromAABB(AABB& aabb)
+{
+	return reinterpret_cast<physx::PxBounds3&>(aabb);
+}
+
 
 X_NAMESPACE_END
