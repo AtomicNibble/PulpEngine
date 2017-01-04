2974498caae314e37e4d72eaf1fa57323e6228f4
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-04 12:01:17 +0000

    Add static checks to ensure my engine types are castable to physx types.

---
+++ Pulp/Physics/MathHelpers.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "MathHelpers.h"
+
+X_NAMESPACE_BEGIN(physics)
+
+namespace
+{
+
+	X_ENSURE_SIZE(Vec3f, sizeof(physx::PxVec3));
+	X_ENSURE_SIZE(Vec3d, sizeof(physx::PxExtendedVec3));
+	X_ENSURE_SIZE(Quatf, sizeof(physx::PxQuat));
+	X_ENSURE_SIZE(AABB, sizeof(physx::PxBounds3));
+	X_ENSURE_SIZE(QuatTransf, sizeof(physx::PxTransform));
+
+
+	static_assert(X_OFFSETOF(Vec3f, x) == X_OFFSETOF(physx::PxVec3, x), "Offset don't match");
+	static_assert(X_OFFSETOF(Vec3f, y) == X_OFFSETOF(physx::PxVec3, y), "Offset don't match");
+	static_assert(X_OFFSETOF(Vec3f, z) == X_OFFSETOF(physx::PxVec3, z), "Offset don't match");
+
+	static_assert(X_OFFSETOF(Vec3d, x) == X_OFFSETOF(physx::PxExtendedVec3, x), "Offset don't match");
+	static_assert(X_OFFSETOF(Vec3d, y) == X_OFFSETOF(physx::PxExtendedVec3, y), "Offset don't match");
+	static_assert(X_OFFSETOF(Vec3d, z) == X_OFFSETOF(physx::PxExtendedVec3, z), "Offset don't match");
+
+	static_assert(X_OFFSETOF(Quatf, v.x) == X_OFFSETOF(physx::PxQuat, x), "Offset don't match");
+	static_assert(X_OFFSETOF(Quatf, v.y) == X_OFFSETOF(physx::PxQuat, y), "Offset don't match");
+	static_assert(X_OFFSETOF(Quatf, v.z) == X_OFFSETOF(physx::PxQuat, z), "Offset don't match");
+	static_assert(X_OFFSETOF(Quatf, w) == X_OFFSETOF(physx::PxQuat, w), "Offset don't match");
+
+	static_assert(X_OFFSETOF(AABB, min) == X_OFFSETOF(physx::PxBounds3, minimum), "Offset don't match");
+	static_assert(X_OFFSETOF(AABB, max) == X_OFFSETOF(physx::PxBounds3, maximum), "Offset don't match");
+
+	static_assert(X_OFFSETOF(QuatTransf, quat) == X_OFFSETOF(physx::PxTransform, q), "Offset don't match");
+	static_assert(X_OFFSETOF(QuatTransf, trans) == X_OFFSETOF(physx::PxTransform, p), "Offset don't match");
+
+
+} // namespace
+
+X_NAMESPACE_END
