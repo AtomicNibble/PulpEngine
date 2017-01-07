3e2c5a62370192f3abdcd8d8715318b9e95bbed3
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-07 15:13:02 +0000

    Playing with the palette, comment out currently but i don't want to throw it away.

--- tools/AssetManager/main.cpp
+++ tools/AssetManager/main.cpp
@@ -66,6 +66,27 @@ int main(int argc, char *argv[])
 	a.setWindowIcon(QIcon(":/misc/img/icon.png"));
 	a.setStyle(QStyleFactory::create("Fusion"));
 
+#if 0
+	QPalette palette = qApp->palette();
+	palette.setColor(QPalette::Window, QColor(53, 53, 53));
+	palette.setColor(QPalette::WindowText, Qt::white);
+	palette.setColor(QPalette::Base, QColor(15, 15, 15));
+	palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+	palette.setColor(QPalette::ToolTipBase, Qt::white);
+	palette.setColor(QPalette::ToolTipText, Qt::white);
+	palette.setColor(QPalette::Text, Qt::white);
+	palette.setColor(QPalette::Button, QColor(53, 53, 53));
+	palette.setColor(QPalette::ButtonText, Qt::white);
+	palette.setColor(QPalette::BrightText, Qt::red);
+
+//	palette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
+//	palette.setColor(QPalette::HighlightedText, Qt::black);
+
+	palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+	palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+	qApp->setPalette(palette);
+#endif
+
 #if 1
 	QFile f("style\\style.qss");
 	if (!f.exists())
