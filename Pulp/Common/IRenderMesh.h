dde237356a7209472d8bad55c59f74139e107f85
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-20 18:31:56 +0000

    Remove the old unused IRenderMesh interface, it was commented out just cleaning up.

--- Pulp/Common/IRenderMesh.h
+++ Pulp/Common/IRenderMesh.h
@@ -9,43 +9,6 @@
 
 X_NAMESPACE_BEGIN(model)
 
-// a mesh that can have both system and video memory.
-
-#if 0
-
-struct IRenderMesh
-{
-	virtual ~IRenderMesh() {};
-
-	virtual const int addRef(void) X_ABSTRACT;
-	virtual const int release(void) X_ABSTRACT;
-
-	virtual bool render(void) X_ABSTRACT;
-
-	// returns false if no Video memory.
-	virtual bool canRender(void) X_ABSTRACT;
-	virtual bool uploadToGpu(void) X_ABSTRACT;
-
-	// genral Info
-	virtual const char* getName(void) const X_ABSTRACT;
-	virtual int getNumVerts(void) const X_ABSTRACT;
-	virtual int getNumIndexes(void) const X_ABSTRACT;
-	virtual int getNumSubMesh(void) const X_ABSTRACT;
-
-	virtual render::shader::VertexFormat::Enum getVertexFmt(void) const X_ABSTRACT;
-
-	// Mem Info
-	virtual int MemoryUsageTotal(void) const X_ABSTRACT;
-	virtual int MemoryUsageSys(void) const X_ABSTRACT;
-	virtual int MemoryUsageVideo(void) const X_ABSTRACT;
-
-
-};
-
-
-
-#endif
-
 
 
 X_NAMESPACE_END
