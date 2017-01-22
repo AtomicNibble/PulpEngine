69c1819579fb8aedf18125e6d24ff95f385c483a
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-22 14:42:08 +0000

    Add a move helper that moves a range to a destination.

--- Pulp/Common/Memory/MemUtil.h
+++ Pulp/Common/Memory/MemUtil.h
@@ -11,7 +11,12 @@ namespace Mem
 	void SIMDMemFill(void* __restrict pDest, __m128 FillVector, size_t numQuadwords);
 
 
+	template<class _InIt, class _OutIt> 
+	inline _OutIt Move(_InIt first, _InIt last, _OutIt dest);
+
 
 } // namepsace Mem
 
 X_NAMESPACE_END
+
+#include "MemUtil.inl"
