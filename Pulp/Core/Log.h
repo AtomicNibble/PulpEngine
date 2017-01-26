73568077bab016f7c366c1667a42b4d465aea9f9
Tom Crowley <tom@tom-crowley.co.uk>
2017-01-26 22:29:29 +0000

    Update override to final.

--- Pulp/Core/Log.h
+++ Pulp/Core/Log.h
@@ -16,24 +16,24 @@ public:
 	XLog();
 	~XLog();
 
-	virtual void Init() X_OVERRIDE;
-	virtual void ShutDown() X_OVERRIDE;
+	virtual void Init() X_FINAL;
+	virtual void ShutDown() X_FINAL;
 
-	virtual void Log(const SourceInfo& sourceInfo, const char* channel, int verbosity, const char* foramt, ...) X_OVERRIDE;
+	virtual void Log(const SourceInfo& sourceInfo, const char* channel, int verbosity, const char* foramt, ...) X_FINAL;
 
-	virtual void Warning(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_OVERRIDE;
-	virtual void Error(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_OVERRIDE;
-	virtual void Fatal(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_OVERRIDE;
-	virtual void Assert(const SourceInfo& sourceInfo, const char* format, va_list args) X_OVERRIDE;
-	virtual void AssertVariable(const SourceInfo& sourceInfo, const char* format, ...) X_OVERRIDE;
+	virtual void Warning(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_FINAL;
+	virtual void Error(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_FINAL;
+	virtual void Fatal(const SourceInfo& sourceInfo, const char* channel, const char* foramt, ...) X_FINAL;
+	virtual void Assert(const SourceInfo& sourceInfo, const char* format, va_list args) X_FINAL;
+	virtual void AssertVariable(const SourceInfo& sourceInfo, const char* format, ...) X_FINAL;
 	 
-	virtual void AddLogger(LoggerBase* logger) X_OVERRIDE;
-	virtual void RemoveLogger(LoggerBase* logger) X_OVERRIDE;
+	virtual void AddLogger(LoggerBase* logger) X_FINAL;
+	virtual void RemoveLogger(LoggerBase* logger) X_FINAL;
 
-	virtual const char* GetIndentation(void) X_OVERRIDE;
+	virtual const char* GetIndentation(void) X_FINAL;
 
-	virtual void Indent(void) X_OVERRIDE;
-	virtual void UnIndent(void) X_OVERRIDE;
+	virtual void Indent(void) X_FINAL;
+	virtual void UnIndent(void) X_FINAL;
 
 
 private:
