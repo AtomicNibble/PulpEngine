4955f9948911060231c1f8667306bb61cc79680c
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-09 23:13:01 +0000

    Make all the RenderNull overrides final.

--- Pulp/RenderSystems/RenderNull/RenderNull.h
+++ Pulp/RenderSystems/RenderNull/RenderNull.h
@@ -10,62 +10,62 @@ class RenderNull : public IRender
 {
 public:
 
-	virtual bool init(HWND hWnd, uint32_t width, uint32_t hieght, texture::Texturefmt::Enum depthFmt) X_OVERRIDE;
-	virtual void shutDown(void) X_OVERRIDE;
-	virtual void freeResources(void) X_OVERRIDE;
+	virtual bool init(HWND hWnd, uint32_t width, uint32_t hieght, texture::Texturefmt::Enum depthFmt) X_FINAL;
+	virtual void shutDown(void) X_FINAL;
+	virtual void freeResources(void) X_FINAL;
 	
-	virtual void release(void) X_OVERRIDE;
+	virtual void release(void) X_FINAL;
 
-	virtual void registerVars(void) X_OVERRIDE;
-	virtual void registerCmds(void) X_OVERRIDE;
+	virtual void registerVars(void) X_FINAL;
+	virtual void registerCmds(void) X_FINAL;
 
-	virtual void renderBegin(void) X_OVERRIDE;
-	virtual void renderEnd(void) X_OVERRIDE;
+	virtual void renderBegin(void) X_FINAL;
+	virtual void renderEnd(void) X_FINAL;
 
-	virtual void submitCommandPackets(CommandBucket<uint32_t>& cmdBucket) X_OVERRIDE;
+	virtual void submitCommandPackets(CommandBucket<uint32_t>& cmdBucket) X_FINAL;
 
-	virtual IRenderAux* getAuxRender(AuxRenderer::Enum user) X_OVERRIDE;
+	virtual IRenderAux* getAuxRender(AuxRenderer::Enum user) X_FINAL;
 
-	virtual Vec2<uint32_t> getDisplayRes(void) const X_OVERRIDE;
+	virtual Vec2<uint32_t> getDisplayRes(void) const X_FINAL;
 
-	virtual IPixelBuffer* createDepthBuffer(const char* pNickName, Vec2i dim) X_OVERRIDE;
+	virtual IPixelBuffer* createDepthBuffer(const char* pNickName, Vec2i dim) X_FINAL;
 	virtual IPixelBuffer* createColorBuffer(const char* pNickName, Vec2i dim, uint32_t numMips,
-		texture::Texturefmt::Enum fmt) X_OVERRIDE;
-	virtual void releasePixelBuffer(render::IPixelBuffer* pPixelBuf) X_OVERRIDE;
+		texture::Texturefmt::Enum fmt) X_FINAL;
+	virtual void releasePixelBuffer(render::IPixelBuffer* pPixelBuf) X_FINAL;
 	
-	virtual IRenderTarget* getCurBackBuffer(uint32_t* pIdx = nullptr) X_OVERRIDE;
+	virtual IRenderTarget* getCurBackBuffer(uint32_t* pIdx = nullptr) X_FINAL;
 
-	virtual VertexBufferHandle createVertexBuffer(uint32_t numElements, uint32_t elementSize, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_OVERRIDE;
-	virtual VertexBufferHandle createVertexBuffer(uint32_t numElements, uint32_t elementSize, const void* pInitialData, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_OVERRIDE;
-	virtual IndexBufferHandle createIndexBuffer(uint32_t numElements, uint32_t elementSize, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_OVERRIDE;
-	virtual IndexBufferHandle createIndexBuffer(uint32_t numElements, uint32_t elementSize, const void* pInitialData, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_OVERRIDE;
+	virtual VertexBufferHandle createVertexBuffer(uint32_t numElements, uint32_t elementSize, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_FINAL;
+	virtual VertexBufferHandle createVertexBuffer(uint32_t numElements, uint32_t elementSize, const void* pInitialData, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_FINAL;
+	virtual IndexBufferHandle createIndexBuffer(uint32_t numElements, uint32_t elementSize, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_FINAL;
+	virtual IndexBufferHandle createIndexBuffer(uint32_t numElements, uint32_t elementSize, const void* pInitialData, BufUsage::Enum usage, CpuAccessFlags accessFlag) X_FINAL;
 
-	virtual void destoryVertexBuffer(VertexBufferHandle handle) X_OVERRIDE;
-	virtual void destoryIndexBuffer(IndexBufferHandle handle) X_OVERRIDE;
+	virtual void destoryVertexBuffer(VertexBufferHandle handle) X_FINAL;
+	virtual void destoryIndexBuffer(IndexBufferHandle handle) X_FINAL;
 
-	virtual void getVertexBufferSize(VertexBufferHandle handle, int32_t* pOriginal, int32_t* pDeviceSize = nullptr) X_OVERRIDE;
-	virtual void getIndexBufferSize(IndexBufferHandle handle, int32_t* pOriginal, int32_t* pDeviceSize = nullptr) X_OVERRIDE;
+	virtual void getVertexBufferSize(VertexBufferHandle handle, int32_t* pOriginal, int32_t* pDeviceSize = nullptr) X_FINAL;
+	virtual void getIndexBufferSize(IndexBufferHandle handle, int32_t* pOriginal, int32_t* pDeviceSize = nullptr) X_FINAL;
 
-	virtual ConstantBufferHandle createConstBuffer(shader::XCBuffer& cbuffer, BufUsage::Enum usage) X_OVERRIDE;
-	virtual void destoryConstBuffer(ConstantBufferHandle handle) X_OVERRIDE;
+	virtual ConstantBufferHandle createConstBuffer(shader::XCBuffer& cbuffer, BufUsage::Enum usage) X_FINAL;
+	virtual void destoryConstBuffer(ConstantBufferHandle handle) X_FINAL;
 
-	texture::ITexture* getTexture(const char* pName, texture::TextureFlags flags) X_OVERRIDE;
+	texture::ITexture* getTexture(const char* pName, texture::TextureFlags flags) X_FINAL;
 	texture::ITexture* getDefaultTexture(void) const X_FINAL;
-	texture::ITexture* createTexture(const char* pNickName, Vec2i dim, texture::Texturefmt::Enum fmt, const uint8_t* pInitialData = nullptr) X_OVERRIDE;
+	texture::ITexture* createTexture(const char* pNickName, Vec2i dim, texture::Texturefmt::Enum fmt, const uint8_t* pInitialData = nullptr) X_FINAL;
 
-	shader::IShaderSource* getShaderSource(const char* pName) X_OVERRIDE;
-	shader::IHWShader* createHWShader(shader::ShaderType::Enum type, const core::string& entry, shader::IShaderSource* pSourceFile) X_OVERRIDE;
-	shader::IShaderPermatation* createPermatation(shader::IHWShader* pVertex, shader::IHWShader* pPixel) X_OVERRIDE;
-	shader::IShaderPermatation* createPermatation(const shader::ShaderStagesArr& stages) X_OVERRIDE;
+	shader::IShaderSource* getShaderSource(const char* pName) X_FINAL;
+	shader::IHWShader* createHWShader(shader::ShaderType::Enum type, const core::string& entry, shader::IShaderSource* pSourceFile) X_FINAL;
+	shader::IShaderPermatation* createPermatation(shader::IHWShader* pVertex, shader::IHWShader* pPixel) X_FINAL;
+	shader::IShaderPermatation* createPermatation(const shader::ShaderStagesArr& stages) X_FINAL;
 
-	void releaseShaderPermatation(shader::IShaderPermatation* pPerm) X_OVERRIDE;
-	void releaseTexture(texture::ITexture* pTex) X_OVERRIDE;
+	void releaseShaderPermatation(shader::IShaderPermatation* pPerm) X_FINAL;
+	void releaseTexture(texture::ITexture* pTex) X_FINAL;
 
-	PassStateHandle createPassState(const RenderTargetFmtsArr& rtfs) X_OVERRIDE;
-	void destoryPassState(PassStateHandle handle) X_OVERRIDE;
+	PassStateHandle createPassState(const RenderTargetFmtsArr& rtfs) X_FINAL;
+	void destoryPassState(PassStateHandle handle) X_FINAL;
 
-	StateHandle createState(PassStateHandle passHandle, const shader::IShaderPermatation* pPerm, const StateDesc& state, const TextureState* pTextStates, size_t numStates) X_OVERRIDE;
-	void destoryState(StateHandle handle) X_OVERRIDE;
+	StateHandle createState(PassStateHandle passHandle, const shader::IShaderPermatation* pPerm, const StateDesc& state, const TextureState* pTextStates, size_t numStates) X_FINAL;
+	void destoryState(StateHandle handle) X_FINAL;
 
 private:
 	XCamera cam_;
