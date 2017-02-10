5fbba8c56165ab3b811c7e8353e74ec68bbb3a0f
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-10 00:30:03 +0000

    Allow "0" or "false" to not force.

--- tools/Converter/Main.cpp
+++ tools/Converter/Main.cpp
@@ -156,7 +156,7 @@ namespace
 		const wchar_t* pForce = gEnv->pCore->GetCommandLineArgForVarW(L"force");
 		if (pForce)
 		{
-			if (core::strUtil::IsEqual(pForce, L"0")) {
+			if (core::strUtil::IsEqual(pForce, L"0") || core::strUtil::IsEqual(pForce, L"false")) {
 				return false;
 			}
 			return true;
