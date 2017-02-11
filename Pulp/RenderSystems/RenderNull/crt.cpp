833e9ad13168ac8bf4371e836ab3a18a58c844e5
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-11 22:24:33 +0000

    Some additional crt logic, still disabled.
    I'm tempted to just make my own crt since default one is so bloated.

--- Pulp/RenderSystems/RenderNull/crt.cpp
+++ Pulp/RenderSystems/RenderNull/crt.cpp
@@ -4,11 +4,25 @@
 
 int const EpochStart = INT_MIN;
 
-extern "C" ULONG _tls_index = 0;
+typedef void(__cdecl *_PVFV)();
+extern _PVFV __xc_a[], __xc_z[];    // C++ initializers
+
+#pragma section(".CRT$XCA", read, write)
+#pragma data_seg(".CRT$XCA")		// start of ctor section
+_PVFV __xc_a[] = { 0 };
+
+#pragma section(".CRT$XCZ", read, write)
+#pragma data_seg(".CRT$XCZ")		// end of ctor section
+_PVFV __xc_z[] = { 0 };
+
 
 extern "C"
 {
+	void _initterm(_PVFV *pfbegin, _PVFV *pfend);
+
+	ULONG _tls_index = 0;
 	int _Init_global_epoch = EpochStart;
+
 	__declspec(thread) int _Init_thread_epoch = EpochStart;
 }
 
@@ -62,17 +76,34 @@ int atexit(void(__cdecl *func)(void))
 	return 0;
 }
 
-BOOL WINAPI _DllMainCRTStartup(
-	HINSTANCE hInstDll,         // handle to the DLL module
-	DWORD fdwReason,            // reason for calling function
-	LPVOID lpvReserved         // reserved
-	)
+
+BOOL WINAPI _DllMainCRTStartup(HINSTANCE hInstDll, DWORD reason, LPVOID lpvReserved)
 {
 	X_UNUSED(hInstDll);
-	X_UNUSED(fdwReason);
 	X_UNUSED(lpvReserved);
 
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		_initterm(__xc_a, __xc_z);
+	}
+
 	return TRUE;
+}
+
+extern "C"
+{
+
+	void _initterm(_PVFV *pfbegin, _PVFV *pfend)
+	{
+		while (pfbegin < pfend)
+		{
+			if (*pfbegin != nullptr) {
+				(**pfbegin)();
+			}
+			++pfbegin;
+		}
+	}
+
 }
 
 #endif
