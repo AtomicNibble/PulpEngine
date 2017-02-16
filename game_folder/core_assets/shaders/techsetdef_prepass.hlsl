cda8ef9226ecd3419508592741c8b778a9d05450
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-16 22:38:06 +0000

    updated default techdef.

---
+++ game_folder/core_assets/shaders/techsetdef_prepass.hlsl
@@ -0,0 +1,48 @@
+
+#include "Vertexbase.inc"
+
+struct VS_INPUT
+{
+  float3 osPosition             : POSITION;
+  float2 tex               : TEXCOORD0;
+  float2 tex1              : TEXCOORD1;
+  float4 color                  : COLOR0;
+  float4 normal                 : NORMAL0;
+};
+
+struct VS_OUTPUT
+{
+    float4 ssPosition               : SV_POSITION;
+    float4 color                    : COLOR;
+};
+
+struct PS_INPUT
+{
+    float4 ssPosition               : SV_POSITION;
+    float4 color                    : COLOR0;
+};
+
+struct PS_OUTPUT
+{
+    float4 color                : SV_TARGET0;
+};
+
+
+
+VS_OUTPUT vs_main( VS_INPUT IN )
+{
+  VS_OUTPUT OUT;
+  OUT.ssPosition = mul( worldToScreenMatrix, float4(IN.osPosition, 1.0) );
+  OUT.color =  IN.color;
+  return OUT;
+}
+
+PS_OUTPUT ps_main( PS_INPUT IN )
+{
+    PS_OUTPUT output;
+    output.color = IN.color;
+    output.color.r =  IN.ssPosition.x;
+    output.color.g =  IN.ssPosition.y;
+    output.color.b=  IN.ssPosition.z;
+    return output;
+}
---
+++ game_folder/core_assets/shaders/techsetdef_unlit.hlsl
@@ -0,0 +1,51 @@
+
+#include "Vertexbase.inc"
+
+struct VS_INPUT
+{
+  float3 osPosition             : POSITION;
+  float2 tex               : TEXCOORD0;
+  float2 tex1              : TEXCOORD1;
+  float4 color                  : COLOR0;
+  float4 normal                 : NORMAL0;
+};
+
+struct VS_OUTPUT
+{
+    float4 ssPosition               : SV_POSITION;
+    float2 texCoord               : TEXCOORD0;
+    float4 color                    : COLOR;
+};
+
+struct PS_INPUT
+{
+    float4 ssPosition               : SV_POSITION;
+    float2 texCoord               : TEXCOORD0;
+    float4 color                    : COLOR0;
+};
+
+struct PS_OUTPUT
+{
+    float4 color                : SV_TARGET0;
+};
+
+Texture2D          diffuse : register(t0);
+SamplerState        diffuseSampler;
+
+
+VS_OUTPUT vs_main( VS_INPUT IN )
+{
+  VS_OUTPUT OUT;
+  OUT.ssPosition = mul( worldToScreenMatrix, float4(IN.osPosition, 1.0) );
+  OUT.texCoord = IN.tex;
+  OUT.color =  IN.color;
+  return OUT;
+}
+
+PS_OUTPUT ps_main( PS_INPUT IN )
+{
+    PS_OUTPUT output;
+    float4 texCol = diffuse.Sample(diffuseSampler, IN.texCoord);
+    output.color = IN.color *  texCol;
+    return output;
+}
