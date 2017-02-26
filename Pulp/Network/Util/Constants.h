25d272e88944d666e22cac5ab55fadcffc756be4
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-26 01:38:57 +0000

    define a unassigned netguid value.

--- Pulp/Network/Util/Constants.h
+++ Pulp/Network/Util/Constants.h
@@ -11,6 +11,7 @@ X_NAMESPACE_BEGIN(net)
 static const SocketHandle INVALID_SOCKET = SocketHandle(~0);
 
 const SystemAdd UNASSIGNED_SYSTEM_ADDRESS;
+const NetGUID UNASSIGNED_NET_GUID(0ull);
 
 static const uint32_t MAX_INTERNAL_IDS = 8;
 static const uint32_t MAX_ORDERED_STREAMS = 255;
