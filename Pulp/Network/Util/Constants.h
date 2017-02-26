125330f73536ca19f78c52bf117027f77902451c
Tom Crowley <tom@tom-crowley.co.uk>
2017-02-26 01:37:58 +0000

    define max/min mtu size.

--- Pulp/Network/Util/Constants.h
+++ Pulp/Network/Util/Constants.h
@@ -14,5 +14,8 @@ const SystemAdd UNASSIGNED_SYSTEM_ADDRESS;
 
 static const uint32_t MAX_INTERNAL_IDS = 8;
 static const uint32_t MAX_ORDERED_STREAMS = 255;
+static const uint32_t MAX_MTU_SIZE = 1492;
+static const uint32_t MIN_MTU_SIZE = 400;
+
 
 X_NAMESPACE_END
