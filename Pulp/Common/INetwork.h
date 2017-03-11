8bc40130d81d4acaa9761e983e5f28729ab65496
Tom Crowley <tom@tom-crowley.co.uk>
2017-03-11 13:13:07 +0000

    double password size, now that we send hash not raw data.

--- Pulp/Common/INetwork.h
+++ Pulp/Common/INetwork.h
@@ -8,7 +8,7 @@ X_NAMESPACE_BEGIN(net)
 static const uint32_t MAX_ORDERED_STREAMS = 16; // can bump this but it increases memory per connection.
 static const uint32_t MAX_SUPPORTED_PEERS = 1 << 10; // go nuts.
 static const uint32_t MAX_BAN_ENTRIES = 256; // max bans entries you can add, wildcards supported.
-static const uint32_t MAX_PASSWORD_LEN = 64;
+static const uint32_t MAX_PASSWORD_LEN = 128;
 static const uint32_t MAX_PEERS = 8; // a server only needs 1 peer.
 
 X_DECLARE_ENUM8(ConnectionState)(
