bcc08faf1b8abf55444afd56e1a46780d4f83f7e
Tom Crowley <tom@tom-crowley.co.uk>
2017-03-18 22:47:38 +0000

    remove some stupid macro's I will propbs remove the other loop ones at somepoint.

--- Pulp/Common/Util/HelperMacros.h
+++ Pulp/Common/Util/HelperMacros.h
@@ -7,21 +7,13 @@
 
 
 // Kinky quick loops
-#define lopv(v)    for(int i=0;i<(v).Num();i++)
-#define lopvj(v)   for(int j=0;j<(v).Num();j++)
-#define lopvk(v)   for(int k=0;k<(v).Num();k++)
-#define lopvx(v)   for(int x=0;x<(v).Num();x++)
-#define lopvrev(v) for(int i=(v).Num()-1;i>=0;i--)
-
 #define loop(v,m) for(int v=0;v<m;v++)
-
 
 #define lopi(m) loop(i,m)
 #define lopj(m) loop(j,m)
 #define lopk(m) loop(k,m)
 #define lopl(m) loop(l,m)
 #define lopx(m) loop(x,m)
-
 
 
 #define X_VALIST_START( fmt ) \
