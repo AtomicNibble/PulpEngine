c666bbe68290091c5a0ab4f477e812f6d5065288
Tom Crowley <tom@tom-crowley.co.uk>
2017-03-18 22:19:07 +0000

    Add a config setting just to disable dlmalloc stats, it's super slow.
    The main problm is that i'm calling it after each alloc / free rather than when i want statistics.
    Not sure how I would stop that currently as the arena's pull allocator stats up.

--- Pulp/Common/Memory/AllocationPolicies/GrowingBlockAllocator.cpp
+++ Pulp/Common/Memory/AllocationPolicies/GrowingBlockAllocator.cpp
@@ -3,6 +3,19 @@
 #include "GrowingBlockAllocator.h"
 #include "Util/PointerUtil.h"
 
+
+#if !X_ENABLE_MEMORY_ALLOCATOR_STATISTICS
+#define NO_MALLINFO
+#define NO_MALLOC_STATS
+#else
+#define NO_MALLOC_STATS
+
+#if X_ENABLE_MEMORY_ALLOCATOR_DMALLOC_STATISTICS
+#define NO_MALLINFO
+#endif // !X_ENABLE_MEMORY_ALLOCATOR_DMALLOC_STATISTICS
+
+#endif // !X_ENABLE_MEMORY_ALLOCATOR_STATISTICS
+
 #include "memory/dlmalloc-2.8.6.h"
 
 X_NAMESPACE_BEGIN(core)
@@ -54,18 +67,18 @@ void* GrowingBlockAllocator::allocate( size_t originalSize, size_t alignment, si
 		statistics_.internalOverhead_ += sizeof( BlockHeader );
 		statistics_.wasteAlignment_ += safe_static_cast<size_t>((uintptr_t)as_void - (uintptr_t)pMem);
 
+#if X_ENABLE_MEMORY_ALLOCATOR_DMALLOC_STATISTICS
 		mallinfo info = mspace_mallinfo( memorySpace_ );
-
 		statistics_.physicalMemoryUsed_ = info.uordblks;
 		statistics_.physicalMemoryAllocated_ = info.uordblks;
 		statistics_.physicalMemoryUsedMax_ = info.usmblks;
 		statistics_.physicalMemoryAllocatedMax_ = info.usmblks;
-		
+#endif // !X_ENABLE_MEMORY_ALLOCATOR_DMALLOC_STATISTICS
 
 		statistics_.allocationCountMax_ = Max( statistics_.allocationCount_, statistics_.allocationCountMax_ );
 		statistics_.wasteAlignmentMax_ = Max( statistics_.wasteAlignment_, statistics_.wasteAlignmentMax_ );
 		statistics_.internalOverheadMax_ = Max( statistics_.internalOverhead_, statistics_.internalOverheadMax_ );
-#endif
+#endif // !X_ENABLE_MEMORY_ALLOCATOR_STATISTICS
 
 		return as_void;
 	}
@@ -93,13 +106,12 @@ void GrowingBlockAllocator::free( void* ptr )
 	statistics_.internalOverhead_ -= sizeof( BlockHeader );
 	statistics_.wasteAlignment_ -= safe_static_cast<size_t>(reinterpret_cast<uintptr_t>(ptr) - 
 		reinterpret_cast<uintptr_t>(as_header->originalAllocation_));
-#endif
+#endif // !X_ENABLE_MEMORY_ALLOCATOR_STATISTICS
 
 	mspace_free( memorySpace_, as_header->originalAllocation_ );
 
-#if X_ENABLE_MEMORY_ALLOCATOR_STATISTICS
+#if X_ENABLE_MEMORY_ALLOCATOR_STATISTICS && X_ENABLE_MEMORY_ALLOCATOR_DMALLOC_STATISTICS
 	mallinfo info = mspace_mallinfo( memorySpace_ );
-
 	statistics_.physicalMemoryUsed_ = info.uordblks;
 	statistics_.physicalMemoryAllocated_ = info.uordblks;
 #endif
