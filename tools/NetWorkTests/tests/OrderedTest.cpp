84abd1f7bab35b539587cb4d80b67c1b045b05c1
Tom Crowley <tom@tom-crowley.co.uk>
2017-03-19 00:21:45 +0000

    Some more tests, empty currently.

---
+++ tools/NetWorkTests/tests/OrderedTest.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+
+X_NAMESPACE_BEGIN(net)
+
+
+TEST(net, OrderedPacketsTest)
+{
+
+
+
+
+}
+
+X_NAMESPACE_END
---
+++ tools/NetWorkTests/tests/SequencedTest.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+
+
+X_NAMESPACE_BEGIN(net)
+
+
+TEST(net, SequencedPacketsTest)
+{
+
+
+
+
+}
+
+X_NAMESPACE_END
