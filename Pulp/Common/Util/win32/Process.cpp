4f4ea8f3465f1d685bb1684440da5fb56b9d7167
Tom Crowley <tom@tom-crowley.co.uk>
2017-04-15 02:12:59 +0100

    Add a Process class with some basic functionality for setting priority.
    I may extended it to support opening other process's and module traversal if needed.

---
+++ Pulp/Common/Util/win32/Process.cpp
@@ -0,0 +1,109 @@
+#include <EngineCommon.h>
+#include "Process.h"
+
+X_NAMESPACE_BEGIN(core)
+
+
+Process::Process(HANDLE process, uint32_t processID) :
+	process_(process),
+	processID_(processID)
+{
+
+}
+
+
+Process::Process() :
+	process_(INVALID_HANDLE_VALUE),
+	processID_(0)
+{
+
+}
+
+Process::~Process()
+{
+
+}
+
+
+Process::Priority::Enum Process::GetPriorityClass(void) const
+{
+	DWORD priorityClass = ::GetPriorityClass(process_);
+	if (priorityClass == 0)
+	{
+		lastError::Description Dsc;
+		X_ERROR("Process", "Failed to get process priority for id: % " PRIu32 ". Error: %s", processID_, lastError::ToString(Dsc));
+		return Priority::NORMAL;
+	}
+
+	switch (priorityClass)
+	{
+		case IDLE_PRIORITY_CLASS:
+			return Priority::IDLE;
+		case BELOW_NORMAL_PRIORITY_CLASS:
+			return Priority::BELOW_NORMAL;
+		case NORMAL_PRIORITY_CLASS:
+			return Priority::NORMAL;
+		case ABOVE_NORMAL_PRIORITY_CLASS:
+			return Priority::ABOVE_NORMAL;
+		case HIGH_PRIORITY_CLASS:
+			return Priority::HIGH;
+		case REALTIME_PRIORITY_CLASS:
+			return Priority::REALTIME;
+
+		default:
+			X_ASSERT_UNREACHABLE();
+			return Priority::NORMAL;
+	}
+}
+
+bool Process::SetPriorityClass(Priority::Enum priority)
+{	
+	int32_t pri = NORMAL_PRIORITY_CLASS;
+
+	switch (priority)
+	{
+		case Priority::IDLE:
+			pri = IDLE_PRIORITY_CLASS;
+			break;
+		case Priority::BELOW_NORMAL:
+			pri = BELOW_NORMAL_PRIORITY_CLASS;
+			break;
+		case Priority::NORMAL:
+			pri = NORMAL_PRIORITY_CLASS;
+			break;
+		case Priority::ABOVE_NORMAL:
+			pri = ABOVE_NORMAL_PRIORITY_CLASS;
+			break;
+		case Priority::HIGH:
+			pri = HIGH_PRIORITY_CLASS;
+			break;
+		case Priority::REALTIME:
+			pri = REALTIME_PRIORITY_CLASS;
+			break;
+
+		default:
+			X_ASSERT_UNREACHABLE();
+			break;
+	}
+
+
+	if (!::SetPriorityClass(process_, pri))
+	{
+		lastError::Description Dsc;
+		X_ERROR("Process", "Failed to set process priority for id: % " PRIu32 ". Error: %s", processID_, lastError::ToString(Dsc));
+		return false;
+	}
+
+	return true;
+}
+
+
+Process Process::GetCurrent(void)
+{
+	auto currentHandle = ::GetCurrentProcess();
+	auto currentID = ::GetCurrentProcessId();
+
+	return Process(currentHandle, currentID);
+}
+
+X_NAMESPACE_END
---
+++ Pulp/Common/Util/win32/Process.h
@@ -0,0 +1,43 @@
+#pragma once
+
+
+
+X_NAMESPACE_BEGIN(core)
+
+class Process
+{
+public:
+
+	X_DECLARE_ENUM(Priority)(
+		IDLE,
+		BELOW_NORMAL,
+		NORMAL,
+		ABOVE_NORMAL,
+		HIGH,
+		REALTIME
+	);
+
+private:
+	explicit Process(HANDLE process, uint32_t processID);
+
+public:
+	Process();
+	~Process();
+
+
+	uint32_t GetID(void);
+
+	Priority::Enum GetPriorityClass(void) const;
+	bool SetPriorityClass(Priority::Enum priority);
+
+public:
+
+	static Process GetCurrent(void);
+
+private:
+	HANDLE process_;
+	uint32_t processID_;
+};
+
+
+X_NAMESPACE_END
