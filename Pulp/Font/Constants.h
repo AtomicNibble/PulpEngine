4f4d99a5e522bdb53646468e853b56f83c0807d9
Tom Crowley <tom@tom-crowley.co.uk>
2017-04-19 00:24:29 +0100

    Define a precomputed cache str.

--- Pulp/Font/Constants.h
+++ Pulp/Font/Constants.h
@@ -13,6 +13,7 @@ static const int32_t FONT_TAB_CHAR_NUM = 4;
 static const float FONT_SPACE_SIZE = 0.2f;
 static const float FONT_GLYPH_PROP_SPACING = 1.f;
 
+static const wchar_t FONT_PRECACHE_STR[190] = L" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
 
 
 
