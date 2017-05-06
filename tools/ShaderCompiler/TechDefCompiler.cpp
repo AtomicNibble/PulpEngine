f7bf62fab3b10a87a5e8d8a932df47f88c9c1597
Tom Crowley <tom@tom-crowley.co.uk>
2017-05-06 15:53:19 +0100

    Add some initial logic for compiling a single perm for each tech in a TechDef.
    IT will need to compile all perms tho.

--- tools/ShaderCompiler/TechDefCompiler.cpp
+++ tools/ShaderCompiler/TechDefCompiler.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "TechDefCompiler.h"
 
-
 X_NAMESPACE_BEGIN(engine)
 
 namespace compiler
@@ -40,6 +39,7 @@ namespace compiler
 
 		for (int32_t i = 0; i < MaterialCat::ENUM_COUNT; i++) {
 			if (!Compile(static_cast<MaterialCat::Enum>(i))) {
+				X_ERROR("TechCompiler", "Failed to compile cat: \"%s\"", MaterialCat::ToString(i));
 				return false;
 			}
 		}
@@ -60,6 +60,7 @@ namespace compiler
 		for (const auto& tech : types)
 		{
 			if (!Compile(cat, tech)) {
+				X_ERROR("TechCompiler", "Failed to compile cat: \"%s\" tech: \"%s\"", MaterialCat::ToString(cat), tech.c_str());
 				return false;
 			}
 		}
@@ -70,6 +71,62 @@ namespace compiler
 
 	bool TechDefCompiler::Compile(MaterialCat::Enum cat, const core::string& techName)
 	{
+		// now we need the techDef.
+		engine::TechSetDef* pTechDef = nullptr;
+		if (!techDefs_.getTechDef(cat, techName.c_str(), pTechDef)) {
+			X_ERROR("TechCompiler", "Failed to get tech def for cat:  \"%s\" tech: \"%s\"", MaterialCat::ToString(cat), techName.c_str());
+			return false;
+		}
+
+		// for each tech.
+		for (auto it = pTechDef->techBegin(); it != pTechDef->techEnd(); ++it)
+		{
+			// get the shaders.
+			const Technique& tech = it->second; 
+
+			using namespace render::shader;
+			
+			for (uint32_t i = 0; i < ShaderType::ENUM_COUNT - 1; i++)
+			{
+				const auto type = static_cast<ShaderType::Enum>(i);
+				const auto stage = staderTypeToStageFlag(type);
+
+				if (!tech.stages.IsSet(stage)) {
+					continue;
+				}
+
+				const auto& shader = tech.shaders[type];
+
+				//  make sure the shader in the vertex slot is a vertex shader etc..
+				X_ASSERT(shader.type == type, "Incorrect shader type for stage index.")(shader.type, type);
+			
+				IShaderSource* pShaderSource = shaderMan_.sourceforName(shader.source);
+				if (!pShaderSource) {
+					X_ERROR("TechCompiler", "Failed to get shader source for compiling: \"%s\"", shader.source.c_str());
+					return false;
+				}
+
+				CompileFlags flags;
+				flags = CompileFlag::OptimizationLvl3 | CompileFlag::TreatWarningsAsErrors;
+
+				PermatationFlags permFlags = Permatation::VertStreams;
+
+				XHWShader* pHWShader = shaderMan_.createHWShader(type, shader.entry, pShaderSource, permFlags);
+				if (!pHWShader) {
+					X_ERROR("TechCompiler", "Failed to create HWShader for compiling: \"%s\"", shader.source.c_str());
+					return false;
+				}
+
+				if (!shaderMan_.compileShader(pHWShader, flags))
+				{
+					X_ERROR("TechCompiler", "Failed to compile HWShader", shader.source.c_str());
+					return false;
+				}
+
+				// we don't release the hwshader, so another tech that uses same source / flags
+				// won't need to bother compiling.
+			}
+		}
 
 		return true;
 	}
