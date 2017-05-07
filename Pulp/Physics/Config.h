f481450b22ac9fe5f1a8df17679b4707f294fe84
Tom Crowley <tom@tom-crowley.co.uk>
2017-05-07 13:59:33 +0100

    Add sections for config specic values.

--- Pulp/Physics/Config.h
+++ Pulp/Physics/Config.h
@@ -5,8 +5,25 @@
 X_NAMESPACE_BEGIN(physics)
 
 
-#define PHYSX_SCENE_REQUIRES_LOCK 1	// 
-#define PHYSX_DEFAULT_ALLOCATOR 0	// use the default allocaotr from physx lib.
+// same for all configs.
+#define PHYSX_SCENE_REQUIRES_LOCK 1	 
+#define PHYSX_DEFAULT_ALLOCATOR 0	
+
+
+#if X_DEBUG
+
+
+#elif X_RELEASE
+
+
+#elif X_SUPER
+
+
+
+#else
+#error "unkonw config"
+#endif
+
 
 
 X_NAMESPACE_END
