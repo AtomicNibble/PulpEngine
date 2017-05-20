75f6f9e64ad9047620f66c6684de795587563aad
Tom Crowley <tom@tom-crowley.co.uk>
2017-05-21 00:06:03 +0100

    Make the flag / enum macros work in clang

--- Pulp/Common/Util/EnumMacros.h
+++ Pulp/Common/Util/EnumMacros.h
@@ -8,7 +8,7 @@
 #include <Prepro\PreproExpandArgs.h>
 #include <Prepro\PreproPassArgs.h>
 
-#define X_DECLARE_ENUM_IMPL_ENUM(value, n)					value = (n),
+#define X_DECLARE_ENUM_IMPL_ENUM(value, n)					value = n,
 #define X_DECLARE_ENUM_IMPL_TO_STRING(value, n)			case value: return X_PP_STRINGIZE(value);
 
 #define X_DECLARE_ENUM_IMPL(...)														\
@@ -17,14 +17,14 @@
 																						\
 		enum Enum																		\
 		{																				\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_ENUM_IMPL_ENUM, __VA_ARGS__)		\
+			X_PP_EXPAND_ARGS(X_DECLARE_ENUM_IMPL_ENUM, __VA_ARGS__)		\
 		};																				\
 																						\
 		static const char* ToString(uint32_t value)										\
 		{																				\
 		switch (value)																	\
 			{																			\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_ENUM_IMPL_TO_STRING, __VA_ARGS__)	\
+			X_PP_EXPAND_ARGS(X_DECLARE_ENUM_IMPL_TO_STRING, __VA_ARGS__)	\
 			default:																	\
 			X_NO_SWITCH_DEFAULT;														\
 			}																			\
@@ -36,13 +36,13 @@
 		static const unsigned int ENUM_COUNT = X_PP_NUM_ARGS(__VA_ARGS__);				\
 		enum Enum : uint8_t																\
 		{																				\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_ENUM_IMPL_ENUM, __VA_ARGS__)		\
+			X_PP_EXPAND_ARGS(X_DECLARE_ENUM_IMPL_ENUM, __VA_ARGS__)		\
 		};																				\
 		static const char* ToString(uint32_t value)										\
 		{																				\
 		switch (value)																	\
 			{																			\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_ENUM_IMPL_TO_STRING, __VA_ARGS__)	\
+			X_PP_EXPAND_ARGS(X_DECLARE_ENUM_IMPL_TO_STRING, __VA_ARGS__)	\
 			default:																	\
 			X_NO_SWITCH_DEFAULT;														\
 			}																			\
--- Pulp/Common/Util/FlagsMacros.h
+++ Pulp/Common/Util/FlagsMacros.h
@@ -55,47 +55,49 @@
 																																			\
 		enum Enum																															\
 		{																																	\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS_IMPL_ENUM, __VA_ARGS__)														\
+			X_PP_EXPAND_ARGS(X_DECLARE_FLAGS_IMPL_ENUM, __VA_ARGS__)														\
 		};																																	\
 																																			\
 		struct Bits																															\
 		{																																	\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS_IMPL_BITS, __VA_ARGS__)														\
+			X_PP_EXPAND_ARGS(X_DECLARE_FLAGS_IMPL_BITS, __VA_ARGS__)														\
 		};																																	\
 																																			\
 		static const char* ToString(uint32_t value)																							\
 		{																																	\
 			switch (value)																													\
 			{																																\
-				X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS_IMPL_TO_STRING, __VA_ARGS__)												\
+				X_PP_EXPAND_ARGS(X_DECLARE_FLAGS_IMPL_TO_STRING, __VA_ARGS__)												\
 				default:																													\
 					X_NO_SWITCH_DEFAULT;																									\
+					return "<err>";																											\
 			}																																\
 		}																																	\
 	}
 
 #define X_DECLARE_FLAGS8_IMPL(...)																											\
 	{																																		\
-	static const unsigned int FLAGS_COUNT = X_PP_NUM_ARGS(__VA_ARGS__);																\
-	static_assert(FLAGS_COUNT <= 8, "Too many flags used in X_DECLARE_FLAGS. A maximum number of 8 flags is allowed.");				\
+			static const unsigned int FLAGS_COUNT = X_PP_NUM_ARGS(__VA_ARGS__);																\
+			static_assert(FLAGS_COUNT <= 8, "Too many flags used in X_DECLARE_FLAGS. A maximum number of 8 flags is allowed.");				\
 	\
-enum Enum : uint8_t																																	\
+		enum Enum : uint8_t																																	\
 		{																																	\
-		X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS_IMPL_ENUM, __VA_ARGS__)														\
+			X_PP_EXPAND_ARGS(X_DECLARE_FLAGS_IMPL_ENUM, __VA_ARGS__)														\
 		};																																	\
 		\
-struct Bits																															\
+		struct Bits																															\
 		{																																	\
-		X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS8_IMPL_BITS, __VA_ARGS__)														\
+			X_PP_EXPAND_ARGS(X_DECLARE_FLAGS8_IMPL_BITS, __VA_ARGS__)														\
 		};																																	\
 		\
 		static const char* ToString(uint8_t value)																							\
 		{																																	\
-		switch (value)																													\
+			switch (value)																													\
 			{																																\
-			X_PP_EXPAND_ARGS X_PP_PASS_ARGS(X_DECLARE_FLAGS_IMPL_TO_STRING, __VA_ARGS__)												\
-		default:																													\
-		X_NO_SWITCH_DEFAULT;																									\
+				X_PP_EXPAND_ARGS(X_DECLARE_FLAGS_IMPL_TO_STRING, __VA_ARGS__)												\
+				default:																													\
+					X_NO_SWITCH_DEFAULT;																									\
+					return "<err>";																											\
 			}																																\
 		}																																	\
 	}
