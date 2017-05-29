347dcbc169726ec070ac2035084e5147453946c2
Tom Crowley <tom@tom-crowley.co.uk>
2017-05-29 11:57:37 +0100

    Add initial default texture / texture loading & processing logic to texMan.
    Alot of this needs refining, but currently it loads the default textures in a async manner.

--- Pulp/3DEngine/Texture/TextureManager.cpp
+++ Pulp/3DEngine/Texture/TextureManager.cpp
@@ -11,9 +11,16 @@ X_NAMESPACE_BEGIN(engine)
 TextureManager::TextureManager(core::MemoryArenaBase* arena) :
 	arena_(arena),
 	textures_(arena, sizeof(TextureResource), core::Max(64_sz, X_ALIGN_OF(TextureResource))),
-	pCILoader_(nullptr)
+	pCILoader_(nullptr),
+
+	blockAlloc_(),
+	blockArena_(&blockAlloc_, "TextureBlockAlloc"),
+	streamQueue_(arena),
+	loadComplete_(true),
+	pTexDefault_(nullptr),
+	pTexDefaultBump_(nullptr)
 {
-		
+	defaultLookup_.fill(nullptr);
 }
 
 TextureManager::~TextureManager()
@@ -53,6 +60,7 @@ bool TextureManager::init(void)
 	static_assert(texture::ImgFileFormat::ENUM_COUNT == 7, "Added additional img src fmts? this code needs updating.");
 
 	textureLoaders_.append(pCILoader_);
+
 	if (vars_.allowFmtDDS()) {
 		textureLoaders_.append(X_NEW(texture::DDS::XTexLoaderDDS, arena_, "DDSLoader"));
 	}
@@ -69,10 +77,12 @@ bool TextureManager::init(void)
 		textureLoaders_.append(X_NEW(texture::TGA::XTexLoaderTGA, arena_, "TGALoader"));
 	}
 
+	if (!loadDefaultTextures()) {
+		return false;
+	}
 
 	return true;
 }
-
 
 void TextureManager::shutDown(void)
 {
@@ -92,7 +102,33 @@ void TextureManager::shutDown(void)
 	textureLoaders_.clear();
 }
 
+bool TextureManager::asyncInitFinalize(void)
+{
+	// we need to know that the default textures have finished loading and are ready.
 
+	while (pTexDefault_->isLoading() && pTexDefaultBump_->isLoading())
+	{
+		loadComplete_.wait();
+	}
+
+	if (pTexDefault_->isLoading()) {
+		X_ERROR("Texture", "failed to load default texture: %s", texture::TEX_DEFAULT_DIFFUSE);
+		return false;
+	}
+	if (pTexDefaultBump_->isLoading()) {
+		X_ERROR("Texture", "failed to load default bump texture: %s", texture::TEX_DEFAULT_BUMP);
+		return false;
+	}
+
+	return true;
+}
+
+
+void TextureManager::scheduleStreaming(void)
+{
+	// we work out what to start streaming.
+
+}
 
 
 Texture* TextureManager::forName(const char* pName, texture::TextureFlags flags)
@@ -112,14 +148,108 @@ Texture* TextureManager::forName(const char* pName, texture::TextureFlags flags)
 	else
 	{
 		pTexRes = textures_.createAsset(name, name, flags);
-
 		threadPolicy.Leave();
 	}
 
 	return pTexRes;
 }
 
+Texture* TextureManager::getDefault(render::TextureSlot::Enum slot) const
+{
+	return X_ASSERT_NOT_NULL(defaultLookup_[slot]);
+}
 
+void TextureManager::releaseTexture(Texture* pTex)
+{
+	TexRes* pTexRes = static_cast<TexRes*>(X_ASSERT_NOT_NULL(pTex));
+
+	if (pTexRes->removeReference() == 0)
+	{
+		textures_.releaseAsset(pTexRes);
+	}
+}
+
+void TextureManager::dispatchRead(Texture* pTexture)
+{
+	X_UNUSED(pTexture);
+
+	// lets dispatch a IO request to read the image data.
+	// where to we store the data?
+	// we want fixed buffers for streaming.
+	core::Path<char> path(pTexture->getName());
+	path.toLower(); // lower case file names only.
+	path.setExtension(texture::CI_FILE_EXTENSION);
+
+	core::IFileSys::fileModeFlags mode;
+	mode.Set(core::IFileSys::fileMode::READ);
+	mode.Set(core::IFileSys::fileMode::SHARE);
+
+	core::IoCallBack del;
+	del.Bind<TextureManager, &TextureManager::IoRequestCallback>(this);
+
+	core::IoRequestOpenRead req;
+	req.callback = del;
+	req.pUserData = pTexture;
+	req.mode = mode;
+	req.path = path;
+	req.arena = &blockArena_;
+	
+	gEnv->pFileSys->AddIoRequestToQue(req);
+}
+
+
+void TextureManager::IoRequestCallback(core::IFileSys&, const core::IoRequestBase* pReqBase, core::XFileAsync* pFile, uint32_t bytesRead)
+{
+	// who is this?
+	if (pReqBase->getType() == core::IoRequest::OPEN_READ_ALL)
+	{
+		auto* pReq = static_cast<const core::IoRequestOpenRead*>(pReqBase);
+		Texture* pTexture = static_cast<Texture*>(pReq->pUserData);
+		auto& flags = pTexture->flags();
+
+		// if we have loaded the data, we now need to process it.
+		if (!pFile || !bytesRead)
+		{
+			flags.Set(texture::TexFlag::LOAD_FAILED);
+		}
+		else
+		{
+			// we don't really want to do this in the UI thread.
+			processCIImageData(pTexture, pReq->pBuf, pReq->dataSize);
+		}
+
+		// raise that we just loaded a image.
+		loadComplete_.raise();
+
+	}
+
+}
+
+void TextureManager::processCIImageData(Texture* pTexture, const uint8_t* pData, size_t length)
+{
+	auto& flags = pTexture->flags();
+
+	core::XFileFixedBuf file(pData, pData + length);
+	texture::XTextureFile imgFile(&blockArena_);
+
+	if (!pCILoader_->loadTexture(&file, imgFile))
+	{
+		flags.Set(texture::TexFlag::LOAD_FAILED);
+		return;
+	}
+	
+	// we don't really want / need to give textures names.
+	// it really should just be the id.
+	auto* iDeviceTex = gEnv->pRender->getDeviceTexture(pTexture->getID(), imgFile, true);
+
+	pTexture->pDeviceTexture_ = iDeviceTex;
+	
+	flags.Set(texture::TexFlag::LOADED);
+
+}
+
+
+#if 0
 bool TextureManager::loadFromFile(texture::XTextureFile& imgFile, const char* pPath)
 {
 	X_ASSERT_NOT_NULL(pCILoader_);
@@ -176,6 +306,57 @@ bool TextureManager::loadFromFile(texture::XTextureFile& imgFile, const char* pP
 	}
 
 	return false;
+}
+#endif
+
+
+bool TextureManager::loadDefaultTextures(void)
+{
+	using namespace texture;
+
+	TextureFlags default_flags = TextureFlags::DONT_RESIZE | TextureFlags::DONT_STREAM;
+
+	pTexDefault_ = forName(TEX_DEFAULT_DIFFUSE, default_flags);
+	pTexDefaultBump_ = forName(TEX_DEFAULT_BUMP, default_flags);
+
+	defaultLookup_.fill(pTexDefault_);
+	defaultLookup_[render::TextureSlot::NORMAL] = pTexDefaultBump_;
+
+
+	// start reading the texture data.
+	dispatchRead(pTexDefault_);
+	dispatchRead(pTexDefaultBump_);
+
+	return true;
+}
+
+void TextureManager::releaseDefaultTextures(void)
+{
+	if (pTexDefault_) {
+		releaseTexture(pTexDefault_);
+	}
+	if (pTexDefaultBump_) {
+		releaseTexture(pTexDefaultBump_);
+	}
+
+	pTexDefault_ = nullptr;
+	pTexDefaultBump_ = nullptr;
+}
+
+void TextureManager::releaseDanglingTextures(void)
+{
+	{
+		core::ScopedLock<TextureContainer::ThreadPolicy> lock(textures_.getThreadPolicy());
+
+		auto it = textures_.begin();
+		for (; it != textures_.end(); ++it) {
+			auto* pTexRes = it->second;
+			releaseTexture(pTexRes);
+			X_WARNING("Texture", "\"%s\" was not deleted. refs: %" PRIi32, pTexRes->getName(), pTexRes->getRefCount());
+		}
+	}
+
+	textures_.free();
 }
 
 void TextureManager::Job_OnFileChange(core::V2::JobSystem& jobSys, const core::Path<char>& name)
--- Pulp/3DEngine/Texture/TextureManager.h
+++ Pulp/3DEngine/Texture/TextureManager.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <Memory\AllocationPolicies\GrowingBlockAllocator.h>
 #include <Assets\AssertContainer.h>
+#include <Containers\Fifo.h>
+#include <Threading\Signal.h>
+
 #include <ITexture.h>
 
 #include "Vars\TextureVars.h"
 
+X_NAMESPACE_DECLARE(core,
+	namespace V2 {
+		struct Job;
+		class JobSystem;
+	}
+
+	struct IoRequestBase;
+	struct XFileAsync;
+)
 
 X_NAMESPACE_DECLARE(texture,
 	namespace CI {
@@ -22,7 +35,23 @@ class TextureManager : public core::IXHotReload
 	typedef TextureContainer::Resource TextureResource;
 	typedef TextureContainer::Resource TexRes;
 
+	typedef std::array<Texture*, render::TextureSlot::ENUM_COUNT> TextureSlotArr;
 	typedef core::FixedArray<texture::ITextureFmt*, 8> TextureLoadersArr;
+	typedef core::Fifo<Texture*> TextureQueue;
+
+	typedef core::MemoryArena<
+		core::MallocFreeAllocator,
+		core::MultiThreadPolicy<core::CriticalSection>,
+#if X_ENABLE_MEMORY_DEBUG_POLICIES
+		core::SimpleBoundsChecking,
+		core::SimpleMemoryTracking,
+		core::SimpleMemoryTagging
+#else
+		core::NoBoundsChecking,
+		core::NoMemoryTracking,
+		core::NoMemoryTagging
+#endif // !X_ENABLE_MEMORY_DEBUG_POLICIES
+	> BlockArena;
 
 public:
 	TextureManager(core::MemoryArenaBase* arena);
@@ -34,15 +63,31 @@ public:
 	bool init(void);
 	void shutDown(void);
 
+	bool asyncInitFinalize(void);
+
+	void scheduleStreaming(void);
+
 
 	Texture* forName(const char* pName, texture::TextureFlags flags);
+	Texture* getDefault(render::TextureSlot::Enum slot) const;
+
+	void releaseTexture(Texture* pTex);
 
 private:
-	bool loadFromFile(texture::XTextureFile& imgFile, const char* pPath);
-	bool processImgFile(Texture* pTex, texture::XTextureFile& imgFile);
+	void dispatchRead(Texture* pTexture);
 
+//	bool loadFromFile(texture::XTextureFile& imgFile, const char* pPath);
+//	bool processImgFile(Texture* pTex, texture::XTextureFile& imgFile);
+
+	bool loadDefaultTextures(void);
+	void releaseDefaultTextures(void);
+	void releaseDanglingTextures(void);
 
 private:
+	void IoRequestCallback(core::IFileSys&, const core::IoRequestBase*, core::XFileAsync*, uint32_t);
+	
+	void processCIImageData(Texture* pTexture, const uint8_t* pData, size_t length);
+
 	// IXHotReload
 	void Job_OnFileChange(core::V2::JobSystem& jobSys, const core::Path<char>& name) X_OVERRIDE;
 	// ~IXHotReload
@@ -54,6 +99,21 @@ private:
 
 	texture::CI::XTexLoaderCI* pCILoader_;
 	TextureLoadersArr textureLoaders_;
+
+private:
+	// streaming stuff
+	BlockArena::AllocationPolicy blockAlloc_;
+	BlockArena blockArena_;
+
+	TextureQueue streamQueue_;
+
+	core::Signal loadComplete_;
+
+private:
+	Texture* pTexDefault_;
+	Texture* pTexDefaultBump_;
+	
+	TextureSlotArr defaultLookup_;
 };
 
 
