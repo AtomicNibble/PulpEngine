63f76076fe42d6ae5f36375f778c7dd5d6267855
Tom Crowley <tom@tom-crowley.co.uk>
2017-06-04 00:43:33 +0100

    Move the compression logic out of main into it's own function.
    This way we can add a train 'mode' more easy.

--- tools/Compress/Compress.cpp
+++ tools/Compress/Compress.cpp
@@ -99,6 +99,207 @@ namespace
 		X_LOG0("Compressor", "^6-lvl^7		(lvl 1-9, default: 5) ^9not-required");
 	}
 
+	int DoCompression(CompressorArena& arena)
+	{
+		std::wstring inFile, outFile;
+
+		Algo::Enum algo = Algo::LZ4;
+		bool defalte = true;
+		CompressLevel::Enum lvl = CompressLevel::NORMAL;
+
+		// args
+		{
+			const wchar_t* pInFile = gEnv->pCore->GetCommandLineArgForVarW(L"if");
+			if (!pInFile) {
+
+				int32_t numArgs = __argc;
+				if (numArgs == 2) {
+					pInFile = __wargv[1];
+					outFile = pInFile;
+					outFile += L".out";
+					defalte = false;
+				}
+				else {
+					X_ERROR("Compress", "Missing required arg -if");
+					return -1;
+				}
+			}
+
+			inFile = pInFile;
+
+			const wchar_t* pMode = gEnv->pCore->GetCommandLineArgForVarW(L"mode");
+			if (pMode) {
+				if (core::strUtil::IsEqualCaseInsen(pMode, L"inflate")) {
+					defalte = false;
+				}
+			}
+
+			const wchar_t* pOutFile = gEnv->pCore->GetCommandLineArgForVarW(L"of");
+			if (pOutFile) {
+				outFile = pOutFile;
+			}
+
+			const wchar_t* pAlgo = gEnv->pCore->GetCommandLineArgForVarW(L"a");
+			if (pAlgo) {
+				int32_t iAlgo = core::strUtil::StringToInt<int32_t>(pAlgo);
+				iAlgo = constrain<int32_t>(iAlgo, 1, 3);
+
+				static_assert(core::Compression::Algo::ENUM_COUNT == 7, "Added additional compression algos? this code needs updating.");
+
+				if (iAlgo == 1) {
+					algo = Algo::LZ4;
+				}
+				else if (iAlgo == 2) {
+					algo = Algo::LZ4HC;
+				}
+				else if (iAlgo == 3) {
+					algo = Algo::LZ5;
+				}
+				else if (iAlgo == 4) {
+					algo = Algo::LZ5HC;
+				}
+				else if (iAlgo == 5) {
+					algo = Algo::LZMA;
+				}
+				else if (iAlgo == 6) {
+					algo = Algo::ZLIB;
+				}
+				else if (iAlgo == 7) {
+					algo = Algo::STORE;
+				}
+				else {
+					X_ASSERT_UNREACHABLE();
+					return -1;
+				}
+			}
+
+			const wchar_t* pLvl = gEnv->pCore->GetCommandLineArgForVarW(L"lvl");
+			if (pLvl) {
+				int32_t lvlArg = core::strUtil::StringToInt<int32_t>(pLvl);
+				lvlArg = constrain<int32_t>(lvlArg, 1, CompressLevel::ENUM_COUNT) - 1;
+
+				lvl = static_cast<CompressLevel::Enum>(lvlArg);
+			}
+		}
+
+		if (!defalte && outFile.empty()) {
+			X_ERROR("Compress", "Output file name missing.");
+			return 1;
+		}
+
+		core::Array<uint8_t> inFileData(&arena);
+		core::Array<uint8_t> outfileData(&arena);
+
+		core::StopWatch timer;
+
+		if (!ReadFileToBuf(inFile, inFileData)) {
+			X_ERROR("Compress", "Failed to read input file");
+			return -1;
+		}
+
+		const float loadTime = timer.GetMilliSeconds();
+		X_LOG0("Compress", "loadTime: ^2%fms", loadTime);
+
+		// if infalting get algo from buf.
+		if (!defalte) {
+			algo = ICompressor::getAlgo(inFileData);
+		}
+
+		ICompressor* pCompressor = nullptr;
+
+		static_assert(core::Compression::Algo::ENUM_COUNT == 7, "Added additional compression algos? this code needs updating.");
+
+		switch (algo)
+		{
+			case Algo::LZ4:
+				pCompressor = X_NEW(Compressor<core::Compression::LZ4>, &arena, "LZ4");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".lz4";
+				}
+				break;
+			case Algo::LZ4HC:
+				pCompressor = X_NEW(Compressor<core::Compression::LZ4HC>, &arena, "LZ4HC");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".lz4";
+				}
+				break;
+			case Algo::LZ5:
+				pCompressor = X_NEW(Compressor<core::Compression::LZ4>, &arena, "LZ4");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".lz4";
+				}
+				break;
+			case Algo::LZ5HC:
+				pCompressor = X_NEW(Compressor<core::Compression::LZ4HC>, &arena, "LZ4HC");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".lz4";
+				}
+				break;
+			case Algo::LZMA:
+				pCompressor = X_NEW(Compressor<core::Compression::LZMA>, &arena, "LZMA");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".lzma";
+				}
+				break;
+			case Algo::ZLIB:
+				pCompressor = X_NEW(Compressor<core::Compression::Zlib>, &arena, "ZLIB");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".zlib";
+				}
+				break;
+			case Algo::STORE:
+				pCompressor = X_NEW(Compressor<core::Compression::Store>, &arena, "Store");
+				if (defalte && outFile.empty()) {
+					outFile = inFile + L".store";
+				}
+				break;
+			default:
+				X_ERROR("Compress", "unknown compression algo: %i", algo);
+				return -1;
+		}
+
+		bool res = false;
+
+		timer.Start();
+
+		if (defalte) {
+			res = pCompressor->deflate(&arena, inFileData, outfileData, lvl);
+		}
+		else {
+			res = pCompressor->inflate(&arena, inFileData, outfileData);
+		}
+
+		const float compressTime = timer.GetMilliSeconds();
+		X_LOG0("Compress", "%s: ^2%fms", defalte ? "deflateTime" : "inflateTime", compressTime);
+
+		if (!res) {
+			X_ERROR("Compress", "%s failed.", defalte ? "deflation" : "inflation");
+			return -1;
+		}
+
+		timer.Start();
+
+		if (!WriteFileFromBuf(outFile, outfileData)) {
+			X_ERROR("Compress", "Failed to write output file");
+			return -1;
+		}
+
+		const float writeTime = timer.GetMilliSeconds();
+		X_LOG0("Compress", "writeTime: ^2%fms", writeTime);
+
+
+		X_DELETE(pCompressor, &arena);
+		return 0;
+	}
+
+	int DoTrain(CompressorArena& arena)
+	{
+		X_UNUSED(arena);
+
+
+		return 0;
+	}
+
 } // namespace 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -133,193 +334,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	PrintArgs();
 
-	std::wstring inFile, outFile;
+	int res = 0;
 
-	Algo::Enum algo = Algo::LZ4;
-	bool defalte = true;
-	CompressLevel::Enum lvl = CompressLevel::NORMAL;
-
-	// args
+	if (1)
 	{
-		const wchar_t* pInFile = gEnv->pCore->GetCommandLineArgForVarW(L"if");
-		if (!pInFile) {
-
-			int32_t numArgs = __argc;
-			if (numArgs == 2) {
-				pInFile = __wargv[1];
-				outFile = pInFile;
-				outFile += L".out";
-				defalte = false;
-			}
-			else {
-				X_ERROR("Compress", "Missing required arg -if");
-				return -1;
-			}
-		}
-
-		inFile = pInFile;
-
-		const wchar_t* pMode = gEnv->pCore->GetCommandLineArgForVarW(L"mode");
-		if (pMode) {
-			if (core::strUtil::IsEqualCaseInsen(pMode, L"inflate")) {
-				defalte = false;
-			}
-		}
-
-		const wchar_t* pOutFile = gEnv->pCore->GetCommandLineArgForVarW(L"of");
-		if (pOutFile) {
-			outFile = pOutFile;
-		}
-
-		const wchar_t* pAlgo = gEnv->pCore->GetCommandLineArgForVarW(L"a");
-		if (pAlgo) {
-			int32_t iAlgo = core::strUtil::StringToInt<int32_t>(pAlgo);
-			iAlgo = constrain<int32_t>(iAlgo, 1, 3);
-
-			static_assert(core::Compression::Algo::ENUM_COUNT == 7, "Added additional compression algos? this code needs updating.");
-
-			if (iAlgo == 1) {
-				algo = Algo::LZ4;
-			}
-			else if (iAlgo == 2) {
-				algo = Algo::LZ4HC;
-			}
-			else if (iAlgo == 3) {
-				algo = Algo::LZ5;
-			}
-			else if (iAlgo == 4) {
-				algo = Algo::LZ5HC;
-			}
-			else if (iAlgo == 5) {
-				algo = Algo::LZMA;
-			}
-			else if (iAlgo == 6) {
-				algo = Algo::ZLIB;
-			}
-			else if (iAlgo == 7) {
-				algo = Algo::STORE;
-			}
-			else {
-				X_ASSERT_UNREACHABLE();
-				return -1;
-			}
-		}
-
-		const wchar_t* pLvl = gEnv->pCore->GetCommandLineArgForVarW(L"lvl");
-		if (pLvl) {
-			int32_t lvlArg = core::strUtil::StringToInt<int32_t>(pLvl);
-			lvlArg = constrain<int32_t>(lvlArg, 1, CompressLevel::ENUM_COUNT) - 1;
-
-			lvl = static_cast<CompressLevel::Enum>(lvlArg);
-		}
+		res = DoCompression(arena);
 	}
-
-	if (!defalte && outFile.empty()) {
-		X_ERROR("Compress", "Output file name missing.");
-		return 1;
-	}
-
-	core::Array<uint8_t> inFileData(&arena);
-	core::Array<uint8_t> outfileData(&arena);
-
-	core::StopWatch timer;
-
-	if (!ReadFileToBuf(inFile, inFileData)) {
-		X_ERROR("Compress", "Failed to read input file");
-		return -1;
-	}
-	
-	const float loadTime = timer.GetMilliSeconds();
-	X_LOG0("Compress", "loadTime: ^2%fms", loadTime);
-
-	// if infalting get algo from buf.
-	if (!defalte) {
-		algo = ICompressor::getAlgo(inFileData);
-	}
-
-	ICompressor* pCompressor = nullptr;
-
-	static_assert(core::Compression::Algo::ENUM_COUNT == 7, "Added additional compression algos? this code needs updating.");
-	
-	switch (algo)
+	else
 	{
-		case Algo::LZ4:
-			pCompressor = X_NEW(Compressor<core::Compression::LZ4>, &arena, "LZ4");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".lz4";
-			}
-			break;
-		case Algo::LZ4HC:
-			pCompressor = X_NEW(Compressor<core::Compression::LZ4HC>, &arena, "LZ4HC");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".lz4";
-			}
-			break;
-		case Algo::LZ5:
-			pCompressor = X_NEW(Compressor<core::Compression::LZ4>, &arena, "LZ4");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".lz4";
-			}
-			break;
-		case Algo::LZ5HC:
-			pCompressor = X_NEW(Compressor<core::Compression::LZ4HC>, &arena, "LZ4HC");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".lz4";
-			}
-			break;
-		case Algo::LZMA:
-			pCompressor = X_NEW(Compressor<core::Compression::LZMA>, &arena, "LZMA");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".lzma";
-			}
-			break;
-		case Algo::ZLIB:
-			pCompressor = X_NEW(Compressor<core::Compression::Zlib>, &arena, "ZLIB");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".zlib";
-			}
-			break;
-		case Algo::STORE:
-			pCompressor = X_NEW(Compressor<core::Compression::Store>, &arena, "Store");
-			if (defalte && outFile.empty()) {
-				outFile = inFile + L".store";
-			}
-			break;
-		default:
-			X_ERROR("Compress", "unknown compression algo: %i", algo);
-			return -1;
+		res = DoTrain(arena);
 	}
 
-	bool res = false;
 
-	timer.Start();
-
-	if (defalte) {
-		res = pCompressor->deflate(&arena, inFileData, outfileData, lvl);
-	}
-	else {
-		res = pCompressor->inflate(&arena, inFileData, outfileData);
-	}
-
-	const float compressTime = timer.GetMilliSeconds();
-	X_LOG0("Compress", "%s: ^2%fms", defalte ? "deflateTime" : "inflateTime", compressTime);
-
-	if (!res) {
-		X_ERROR("Compress", "%s failed.", defalte ? "deflation" : "inflation");
-		return -1;
-	}
-
-	timer.Start();
-
-	if (!WriteFileFromBuf(outFile, outfileData)) {
-		X_ERROR("Compress", "Failed to write output file");
-		return -1;
-	}
-
-	const float writeTime = timer.GetMilliSeconds();
-	X_LOG0("Compress", "writeTime: ^2%fms", writeTime);
-
-
-	X_DELETE(pCompressor, &arena);
-    return 0;
+    return res;
 }
