f012467496f1dd18072215c8574bcc647e0ce4c8
Tom Crowley <tom@tom-crowley.co.uk>
2017-06-10 23:04:04 +0100

    Add a limit for max tech defs.

--- Pulp/Common/IMaterial.h
+++ Pulp/Common/IMaterial.h
@@ -33,6 +33,7 @@ static const uint32_t    MTL_MAX_SAMPLERS = 8;
 static const uint32_t    MTL_MAX_PARAMS = 16;
 
 
+static const uint32_t    TECH_DEFS_MAX = 256;
 static const char*		 TECH_DEFS_DIR = "techdefs";
 static const char*		 TECH_DEFS_FILE_EXTENSION = "techsetdef";
 
