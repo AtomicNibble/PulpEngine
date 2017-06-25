2c96eb72afc4b0d1d97ef9a98a6da0c6e7f1f904
Tom Crowley <tom@tom-crowley.co.uk>
2017-06-25 14:11:55 +0100

    Move some node functions to the bspNode source file.

--- tools/LevelBuilder/bspNode.cpp
+++ tools/LevelBuilder/bspNode.cpp
@@ -4,6 +4,21 @@
 
 X_NAMESPACE_BEGIN(lvl)
 
+bspNode::bspNode() :
+	brushes(g_arena)
+{
+	planenum = 0;
+	parent = nullptr;
+
+	children[Side::FRONT] = nullptr;
+	children[Side::BACK] = nullptr;
+	nodeNumber = 0;
+
+	portals = nullptr;
+	opaque = false;
+	area = -1;
+	occupied = 0;
+}
 void bspNode::MakeTreePortals_r(XPlaneSet& planeSet)
 {
 	X_ASSERT_NOT_NULL(this);
@@ -547,5 +562,69 @@ int32_t bspNode::NumChildNodes_r(bspNode* pNode)
 	return num;
 }
 
+
+void bspNode::TreePrint_r(const XPlaneSet& planes, size_t depth) const
+{
+	X_LOG0("bspNode", "==========================");
+	X_LOG0("bspNode", "Depth: ^6%i", depth);
+
+
+	// is this a leaf node.
+	if (planenum == PLANENUM_LEAF)
+	{
+		X_LOG0("bspNode", "^1LEAF");
+	}
+
+	X_LOG0("bspNode", "this: %p", this);
+	X_LOG0("bspNode", "Opaque: %i", opaque);
+	X_LOG0("bspNode", "Occupied: %" PRIi32, occupied);
+	X_LOG0("bspNode", "Area: %" PRIi32, area);
+	X_LOG0("bspNode", "Portals: %p", portals);
+
+
+	if (planenum == PLANENUM_LEAF)
+	{
+		X_ASSERT_NOT_NULL(portals);
+
+		AABB::StrBuf boundsStr;
+		X_LOG0("bspNode", "bounds: %s", bounds.toString(boundsStr));
+
+		int32_t side;
+		for (auto* p = portals; p; p = p->next[side])
+		{
+			if (p->nodes[Side::FRONT] == this) {
+				side = 0;
+			}
+			else if (p->nodes[Side::BACK] == this) {
+				side = 1;
+			}
+
+			X_LOG0("bspNode", "	portal: on: %p node-f: %p node-b: %p", p->onNode, p->nodes[Side::FRONT], p->nodes[Side::BACK]);
+		}
+
+		X_LOG0("bspNode", "==========================");
+		return;
+	}
+	else
+	{
+		const Planef& plane = planes[this->planenum];
+		const Vec3f& Pn = plane.getNormal();
+
+		X_LOG0("bspNode", "PlaneNum: %i Plane: (%g,%g,%g) %g",
+			this->planenum, Pn[0], Pn[1], Pn[2], plane.getDistance());
+	}
+
+	// if it's not a leaf node then we should not have any null children.
+	X_ASSERT_NOT_NULL(children[Side::FRONT]);
+	X_ASSERT_NOT_NULL(children[Side::BACK]);
+
+	// print the children.	
+	depth++;
+
+	X_LOG0("bspNode", "==========================");
+
+	children[Side::FRONT]->TreePrint_r(planes, depth);
+	children[Side::BACK]->TreePrint_r(planes, depth);
+}
 
 X_NAMESPACE_END
