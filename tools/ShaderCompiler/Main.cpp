f0c3df68042678df4eed6d6e5b339bcd3a51650b
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-02 23:42:55 +0100

    Need a thread safe arena for the shaderCompiler as it's setup to be thread safe for game runtime.

--- tools/ShaderCompiler/Main.cpp
+++ tools/ShaderCompiler/Main.cpp
@@ -31,7 +31,7 @@ namespace
 
 	typedef core::MemoryArena<
 		core::MallocFreeAllocator,
-		core::SingleThreadPolicy,
+		core::MultiThreadPolicy<core::Spinlock>,
 #if X_ENABLE_MEMORY_DEBUG_POLICIES
 		core::SimpleBoundsChecking,
 		core::SimpleMemoryTracking,
