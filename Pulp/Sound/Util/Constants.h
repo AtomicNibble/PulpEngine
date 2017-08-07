c77876ff21cac7b89b22b91b67112d2155d0a09a
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-07 21:32:14 +0100

    Correct global game object id.

--- Pulp/Sound/Util/Constants.h
+++ Pulp/Sound/Util/Constants.h
@@ -11,7 +11,7 @@ X_NAMESPACE_BEGIN(sound)
 
 
 
-static const AkGameObjectID GLOBAL_GAME_OBJECT_ID = static_cast<AkGameObjectID>(-1);
+static const AkGameObjectID GLOBAL_GAME_OBJECT_ID = static_cast<AkGameObjectID>(-2);
 
 
 X_NAMESPACE_END
