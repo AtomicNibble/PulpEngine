85b426a45d928b4a3ecedff52bfec39443987478
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-08 22:45:33 +0100

    Updated ID file.

--- Pulp/Sound/IDs/Wwise_IDs.h
+++ Pulp/Sound/IDs/Wwise_IDs.h
@@ -13,8 +13,61 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PAIN = 1996139369U;
         static const AkUniqueID PLAY_AMBIENT = 1562304622U;
+        static const AkUniqueID PLAYER_BRAG = 3671645601U;
+        static const AkUniqueID PLAYER_FOOTSTEP = 2453392179U;
+        static const AkUniqueID PLAYER_JUMP = 1305133589U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BRICK = 504532776U;
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID CERAMIC = 1968058251U;
+                static const AkUniqueID CLOTH = 1367384683U;
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID FLESH = 1153642577U;
+                static const AkUniqueID FOLIAGE = 1162469934U;
+                static const AkUniqueID GLASS = 2449969375U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID ICE = 344481046U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID METAL_HOLLOW = 2523342644U;
+                static const AkUniqueID METAL_THIN = 1949087672U;
+                static const AkUniqueID MUD = 712897245U;
+                static const AkUniqueID PAPER = 3126687001U;
+                static const AkUniqueID PLASTER = 593724830U;
+                static const AkUniqueID PLASTIC = 660835419U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID RUBBER = 437659151U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID SNOW = 787898836U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+        namespace PLAYERFOOT
+        {
+            static const AkUniqueID GROUP = 2664667690U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LEFT = 4109362U;
+                static const AkUniqueID RIGHT = 3893817417U;
+            } // namespace SWITCH
+        } // namespace PLAYERFOOT
+
+    } // namespace SWITCHES
 
     namespace GAME_PARAMETERS
     {
@@ -27,7 +80,8 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID PLAYERSOUNDS = 1327972334U;
     } // namespace BANKS
 
     namespace BUSSES
