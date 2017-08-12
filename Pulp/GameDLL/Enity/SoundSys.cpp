121939bc91791ad1363adcb5aa90694c3940da8d
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-12 14:48:32 +0100

    Add a sound system, that will just update the position of any sound object with the sound system.

---
+++ Pulp/GameDLL/Enity/SoundSys.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "SoundSys.h"
+
+X_NAMESPACE_BEGIN(game)
+
+namespace entity
+{
+
+	SoundSystem::SoundSystem()
+	{
+		pSound_ = gEnv->pSound;
+	}
+
+	bool SoundSystem::init(void)
+	{
+
+
+		return true;
+	}
+
+
+
+	void SoundSystem::update(core::FrameData& frame, EnitiyRegister& reg)
+	{
+		X_UNUSED(frame);
+
+		auto view = reg.view<SoundObject, TransForm>();
+		for (auto entity : view)
+		{
+			auto& snd = reg.get<SoundObject>(entity);
+			auto& trans = reg.get<TransForm>(entity);
+
+			pSound_->SetPosition(reinterpret_cast<sound::GameObjectID>(&snd), trans);
+		}
+	}
+
+
+}
+
+X_NAMESPACE_END
---
+++ Pulp/GameDLL/Enity/SoundSys.h
@@ -0,0 +1,28 @@
+#pragma once
+
+X_NAMESPACE_DECLARE(core,
+	struct FrameTimeData;
+)
+
+X_NAMESPACE_BEGIN(game)
+
+namespace entity
+{
+
+	class SoundSystem
+	{
+	public:
+		SoundSystem();
+
+		bool init(void);
+		void update(core::FrameData& frame, EnitiyRegister& reg);
+
+
+	private:
+		sound::ISound* pSound_;
+	};
+
+
+}
+
+X_NAMESPACE_END
