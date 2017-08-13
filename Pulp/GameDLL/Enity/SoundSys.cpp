2b2ae6d0440371b2fd8f759e3625c90af25dbab2
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-13 17:39:39 +0100

    Use the new handle in SetPosition.

--- Pulp/GameDLL/Enity/SoundSys.cpp
+++ Pulp/GameDLL/Enity/SoundSys.cpp
@@ -30,7 +30,7 @@ namespace entity
 			auto& snd = reg.get<SoundObject>(entity);
 			auto& trans = reg.get<TransForm>(entity);
 
-			pSound_->setPosition(reinterpret_cast<sound::GameObjectID>(&snd), trans);
+			pSound_->setPosition(snd.handle, trans);
 		}
 	}
 
