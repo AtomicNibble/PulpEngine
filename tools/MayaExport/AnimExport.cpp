9187ceca875d87a7b4849b46f993693c4fd98437
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-20 15:47:17 +0100

    Organise order of code a bit.

--- tools/MayaExport/AnimExport.cpp
+++ tools/MayaExport/AnimExport.cpp
@@ -56,271 +56,6 @@ PotatoAnimExporter::~PotatoAnimExporter()
 }
 
 
-MStatus PotatoAnimExporter::getInputObjects(void)
-{
-	// we just want to load all nodes with animations
-	uint32_t x;
-	MStatus status = MS::kSuccess;
-
-	MDagPathArray pathArry;
-	MSelectionList list;
-	MStringArray nameArr;
-
-	nodes_.split(' ', nameArr);
-
-	for (x = 0; x < nameArr.length(); x++) {
-		list.add(nameArr[x]);
-	}
-
-	for (x = 0; x < list.length(); x++)
-	{
-		MDagPath path;
-		status = list.getDagPath(x, path);
-		if (status) {
-			pathArry.append(path);
-		}
-		else {
-			MayaUtil::MayaPrintError("getDagPath failed: %s", status.errorString().asChar());
-			return MS::kFailure;
-		}
-	}
-
-
-	if (pathArry.length() < 1) {
-		return MS::kFailure;
-	}
-
-	exportObjects_ = pathArry;
-	return MS::kSuccess;
-}
-
-MStatus PotatoAnimExporter::getExportObjects(void)
-{
-	PROFILE_MAYA("get objects");
-
-	return getInputObjects();
-}
-
-MStatus PotatoAnimExporter::loadBones(void)
-{
-	MDagPath jointDag;
-
-	uint32_t num = exportObjects_.length();
-	size_t numFrames = getNumFrames();
-
-	bones_.reserve(num);
-
-	for (uint32_t i = 0; i < num; i++)
-	{
-		jointDag = exportObjects_[i];
-
-		if (jointDag.hasFn(MFn::kJoint))
-		{
-			MFnDagNode node(jointDag);
-			
-			MayaUtil::MayaPrintMsg("Bone(%" PRIu32 ") name: %s", i, node.name().asChar());
-
-			Bone& bone = bones_.AddOne();
-			bone.dag = jointDag;
-			bone.name = MayaUtil::RemoveNameSpace(node.name());
-			bone.data.reserve(numFrames);
-		}
-		else
-		{
-			MayaUtil::MayaPrintMsg("Not a valid joint: %" PRIu32, i);
-		}
-	}
-
-	return MS::kSuccess;
-}
-
-MStatus PotatoAnimExporter::getAnimationData(void)
-{
-	PROFILE_MAYA_NAME("getAnimationData:");
-
-	MTime time;
-
-	// for each frame load anim data.
-	int32_t curFrame = startFrame_;
-
-	MayaUtil::MayaPrintMsg("Loading frames: %" PRIi32 " - %" PRIi32, startFrame_, endFrame_);
-
-	while (curFrame <= endFrame_)
-	{
-		time.setValue(curFrame);
-		MAnimControl::setCurrentTime(time);
-
-		MayaUtil::MayaPrintVerbose("Loading frame: %" PRIi32, curFrame);
-		{
-			MVector position;
-
-			size_t i, num = bones_.size();
-			for (i = 0; i < num; i++)
-			{
-				Bone& bone = bones_[i];
-
-				MayaUtil::MayaPrintVerbose("Bone(%" PRIuS "): %s", i, bone.name.asChar());
-
-
-				MTransformationMatrix worldMatrix = bone.dag.inclusiveMatrix();
-				FrameData& data = bone.data.AddOne();
-
-				data.position.x = worldMatrix.asMatrix()(3, 0);
-				data.position.y = worldMatrix.asMatrix()(3, 1);
-				data.position.z = worldMatrix.asMatrix()(3, 2);
-
-				// rotation.
-				double qx, qy, qz, qw;
-				worldMatrix.getRotationQuaternion(qx, qy, qz, qw);
-
-				data.rotation = MQuaternion(qx, qy, qz, qw);
-
-				data.scale = MVector::one;
-
-
-				if(MayaUtil::IsVerbose())
-				{
-					MEulerRotation euler = data.rotation.asEulerRotation();
-					const MQuaternion& q = data.rotation;
-
-					MayaUtil::MayaPrintVerbose("pos: (%g,%g,%g)",
-						data.position.x, data.position.y, data.position.z);
-					MayaUtil::MayaPrintVerbose("ang: (%g,%g,%g) quat: (%g,%g,%g,%g)",
-						::toDegrees(euler.x), ::toDegrees(euler.y), ::toDegrees(euler.z),
-						q.x, q.y, q.z, q.w);
-				}
-			}
-		}
-		MayaUtil::IncProcess();
-		MayaUtil::MayaPrintVerbose("=========================");
-
-		curFrame++;
-	}
-
-	return MS::kSuccess;
-}
-
-MStatus PotatoAnimExporter::writeIntermidiate_int(core::ByteStream& stream)
-{
-	// I will store each tags data.
-	// for each frame there will be a position and a quat.
-	const int32_t numBones = safe_static_cast<int32_t>(bones_.size());
-	const int32_t numFrames = getNumFrames();
-	const int32_t fps = static_cast<int32_t>(fps_);
-
-	MString cuirrentFile = MFileIO::currentFile();
-
-	// work out max possible size bufffer needed.
-	const size_t maxFloatBytes = 24; // -6.4
-	const size_t numFloatsPerEntry = 3 + 3 + 4;
-	const size_t tagSizes = sizeof("POS ") + sizeof("SCALE ") + sizeof("ANG ");
-	const size_t paddingSize = 10 * 6; // more than needed 
-	const size_t maxSizePerEntry = (maxFloatBytes * numFloatsPerEntry) +
-		tagSizes + paddingSize;
-
-	const size_t headerSize = 8096; // plenty for shiz
-	const size_t requiredSize = ((maxSizePerEntry * numFrames) * numBones) + headerSize;
-
-	stream.reset();
-	stream.reserve(requiredSize);
-
-	core::StackString<4096> buf;
-	buf.clear();
-	buf.appendFmt("// " X_ENGINE_NAME " intermidiate animation format\n");
-	buf.appendFmt("// Source: \"%s\"\n", cuirrentFile.asChar());
-	buf.appendFmt("// TimeLine range: %" PRIi32 " <-> %" PRIi32 "\n", startFrame_, endFrame_);
-	buf.appendFmt("\n");
-	buf.appendFmt("VERSION %" PRIu32 "\n", anim::ANIM_INTER_VERSION);
-	buf.appendFmt("BONES %" PRIi32 "\n", numBones);
-	buf.appendFmt("FRAMES %" PRIi32 "\n", numFrames);
-	buf.appendFmt("FPS %" PRIi32 "\n", fps);
-	buf.appendFmt("\n");
-
-	// list the bones.
-	for (const auto& bone : bones_) {
-		const char* pName = bone.name.asChar();
-		buf.appendFmt("BONE \"%s\"\n", pName);
-	}
-
-	buf.append("\n");
-
-	stream.write(buf.c_str(), buf.length());
-	buf.clear();
-
-	// write each bones data.
-	for (const auto& bone : bones_)
-	{
-		buf.append("BONE_DATA\n");
-		stream.write(buf.c_str(), buf.length());
-
-		for (int32_t i = 0; i < numFrames; i++)
-		{
-			const FrameData& data = bone.data[i];
-
-			buf.clear();
-			buf.append("POS ");
-			buf.appendFmt("( %f %f %f )\n",
-				data.position.x,
-				data.position.y,
-				data.position.z);
-
-			buf.append("SCALE ");
-			buf.appendFmt("( %.4g %.4g %.4g )\n",
-				data.scale.x,
-				data.scale.y,
-				data.scale.z);
-
-			buf.append("ANG ");
-			buf.appendFmt("( %.8g %.8g %.4g %.8g )\n",
-				data.rotation.x,
-				data.rotation.y,
-				data.rotation.z,
-				data.rotation.w);
-
-			buf.append("\n");
-
-			stream.write(buf.c_str(), buf.length());
-		}
-
-		MayaUtil::IncProcess();
-	}
-
-	return MS::kSuccess;
-}
-
-void PotatoAnimExporter::setFileName(const MString& path)
-{
-	// we don't replace seperators on the name as asset names 
-	// have a fixed slash, regardless of native slash of the platform.
-	// so we replace slashes only when building file paths.
-
-	fileName_.set(path.asChar());
-	fileName_.trim();
-	fileName_.setExtension(anim::ANIM_FILE_EXTENSION);
-}
-
-void PotatoAnimExporter::setOutdir(const MString& path)
-{
-	outDir_.set(path.asChar());
-	outDir_.trim();
-	outDir_.replaceSeprators();
-}
-
-core::Path<char> PotatoAnimExporter::getFilePath(void) const
-{
-	core::Path<char> path(outDir_);
-	path /= fileName_;
-	path.replaceSeprators();
-	return path;
-}
-
-
-core::string PotatoAnimExporter::getName(void) const
-{
-	return core::string(fileName_.begin(), fileName_.end());
-}
-
-
 MStatus PotatoAnimExporter::convert(const MArgList &args)
 {
 	MStatus status = processArgs(args);
@@ -334,7 +69,7 @@ MStatus PotatoAnimExporter::convert(const MArgList &args)
 	MayaUtil::MayaPrintMsg("Frames -> start: %" PRIi32 " end: %" PRIi32 " num: %" PRIi32, startFrame_, endFrame_, numFrames);
 	MayaUtil::MayaPrintMsg(""); // new line
 
-	// print the error after printing info about frames and the name.
+								// print the error after printing info about frames and the name.
 	if (numFrames < 1) {
 		MayaUtil::MayaPrintError("Can't export a animation with less than one frame");
 		return MS::kFailure;
@@ -504,6 +239,274 @@ MStatus PotatoAnimExporter::convert(const MArgList &args)
 	return status;
 }
 
+
+void PotatoAnimExporter::setFileName(const MString& path)
+{
+	// we don't replace seperators on the name as asset names 
+	// have a fixed slash, regardless of native slash of the platform.
+	// so we replace slashes only when building file paths.
+
+	fileName_.set(path.asChar());
+	fileName_.trim();
+	fileName_.setExtension(anim::ANIM_FILE_EXTENSION);
+}
+
+void PotatoAnimExporter::setOutdir(const MString& path)
+{
+	outDir_.set(path.asChar());
+	outDir_.trim();
+	outDir_.replaceSeprators();
+}
+
+core::Path<char> PotatoAnimExporter::getFilePath(void) const
+{
+	core::Path<char> path(outDir_);
+	path /= fileName_;
+	path.replaceSeprators();
+	return path;
+}
+
+
+core::string PotatoAnimExporter::getName(void) const
+{
+	return core::string(fileName_.begin(), fileName_.end());
+}
+
+
+
+MStatus PotatoAnimExporter::getInputObjects(void)
+{
+	// we just want to load all nodes with animations
+	uint32_t x;
+	MStatus status = MS::kSuccess;
+
+	MDagPathArray pathArry;
+	MSelectionList list;
+	MStringArray nameArr;
+
+	nodes_.split(' ', nameArr);
+
+	for (x = 0; x < nameArr.length(); x++) {
+		list.add(nameArr[x]);
+	}
+
+	for (x = 0; x < list.length(); x++)
+	{
+		MDagPath path;
+		status = list.getDagPath(x, path);
+		if (status) {
+			pathArry.append(path);
+		}
+		else {
+			MayaUtil::MayaPrintError("getDagPath failed: %s", status.errorString().asChar());
+			return MS::kFailure;
+		}
+	}
+
+
+	if (pathArry.length() < 1) {
+		return MS::kFailure;
+	}
+
+	exportObjects_ = pathArry;
+	return MS::kSuccess;
+}
+
+MStatus PotatoAnimExporter::getExportObjects(void)
+{
+	PROFILE_MAYA("get objects");
+
+	return getInputObjects();
+}
+
+MStatus PotatoAnimExporter::loadBones(void)
+{
+	MDagPath jointDag;
+
+	uint32_t num = exportObjects_.length();
+	size_t numFrames = getNumFrames();
+
+	bones_.reserve(num);
+
+	for (uint32_t i = 0; i < num; i++)
+	{
+		jointDag = exportObjects_[i];
+
+		if (jointDag.hasFn(MFn::kJoint))
+		{
+			MFnDagNode node(jointDag);
+			
+			MayaUtil::MayaPrintMsg("Bone(%" PRIu32 ") name: %s", i, node.name().asChar());
+
+			Bone& bone = bones_.AddOne();
+			bone.dag = jointDag;
+			bone.name = MayaUtil::RemoveNameSpace(node.name());
+			bone.data.reserve(numFrames);
+		}
+		else
+		{
+			MayaUtil::MayaPrintMsg("Not a valid joint: %" PRIu32, i);
+		}
+	}
+
+	return MS::kSuccess;
+}
+
+MStatus PotatoAnimExporter::getAnimationData(void)
+{
+	PROFILE_MAYA_NAME("getAnimationData:");
+
+	MTime time;
+
+	// for each frame load anim data.
+	int32_t curFrame = startFrame_;
+
+	MayaUtil::MayaPrintMsg("Loading frames: %" PRIi32 " - %" PRIi32, startFrame_, endFrame_);
+
+	while (curFrame <= endFrame_)
+	{
+		time.setValue(curFrame);
+		MAnimControl::setCurrentTime(time);
+
+		MayaUtil::MayaPrintVerbose("Loading frame: %" PRIi32, curFrame);
+		{
+			MVector position;
+
+			size_t i, num = bones_.size();
+			for (i = 0; i < num; i++)
+			{
+				Bone& bone = bones_[i];
+
+				MayaUtil::MayaPrintVerbose("Bone(%" PRIuS "): %s", i, bone.name.asChar());
+
+
+				MTransformationMatrix worldMatrix = bone.dag.inclusiveMatrix();
+				FrameData& data = bone.data.AddOne();
+
+				data.position.x = worldMatrix.asMatrix()(3, 0);
+				data.position.y = worldMatrix.asMatrix()(3, 1);
+				data.position.z = worldMatrix.asMatrix()(3, 2);
+
+				// rotation.
+				double qx, qy, qz, qw;
+				worldMatrix.getRotationQuaternion(qx, qy, qz, qw);
+
+				data.rotation = MQuaternion(qx, qy, qz, qw);
+
+				data.scale = MVector::one;
+
+
+				if(MayaUtil::IsVerbose())
+				{
+					MEulerRotation euler = data.rotation.asEulerRotation();
+					const MQuaternion& q = data.rotation;
+
+					MayaUtil::MayaPrintVerbose("pos: (%g,%g,%g)",
+						data.position.x, data.position.y, data.position.z);
+					MayaUtil::MayaPrintVerbose("ang: (%g,%g,%g) quat: (%g,%g,%g,%g)",
+						::toDegrees(euler.x), ::toDegrees(euler.y), ::toDegrees(euler.z),
+						q.x, q.y, q.z, q.w);
+				}
+			}
+		}
+		MayaUtil::IncProcess();
+		MayaUtil::MayaPrintVerbose("=========================");
+
+		curFrame++;
+	}
+
+	return MS::kSuccess;
+}
+
+MStatus PotatoAnimExporter::writeIntermidiate_int(core::ByteStream& stream)
+{
+	// I will store each tags data.
+	// for each frame there will be a position and a quat.
+	const int32_t numBones = safe_static_cast<int32_t>(bones_.size());
+	const int32_t numFrames = getNumFrames();
+	const int32_t fps = static_cast<int32_t>(fps_);
+
+	MString currentFile = MFileIO::currentFile();
+
+	// work out max possible size bufffer needed.
+	const size_t maxFloatBytes = 24; // -6.4
+	const size_t numFloatsPerEntry = 3 + 3 + 4;
+	const size_t tagSizes = sizeof("POS ") + sizeof("SCALE ") + sizeof("ANG ");
+	const size_t paddingSize = 10 * 6; // more than needed 
+	const size_t maxSizePerEntry = (maxFloatBytes * numFloatsPerEntry) +
+		tagSizes + paddingSize;
+
+	const size_t headerSize = 8096; // plenty for shiz
+	const size_t requiredSize = ((maxSizePerEntry * numFrames) * numBones) + headerSize;
+
+	stream.reset();
+	stream.reserve(requiredSize);
+
+	core::StackString<4096> buf;
+	buf.clear();
+	buf.appendFmt("// " X_ENGINE_NAME " intermidiate animation format\n");
+	buf.appendFmt("// Source: \"%s\"\n", currentFile.asChar());
+	buf.appendFmt("// TimeLine range: %" PRIi32 " <-> %" PRIi32 "\n", startFrame_, endFrame_);
+	buf.appendFmt("\n");
+	buf.appendFmt("VERSION %" PRIu32 "\n", anim::ANIM_INTER_VERSION);
+	buf.appendFmt("BONES %" PRIi32 "\n", numBones);
+	buf.appendFmt("FRAMES %" PRIi32 "\n", numFrames);
+	buf.appendFmt("FPS %" PRIi32 "\n", fps);
+	buf.appendFmt("\n");
+
+	// list the bones.
+	for (const auto& bone : bones_) {
+		const char* pName = bone.name.asChar();
+		buf.appendFmt("BONE \"%s\"\n", pName);
+	}
+
+	buf.append("\n");
+
+	stream.write(buf.c_str(), buf.length());
+	buf.clear();
+
+	// write each bones data.
+	for (const auto& bone : bones_)
+	{
+		buf.append("BONE_DATA\n");
+		stream.write(buf.c_str(), buf.length());
+
+		for (int32_t i = 0; i < numFrames; i++)
+		{
+			const FrameData& data = bone.data[i];
+
+			buf.clear();
+			buf.append("POS ");
+			buf.appendFmt("( %f %f %f )\n",
+				data.position.x,
+				data.position.y,
+				data.position.z);
+
+			buf.append("SCALE ");
+			buf.appendFmt("( %.4g %.4g %.4g )\n",
+				data.scale.x,
+				data.scale.y,
+				data.scale.z);
+
+			buf.append("ANG ");
+			buf.appendFmt("( %.8g %.8g %.4g %.8g )\n",
+				data.rotation.x,
+				data.rotation.y,
+				data.rotation.z,
+				data.rotation.w);
+
+			buf.append("\n");
+
+			stream.write(buf.c_str(), buf.length());
+		}
+
+		MayaUtil::IncProcess();
+	}
+
+	return MS::kSuccess;
+}
+
+
 MStatus PotatoAnimExporter::processArgs(const MArgList &args)
 {
 	MStatus status = MS::kFailure;
@@ -644,24 +647,6 @@ MStatus PotatoAnimExporter::processArgs(const MArgList &args)
 
 	MayaUtil::SetProgressCtrl(progressCntl);
 
-
-#if 0
-	if (Mfilename.length() > 0) 
-	{
-		fileName_.set(Mfilename.asChar());
-		filePath_.append(Mfilename.asChar());
-
-		if (intermidiate_) {
-			filePath_.setExtension(anim::ANIM_INTER_FILE_EXTENSION);
-		}
-		else {
-			filePath_.setExtension(anim::ANIM_FILE_EXTENSION);
-		}
-
-		status = MS::kSuccess;
-	}
-#endif
-
 	return status;
 }
 
@@ -740,7 +725,7 @@ AnimExporterCmd::~AnimExporterCmd()
 
 MStatus AnimExporterCmd::doIt(const MArgList &args)
 {
-	PotatoAnimExporter animExport;
+	PotatoAnimExporter animExport(g_arena);
 
 	return animExport.convert(args);
 }
