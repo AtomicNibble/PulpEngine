4a5881edb1d409d13426453ffb2f120095ac8244
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-21 22:21:46 +0100

    Make the techsetdef shader support instancing.

--- game_folder/core_assets/shaders/techsetdef_unlit.hlsl
+++ game_folder/core_assets/shaders/techsetdef_unlit.hlsl
@@ -1,4 +1,3 @@
-
 #include "Vertexbase.inc"
 
 struct VS_INPUT
@@ -11,6 +10,11 @@ struct VS_INPUT
 
   float4 color                  : COLOR0;
   float4 normal                 : NORMAL0;
+
+#if X_HWSKIN
+  int4 boneIndexes : BLENDINDICES0;
+  float4 weights : BLENDWEIGHT0;
+#endif
 };
 
 struct VS_OUTPUT
@@ -18,13 +22,6 @@ struct VS_OUTPUT
     float4 ssPosition               : SV_POSITION;
     float2 texCoord               : TEXCOORD0;
     float4 color                    : COLOR;
-};
-
-struct PS_INPUT
-{
-    float4 ssPosition               : SV_POSITION;
-    float2 texCoord               : TEXCOORD0;
-    float4 color                    : COLOR0;
 };
 
 struct PS_OUTPUT
@@ -35,18 +32,37 @@ struct PS_OUTPUT
 Texture2D          diffuse : register(t0);
 SamplerState        diffuseSampler;
 
+#if X_HWSKIN
+StructuredBuffer<float4x4> BoneMatrices;
+#endif // !X_HWSKIN
 
 VS_OUTPUT vs_main( VS_INPUT IN )
 {
   VS_OUTPUT OUT;
-  float4 worldPosition = mul( float4(IN.osPosition, 1.0), worldMatrix );
+
+    float4 pos = float4(IN.osPosition, 1.0);
+
+#if X_HWSKIN
+    float4 v = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+    for(int i = 0; i < 4; i++)
+    {
+      //Multiply position by bone matrix
+      v += IN.weights * mul(pos, BoneMatrices[IN.boneIndexes[i]]);
+    }
+
+    pos += v;
+
+#endif // !X_HWSKIN
+
+  float4 worldPosition = mul( pos, worldMatrix );
   OUT.ssPosition = mul( worldPosition, worldToScreenMatrix );
   OUT.texCoord = IN.tex;
   OUT.color =  IN.color;
   return OUT;
 }
 
-PS_OUTPUT ps_main( PS_INPUT IN )
+PS_OUTPUT ps_main( VS_OUTPUT IN )
 {
     PS_OUTPUT output;
     float4 texCol = diffuse.Sample(diffuseSampler, IN.texCoord);
