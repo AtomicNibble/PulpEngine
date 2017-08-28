3f451839483f835c29222f3656155e6354ef0b47
Tom Crowley <tom@tom-crowley.co.uk>
2017-08-28 23:38:10 +0100

    Cleanup.

--- Pulp/Common/Debugging/Assert.h
+++ Pulp/Common/Debugging/Assert.h
@@ -7,44 +7,6 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup Debugging
-/// \class Assert
-/// \brief Provides an improved assertion facility.
-/// \details There are a few bits missing in the standard assert(), such as outputting a formatted message to the user,
-/// halting the debugger at the exact line the assertion statement was written, and outputting individual variables
-/// (and their values) used in the assertion condition.
-///
-/// This custom assert implementation tackles the above-mentioned shortcomings by implementing an assert class
-/// in conjunction with custom macros. The assert class itself is responsible for forwarding source-code info
-/// (such as file name, line number and function name) and the formatted message to all registered assertion handlers
-/// and loggers. The macros allow users to write custom assert statements in a clear and concise way, not having to
-/// deal with internal implementation details.
-///
-/// Individual assertion handlers are registered/unregistered at the assertionDispatch, which takes care of forwarding
-/// asserts being fired to all registered handlers. Similarly, all fired assertions will also be forwarded to the
-/// registered loggers using the logDispatch.
-///
-/// Internally, the assertion facility builds upon the C++ idiom of creating a temporary object, calling an infinite
-/// number of methods on this object, as in the following example:
-/// \code
-///   Assert(sourceInfo, "Custom assert message involving two variables").Variable("var1", var1).Variable("var2", var2);
-/// \endcode
-/// Because a call to Assert::Variable() returns a reference to itself, any number of calls to this method can be chained
-/// together. Each call to Assert::Variable() will dispatch the call to all registered assertion handlers and loggers,
-/// which in turn can e.g. output the variable's name and its value to the console, to a file, etc.
-///
-/// The assert supports dispatching variables of all built-in types, as well as custom user types. User types are
-/// supported by providing a template specialization to the following method:
-/// \code
-///   template <typename T>
-///   Assert& Variable(const char* const name, const T& var);
-/// \endcode
-/// For a thorough explanation of how the assertion system works internally,
-/// see http://www.altdevblogaday.com/2011/10/12/upgrading-assert-using-the-preprocessor/.
-/// \remark This class is not intended to be instantiated by the user, but is used by the \ref X_ASSERT macro, and other
-/// convenience macros for asserting certain conditions. The user should always use one of the available \ref X_ASSERT
-/// macros.
-/// \sa assertionDispatch logDispatch X_ASSERT X_ASSERT_NOT_NULL X_ASSERT_UNREACHABLE X_ASSERT_NOT_IMPLEMENTED X_ASSERT_ALIGNMENT
 class Assert
 {
 public:
