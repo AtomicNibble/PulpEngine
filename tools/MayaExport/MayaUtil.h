41cc7a4b76b6e3a45be691dabb9310aa6c44a30a
Tom Crowley <tom@tom-crowley.co.uk>
2017-09-09 17:42:29 +0100

    Use the row col operator.

--- tools/MayaExport/MayaUtil.h
+++ tools/MayaExport/MayaUtil.h
@@ -80,9 +80,10 @@ namespace MayaUtil
 		int		j, k;
 		Matrix33f	mat;
 
+
 		for (j = 0; j < 3; j++) {
 			for (k = 0; k < 3; k++) {
-				mat.at(j, k) = static_cast<float>(matrix[j][k]);
+				mat.at(j, k) = static_cast<float>(matrix(j,k));
 			}
 		}
 
