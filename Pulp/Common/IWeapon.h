721f6a5cf199544e64451f73ff8b6309ba67629f
Tom Crowley <tom@tom-crowley.co.uk>
2017-09-18 23:02:28 +0100

    Add some basicweapon defines, not decided how i'm goning to store / load weapons yet.

--- Pulp/Common/IWeapon.h
+++ Pulp/Common/IWeapon.h
@@ -3,6 +3,72 @@
 X_NAMESPACE_BEGIN(game)
 
 
+X_DECLARE_ENUM(WeaponClass)(
+	Pistol,
+	Rifle,
+	Smg,
+	Mg
+);
+
+X_DECLARE_ENUM(InventoryType)(
+	Primary
+);
+
+X_DECLARE_ENUM(FireType)(
+	FullAuto,
+	Single,
+	Burst2,
+	Burst3
+);
+
+X_DECLARE_ENUM(AmmoCounterStyle)(
+	Magazine,
+	ShortMagazine,
+	ShotGun,
+	Rocket,
+	BeltFed
+);
+
+X_DECLARE_FLAGS(WeaponFlag)(
+	Ads,
+	AdsFire,
+	NoProne
+);
+
+
+struct WeaponDesc
+{
+	uint32_t minDmgRange;
+	uint32_t maxDmgRange;
+
+
+	uint16_t minDmg;
+	uint16_t maxDmg;
+	uint16_t meleeDmg;
+	uint16_t _pad;
+
+	// models
+	core::string gunModel;
+	core::string worldModel;
+
+	// animations
+	core::string animIdle;
+	core::string animFire;
+	core::string animRaise;
+	core::string animFirstRaise;
+	core::string animDrop;
+
+	// sounds.
+	core::string sndPickUp;
+	core::string sndAmmoPickUp;
+	core::string sndFire;
+	core::string sndLastShot;
+	core::string sndEmtpyFire;
+	core::string snd;
+
+	// hud
+	core::string hudIcon;
+};
 
 
 X_NAMESPACE_END
