185bc9fb443026eb4d5c2e63934b3fb4da6ad042
Tom Crowley <tom@tom-crowley.co.uk>
2017-10-06 20:58:17 +0100

    Remove the old BaseAsset / container.

--- Pulp/Common/Assets/AssetContainer.cpp
+++ Pulp/Common/Assets/AssetContainer.cpp
@@ -4,39 +4,7 @@
 
 X_NAMESPACE_BEGIN(core)
 
-const int XBaseAsset::release()
-{
-	--RefCount_;
-
-	int ref = RefCount_;
-
-	if (ref <= 0)
-	{
-		X_ASSERT_NOT_NULL(pContainer_);
-
-
-		pContainer_->removeAsset(this);
-		return 0;
-	}
-
-	return ref;
-}
-
-
-bool XResourceContainer::removeAsset(XBaseAsset* pAsset)
-{
-	X_ASSERT_NOT_NULL(pAsset);
-
-	if (list[pAsset->ID_] == pAsset)
-	{
-		list[pAsset->ID_] = nullptr;
-		hash.erase(pAsset->resourceName_);
-		return true;
-	}
-	return false;
-}
-
-
+X_DISABLE_EMPTY_FILE_WARNING
 
 
 X_NAMESPACE_END
