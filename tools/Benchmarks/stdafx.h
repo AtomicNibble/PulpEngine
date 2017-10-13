55d8201db3574c621b8fb3257c5cea4b913967e7
Tom Crowley <tom@tom-crowley.co.uk>
2017-10-13 19:39:25 +0100

    Updated basic memcpy benchmark.

--- tools/Benchmarks/stdafx.h
+++ tools/Benchmarks/stdafx.h
@@ -2,6 +2,7 @@
 
 #include <EngineCommon.h>
 
+#define BENCHMARK_HAS_CXX11
 #include <benchmark\benchmark.h>
 
 extern core::MemoryArenaBase* g_arena;
--- tools/Benchmarks/tests/memcpy.cpp
+++ tools/Benchmarks/tests/memcpy.cpp
@@ -2,20 +2,42 @@
 
 
 void BM_memcpy(benchmark::State& state) {
-
-	char* pSrc = new char[state.range(0)];
-	char* pDst = new char[state.range(0)];
+	
+	char* pSrc = X_NEW_ARRAY_ALIGNED(char, state.range(0), g_arena, "TestSrc", 64);
+	char* pDst = X_NEW_ARRAY_ALIGNED(char, state.range(0), g_arena, "TestSrc", 64);
 
 	std::memset(pSrc, 'x', state.range(0));
 
+	const size_t num = state.range(0);
+
 	while (state.KeepRunning()) {
-		std::memcpy(pDst, pSrc, state.range(0));
+		std::memcpy(pDst, pSrc, num);
 	}
 
-	state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) + static_cast<int64_t>(state.range(0)));
+	state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * static_cast<int64_t>(state.range(0)));
 
-	delete[] pSrc;
-	delete[] pDst;
+	X_DELETE_ARRAY(pSrc, g_arena);
+	X_DELETE_ARRAY(pDst, g_arena);
 }
 
-BENCHMARK(BM_memcpy)->RangeMultiplier(2)->Range(8, 8 << 10);
+void BM_memcpy_SIMD(benchmark::State& state) {
+
+	char* pSrc = X_NEW_ARRAY_ALIGNED(char, state.range(0), g_arena, "TestSrc", 64);
+	char* pDst = X_NEW_ARRAY_ALIGNED(char, state.range(0), g_arena, "TestSrc", 64);
+
+	std::memset(pSrc, 'x', state.range(0));
+
+	const size_t num = state.range(0) >> 4;
+
+	while (state.KeepRunning()) {
+		core::Mem::SIMDMemCopy(pDst, pSrc, num);
+	}
+
+	state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * static_cast<int64_t>(state.range(0)));
+
+	X_DELETE_ARRAY(pSrc, g_arena);
+	X_DELETE_ARRAY(pDst, g_arena);
+}
+
+BENCHMARK(BM_memcpy)->RangeMultiplier(2)->Range(8 << 20, 8 << 26);
+BENCHMARK(BM_memcpy_SIMD)->RangeMultiplier(2)->Range(8 << 20, 8 << 26);
