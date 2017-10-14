c5b23a5c0087ca0fff21afc2b1df0fdf7c2cb47a
Tom Crowley <tom@tom-crowley.co.uk>
2017-10-14 17:23:01 +0100

    Add some random number benchmarks.

---
+++ tools/Benchmarks/tests/random.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+
+#include <Random\MultiplyWithCarry.h>
+#include <Random\XorShift.h>
+
+void BM_rnd_rand(benchmark::State& state)
+{
+	while (state.KeepRunning()) {
+		benchmark::DoNotOptimize(rand());
+	}
+}
+
+void BM_rnd_multiplycarry(benchmark::State& state)
+{
+	core::random::MultiplyWithCarry mwc;
+
+	while (state.KeepRunning()) {
+		benchmark::DoNotOptimize(mwc.rand());
+	}
+}
+
+void BM_rnd_xorshift(benchmark::State& state)
+{
+	core::random::XorShift mwc;
+
+	while (state.KeepRunning()) {
+		benchmark::DoNotOptimize(mwc.rand());
+	}
+}
+
+void BM_rnd_xorshift128(benchmark::State& state)
+{
+	core::random::XorShift128 mwc;
+
+	while (state.KeepRunning()) {
+		benchmark::DoNotOptimize(mwc.rand());
+	}
+}
+
+BENCHMARK(BM_rnd_rand);
+BENCHMARK(BM_rnd_multiplycarry);
+BENCHMARK(BM_rnd_xorshift);
+BENCHMARK(BM_rnd_xorshift128);
