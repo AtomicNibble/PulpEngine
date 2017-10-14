d3607d43664dffe87738a27e3c44e4b4e60d4fcb
Tom Crowley <tom@tom-crowley.co.uk>
2017-10-14 16:43:24 +0100

    Make it so have memory tests to check arena overhead.

--- tools/Benchmarks/tests/allocators.cpp
+++ tools/Benchmarks/tests/allocators.cpp
@@ -1,10 +1,10 @@
 #include "stdafx.h"
 
 #include <Memory\AllocationPolicies\GrowingBlockAllocator.h>
+#include <Memory\SimpleMemoryArena.h>
 
-core::GrowingBlockAllocator allocator;
 
-void BM_mem_a_malloc(benchmark::State& state) {
+void BM_mem_malloc(benchmark::State& state) {
 
 	const size_t size = state.range(0);
 
@@ -14,23 +14,12 @@ void BM_mem_a_malloc(benchmark::State& state) {
 	}
 }
 
-void BM_mem_a_arena(benchmark::State& state) {
+
+void BM_mem_blockallocator(benchmark::State& state) {
 
 	const size_t size = state.range(0);
 
-	auto* pArena = g_arena;
-
-	while (state.KeepRunning()) {
-		char* pMem = X_NEW_ARRAY(char, size, pArena, "test");
-		X_NAMESPACE(core)::Mem::DeleteArray(pMem, pArena);
-	}
-}
-
-void BM_mem_a_allocator(benchmark::State& state) {
-
-	const size_t size = state.range(0);
-
-	auto* pArena = g_arena;
+	core::GrowingBlockAllocator allocator;
 
 	while (state.KeepRunning()) {
 		void* pMem = allocator.allocate(size, X_ALIGN_OF(char), 0);
@@ -38,7 +27,47 @@ void BM_mem_a_allocator(benchmark::State& state) {
 	}
 }
 
+void BM_mem_blockallocator_simplearena(benchmark::State& state) {
 
-// BENCHMARK(BM_mem_a_malloc)->RangeMultiplier(2)->Range(16, 1 << 16);
-BENCHMARK(BM_mem_a_arena)->RangeMultiplier(2)->Range(16, 1 << 16);
-BENCHMARK(BM_mem_a_allocator)->RangeMultiplier(2)->Range(16, 1 << 16);
+	const size_t size = state.range(0);
+
+	typedef core::SimpleMemoryArena<core::GrowingBlockAllocator> BlockArena;
+
+	BlockArena::AllocationPolicy allocator;
+	BlockArena arena(&allocator, "BenchmarkArena");
+
+	while (state.KeepRunning()) {
+		char* pMem = X_NEW_ARRAY(char, size, &arena, "test");
+		X_NAMESPACE(core)::Mem::DeleteArray(pMem, &arena);
+	}
+}
+
+void BM_mem_blockallocator_arena(benchmark::State& state) {
+
+	const size_t size = state.range(0);
+
+	typedef core::MemoryArena<
+		core::GrowingBlockAllocator,
+		core::SingleThreadPolicy,
+		core::NoBoundsChecking,
+		core::NoMemoryTracking,
+		core::NoMemoryTagging
+	> BlockArena;
+
+	BlockArena::AllocationPolicy allocator;
+	BlockArena arena(&allocator, "BenchmarkArena");
+
+	while (state.KeepRunning()) {
+		char* pMem = X_NEW_ARRAY(char, size, &arena, "test");
+		X_NAMESPACE(core)::Mem::DeleteArray(pMem, &arena);
+	}
+}
+
+// malloc speeds
+BENCHMARK(BM_mem_malloc)->RangeMultiplier(2)->Range(16, 1 << 16);
+
+// test the blockallocator to get base line, then see overhead of various arenas.
+BENCHMARK(BM_mem_blockallocator)->RangeMultiplier(2)->Range(16, 1 << 16);
+BENCHMARK(BM_mem_blockallocator_simplearena)->RangeMultiplier(2)->Range(16, 1 << 16);
+BENCHMARK(BM_mem_blockallocator_arena)->RangeMultiplier(2)->Range(16, 1 << 16);
+
