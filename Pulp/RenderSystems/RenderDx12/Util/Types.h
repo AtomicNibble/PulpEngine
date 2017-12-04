331a3e81e8a0e527ebcdc88f498b2f3851431103
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-04 21:32:22 +0000

    Add some internal render types

---
+++ Pulp/RenderSystems/RenderDx12/Util/Types.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+X_NAMESPACE_BEGIN(render)
+
+X_DECLARE_ENUM(DescriptorHeapType)(
+	CBV_SRV_UAV,
+	SAMPLER
+);
+
+static_assert(DescriptorHeapType::CBV_SRV_UAV == D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, "Value not match");
+static_assert(DescriptorHeapType::SAMPLER == D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER, "Value not match");
+
+
+
+X_NAMESPACE_END
