d75ebd7cdcf6f875dff2599d8699cbcc49c256d5
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-15 12:33:45 +0000

    Log format update

--- Pulp/Script/TableDump.cpp
+++ Pulp/Script/TableDump.cpp
@@ -17,13 +17,13 @@ XScriptTableDumpConsole::~XScriptTableDumpConsole()
 
 void XScriptTableDumpConsole::onElementFound(const char* pName, Type::Enum type)
 {
-	X_LOG0("Script", "%s %s", pName, Type::ToString(type));
+	X_LOG0("Script", "\"%s\" ^6%s", pName, Type::ToString(type));
 
 }
 
 void XScriptTableDumpConsole::onElementFound(int idx, Type::Enum type)
 {
-	X_LOG0("Script", "%i> %s", idx, Type::ToString(type));
+	X_LOG0("Script", "%i ^6%s", idx, Type::ToString(type));
 }
 
 
