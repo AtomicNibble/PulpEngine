9edf2f8f1cfbbcf86e9e6db487bbaf0afde24af6
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-24 16:23:58 +0000

    Add base Linker type to LinkerLib.

--- tools/LinkerLib/Linker.h
+++ tools/LinkerLib/Linker.h
@@ -1,1 +1,20 @@
 #pragma once
+
+X_NAMESPACE_BEGIN(linker)
+
+class Linker
+{
+
+public:
+	LINKERLIB_EXPORT Linker(core::MemoryArenaBase* scratchArea);
+	LINKERLIB_EXPORT ~Linker();
+
+	LINKERLIB_EXPORT void PrintBanner(void);
+	LINKERLIB_EXPORT bool Init(void);
+
+private:
+
+};
+
+
+X_NAMESPACE_END
