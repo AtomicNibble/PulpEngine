0d9b10ad2333aa49761f09204b266b4f26757914
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-25 13:20:01 +0000

    Make it so the AssetDb, knows all the file extensions.

---
+++ tools/AssetDB/AssetFileExtensions.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "AssetFileExtensions.h"
+
+#include <IModel.h>
+#include <IAnimation.h>
+#include <IMaterial.h>
+#include <ICi.h>
+#include <IVideo.h>
+#include <IWeapon.h>
+
+X_NAMESPACE_BEGIN(assetDb)
+
+namespace
+{
+	// provides a nicer way to set all the extensions, without having to remember the index
+	// each asset enum is.
+	// also is resilent to enum order changes.
+	class ExtensionHelper
+	{
+	public:
+		ExtensionHelper() 
+		{
+			extensions_.fill("");
+			extensions_[AssetType::MODEL] = model::MODEL_FILE_EXTENSION;
+			extensions_[AssetType::ANIM] = anim::ANIM_FILE_EXTENSION;
+			extensions_[AssetType::MATERIAL] = engine::MTL_B_FILE_EXTENSION;
+			extensions_[AssetType::IMG] = texture::CI_FILE_EXTENSION;
+			extensions_[AssetType::WEAPON] = game::weapon::WEAPON_FILE_EXTENSION;
+			extensions_[AssetType::VIDEO] = video::VIDEO_FILE_EXTENSION;
+		}
+
+		inline const char* operator[](AssetType::Enum type) const {
+			return extensions_[type];
+		}
+
+	private:
+		std::array<const char*, AssetType::ENUM_COUNT> extensions_;
+	};
+
+	const ExtensionHelper extensions;
+}
+
+const char* getAssetTypeExtension(AssetType::Enum type)
+{
+	return extensions[type];
+}
+
+X_NAMESPACE_END
---
+++ tools/AssetDB/AssetFileExtensions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <IAssetDb.h>
+
+
+X_NAMESPACE_BEGIN(assetDb)
+
+const char* getAssetTypeExtension(AssetType::Enum type);
+
+X_NAMESPACE_END
