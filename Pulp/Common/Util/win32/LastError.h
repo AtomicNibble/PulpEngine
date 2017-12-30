4c6c405990e1c1234c08d8ab4b50cff35af3a1a7
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-30 17:06:13 +0000

    Remove some over verbose comments.

--- Pulp/Common/Util/win32/LastError.h
+++ Pulp/Common/Util/win32/LastError.h
@@ -6,27 +6,14 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup Util
-/// \namespace lastError
-/// \brief Wraps Windows-specific GetLastError functionality.
-/// \details The functions in this namespace can be used to retrieve the last error set by certain Windows APIs, and
-/// additionally offers functionality for converting those error codes into human-readable strings.
 namespace lastError
 {
-	/// A character array used for holding a human-readable description of an error.
 	typedef char Description[512];
 
-	/// Returns the last error.
 	unsigned int Get(void);
 
-	/// \brief Converts any Windows-specific error code into a human-readable string, and returns a pointer to the description string.
-	/// \remark The function does not allocate memory for the string. The returned pointer merely points to the \a desc
-	/// character array so it can be used in other function calls.
 	const char* ToString(DWORD error, Description& desc);
 
-	/// \brief Converts the last error into a human-readable string, and returns a pointer to the description string.
-	/// \remark The function does not allocate memory for the string. The returned pointer merely points to the \a desc
-	/// character array so it can be used in other function calls.
 	const char* ToString(Description& desc);
 }
 
