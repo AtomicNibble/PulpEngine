6bd1791b384f810c4ecf30569a1a31b1ab29b0d5
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-30 22:37:00 +0000

    Bump the IO buffer size a bit.

--- Pulp/Video/XVideo.h
+++ Pulp/Video/XVideo.h
@@ -29,7 +29,7 @@ X_DECLARE_ENUM8(VideoState)(
 
 class Video : public core::AssetBase, public IVideo
 {
-	const size_t IO_BUFFER_SIZE = 1024 * 64;
+	const size_t IO_BUFFER_SIZE = 1024 * 128;
 	const size_t RING_BUFFER_SIZE = 1024 * 1024 * 1; // 1MB
 
 	typedef core::Array<uint8_t, core::ArrayAlignedAllocatorFixed<uint8_t, 64>, core::growStrat::Multiply> DataVec;
