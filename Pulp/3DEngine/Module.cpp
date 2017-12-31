274ec831aad1690ac51360d50d31c42296680c0f
Tom Crowley <tom@tom-crowley.co.uk>
2017-12-31 20:11:54 +0000

    use env instead of gEnv

--- Pulp/3DEngine/Module.cpp
+++ Pulp/3DEngine/Module.cpp
@@ -55,21 +55,21 @@ class XEngineModule_3DEngine : public IEngineModule
 		X_ASSERT_NOT_NULL(gEnv->pCore);
 		X_UNUSED(initParams);
 
-		g_3dEngineArena = X_NEW_ALIGNED(Engine3DArena, gEnv->pArena, "3DEngineArena", 8)(&g_3dEngineAlloc, "3DEngineArena");
+		g_3dEngineArena = X_NEW_ALIGNED(Engine3DArena, env.pArena, "3DEngineArena", 8)(&g_3dEngineAlloc, "3DEngineArena");
 
-		if (!gEnv->pCore->IntializeLoadedConverterModule("Engine_MaterialLib", "Engine_MaterialLib")) {
+		if (!env.pCore->IntializeLoadedConverterModule("Engine_MaterialLib", "Engine_MaterialLib")) {
 			X_ERROR("3DEngine", "Failed to init MaterialLib");
 			return false;
 		}
-		if (!gEnv->pCore->IntializeLoadedConverterModule("Engine_ImgLib", "Engine_ImgLib")) {
+		if (!env.pCore->IntializeLoadedConverterModule("Engine_ImgLib", "Engine_ImgLib")) {
 			X_ERROR("3DEngine", "Failed to init ImgLib");
 			return false;
 		}
-		if (!gEnv->pCore->IntializeLoadedConverterModule("Engine_ModelLib", "Engine_ModelLib")) {
+		if (!env.pCore->IntializeLoadedConverterModule("Engine_ModelLib", "Engine_ModelLib")) {
 			X_ERROR("3DEngine", "Failed to init ModelLib");
 			return false;
 		}
-		if (!gEnv->pCore->IntializeLoadedConverterModule("Engine_AnimLib", "Engine_AnimLib")) {
+		if (!env.pCore->IntializeLoadedConverterModule("Engine_AnimLib", "Engine_AnimLib")) {
 			X_ERROR("3DEngine", "Failed to init AnimLib");
 			return false;
 		}
