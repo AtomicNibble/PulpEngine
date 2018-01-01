82d4d276b638b1b26b56c7a0185900d53ec737ed
Tom Crowley <tom@tom-crowley.co.uk>
2018-01-01 16:15:30 +0000

    When we compile a font just bake some glyphs.
    I will still need the original trueType file at runtime, so need to work out how I want to store that.

--- tools/FontLib/Lib/FontLib.cpp
+++ tools/FontLib/Lib/FontLib.cpp
@@ -3,6 +3,8 @@
 
 #include <IFileSys.h>
 
+#include "Compiler\Compiler.h"
+
 X_NAMESPACE_BEGIN(font)
 
 	FontLib::FontLib()
@@ -23,7 +25,94 @@ X_NAMESPACE_BEGIN(font)
 	{
 		X_UNUSED(host, assetId, args, destPath);
 
-	
+		// load trueTypeFont from assetDB.
+		core::Array<uint8_t> fileData(host.getScratchArena());
+		if (!host.GetAssetData(assetId, fileData)) {
+			X_ERROR("Font", "Failed to get asset data");
+			return false;
+		}
+
+		if (fileData.isEmpty()) {
+			X_ERROR("Font", "File data is empty");
+			return false;
+		}
+
+		int32_t width = 32;
+		int32_t height = 32;
+		float sizeRatio = 1.f;
+		bool sdf = true;
+
+		{
+			core::json::Document d;
+			if (d.Parse(args.c_str(), args.length()).HasParseError()) {
+				X_ERROR("Font", "Error parsing json");
+				return false;
+			}
+
+			std::array<std::pair<const char*, core::json::Type>, 3
+			> requiredValues = { {
+				{ "glyphWidth", core::json::kNumberType },
+				{ "glyphHeight", core::json::kNumberType },
+				{ "sizeRatio", core::json::kNumberType }
+			} };
+
+			for (size_t i = 0; i < requiredValues.size(); i++)
+			{
+				const auto& item = requiredValues[i];
+				if (!d.HasMember(item.first)) {
+					X_ERROR("Font", "Missing required value: \"%s\"", item.first);
+					return false;
+				}
+
+				if (d[item.first].GetType() != item.second) {
+					X_ERROR("Font", "Incorrect type for \"%s\"", item.first);
+					return false;
+				}
+			}
+
+			width = d["glyphWidth"].GetInt();
+			height = d["glyphHeight"].GetInt();
+			sizeRatio = d["sizeRatio"].GetFloat();
+		}
+
+		FontCompiler compiler(g_FontLibArena);	
+		if (!compiler.setFont(fileData, width, height, sizeRatio)) {
+			X_ERROR("Font", "Failed to set source font data");
+			return false;
+		}
+
+		if (!compiler.bake(sdf)) {
+			X_ERROR("Font", "Failed to bake font data");
+			return false;
+		}
+
+		core::XFileScoped file;
+		core::fileModeFlags mode = core::fileMode::RECREATE | core::fileMode::WRITE;
+
+		if (!file.openFile(destPath.c_str(), mode)) {
+			X_ERROR("Font", "Failed to open output file");
+			return false;
+		}
+
+		if (!compiler.writeToFile(file.GetFile())) {
+			X_ERROR("Font", "Failed to write baked font file");
+			return false;
+		}
+
+		file.close();
+
+		OutPath imagePath(destPath);
+		imagePath.append(".bmp");
+		if (!file.openFile(imagePath.c_str(), mode)) {
+			X_ERROR("Font", "Failed to open img file");
+			return false;
+		}
+
+		if (!compiler.writeImageToFile(file.GetFile())) {
+			X_ERROR("Font", "Failed to write baked font file");
+			return false;
+		}
+
 
 		return true;
 	}
