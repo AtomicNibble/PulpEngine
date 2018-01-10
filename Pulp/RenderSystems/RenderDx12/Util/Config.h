a77958b1702a7a09dde74e751b3d5121a63522f7
Tom Crowley <tom@tom-crowley.co.uk>
2018-01-10 23:34:58 +0000

    Add config for PSO hot reload support.

--- Pulp/RenderSystems/RenderDx12/Util/Config.h
+++ Pulp/RenderSystems/RenderDx12/Util/Config.h
@@ -8,18 +8,21 @@ X_NAMESPACE_BEGIN(render)
 #define RENDER_STATS 1
 #define VID_MEMORY_STATS 1
 #define DEVICE_STATE_STORE_CPU_DESC 1 // for debugging just stores a copy of cpu state used to make device state.
+#define PSO_HOT_RELOAD 1 // needed for shader and state hot reloading.
 
 #elif X_RELEASE
 
 #define RENDER_STATS 1
 #define VID_MEMORY_STATS 1
 #define DEVICE_STATE_STORE_CPU_DESC 0
+#define PSO_HOT_RELOAD 0 
 
 #elif X_SUPER
 
 #define RENDER_STATS 0
 #define VID_MEMORY_STATS 0
 #define DEVICE_STATE_STORE_CPU_DESC 0 
+#define PSO_HOT_RELOAD 0 
 
 #else
 #error "unkonw config"
