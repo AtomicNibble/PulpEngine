235954a9ad887ec558b719b662bb2d3fa0da7dc9
Tom Crowley <tom@tom-crowley.co.uk>
2018-01-12 23:02:09 +0000

    Only loop flag count.

--- tools/ShaderLib/ShaderManager.cpp
+++ tools/ShaderLib/ShaderManager.cpp
@@ -31,7 +31,7 @@ namespace shader
 		{
 			if (srcflags.IsAnySet() && srcflags.ToInt() != dest.ToInt())
 			{
-				for (size_t i = 0; i < 32; i++)
+				for (size_t i = 0; i < TFlags::FLAG_COUNT; i++)
 				{
 					TFlags::Enum flag = static_cast<TFlags::Enum>(1 << i);
 					if (srcflags.IsSet(flag) && !dest.IsSet(flag)) {
