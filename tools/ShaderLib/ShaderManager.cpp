9830b248d1441072df90239d273618f820466e61
Tom Crowley <tom@tom-crowley.co.uk>
2018-01-14 16:31:56 +0000

    Log compile count when reloading enabled, need to come up with nicer way to log this.

--- tools/ShaderLib/ShaderManager.cpp
+++ tools/ShaderLib/ShaderManager.cpp
@@ -448,7 +448,25 @@ namespace shader
 		{
 			const auto* pShader = it;
 
-			X_LOG0("Shader", "Name: ^2\"%s\"^7 Status: ^2%s^7 Type: ^2%s^7 IL: ^2%s^7 NumInst: ^2%" PRIi32 "^7 Refs: ^2%" PRIi32,
+			const char* pFmt = "Name: ^2\"%s\"^7 Status: ^2%s^7 Type: ^2%s^7 IL: ^2%s^7 NumInst: ^2%" PRIi32 "^7 Refs: ^2%" PRIi32
+#if X_ENABLE_RENDER_SHADER_RELOAD
+				"^7 CompileCnt: ^2%" PRIi32
+#endif // !X_ENABLE_RENDER_SHADER_RELOAD
+			;
+
+			// little ugly as can't #if def inside macro call.
+#if X_ENABLE_RENDER_SHADER_RELOAD
+			X_LOG0("Shader", pFmt,
+				pShader->getName().c_str(),
+				ShaderStatus::ToString(pShader->getStatus()),
+				ShaderType::ToString(pShader->getType()),
+				InputLayoutFormat::ToString(pShader->getILFormat()),
+				pShader->getNumInstructions(),
+				pShader->getRefCount(),
+				pShader->getCompileCount()
+			);
+#else
+			X_LOG0("Shader", pFmt,
 				pShader->getName().c_str(),
 				ShaderStatus::ToString(pShader->getStatus()),
 				ShaderType::ToString(pShader->getType()),
@@ -456,6 +474,7 @@ namespace shader
 				pShader->getNumInstructions(),
 				pShader->getRefCount()
 			);
+#endif // !X_ENABLE_RENDER_SHADER_RELOAD
 		}
 		X_LOG0("Shader", "------------ ^8Shaders End^7 -------------");
 	}
