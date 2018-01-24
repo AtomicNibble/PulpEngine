8d3ab926bf1c4c9adb2046f022208455a261b713
Tom Crowley <tom@tom-crowley.co.uk>
2018-01-24 00:30:05 +0000

    Add helper for getting assetType from string.

---
+++ tools/AssetDB/AssetDBUtil.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <IAssetDb.h>
+
+X_NAMESPACE_BEGIN(assetDb)
+
+namespace Util
+{
+
+	DLL_EXPORT AssetType::Enum AssetTypeFromStr(const char* pBegin, const char* pEnd);
+
+
+} // namespace Util
+
+X_NAMESPACE_END
