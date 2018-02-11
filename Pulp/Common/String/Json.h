251536d140d56faaa33e938022584168c85fa85f
Tom Crowley <tom@tom-crowley.co.uk>
2018-02-11 20:28:38 +0000

    Add error helpers for json.

--- Pulp/Common/String/Json.h
+++ Pulp/Common/String/Json.h
@@ -28,4 +28,31 @@
 #include <../../3rdparty/source/rapidjson/error/en.h>
 
 
+RAPIDJSON_NAMESPACE_BEGIN
+
+typedef core::StackString512 Description;
+
+inline const char* ErrorToString(const Document& d, Description& desc)
+{
+	auto err = d.GetParseError();
+	const char* pErrStr = GetParseError_En(err);
+	size_t offset = d.GetErrorOffset();
+
+	desc.appendFmt("desc(%" PRIi32 ") : Offset: %" PRIuS " Err : %s", err, offset, pErrStr);
+	return desc.c_str();
+}
+
+inline const char* ErrorToString(const Document& d, const char* pBegin, const char* pEnd, Description& desc)
+{
+	auto err = d.GetParseError();
+	const char* pErrStr = GetParseError_En(err);
+	size_t offset = d.GetErrorOffset();
+	size_t line = strUtil::LineNumberForOffset(pBegin, pEnd, offset);
+
+	desc.appendFmt("desc(%" PRIi32 ") : Offset: %" PRIuS " Line : %" PRIuS " Err : %s", err, offset, line, pErrStr);
+	return desc.c_str();
+}
+
+RAPIDJSON_NAMESPACE_END
+
 #endif // !X_STRING_JSON_H_
