6acc93024d642d63882bb56c51e06574bb4c88aa
Tom Crowley <tom@tom-crowley.co.uk>
2018-05-01 23:04:50 +0100

    Work on the network session / lobby.

--- Pulp/Network/Session/Session.cpp
+++ Pulp/Network/Session/Session.cpp
@@ -8,25 +8,358 @@
 
 X_NAMESPACE_BEGIN(net)
 
+Lobby::Lobby(IPeer* pPeer, LobbyType::Enum type, core::MemoryArenaBase* arena) :
+    pPeer_(pPeer),
+    users_(arena)
+{
+    type_ = type;
+    state_ = LobbyState::Idle;
+
+    isHost_ = false;
+    finishedLoading_ = false;
+
+}
+
+
+bool Lobby::handleState(void)
+{
+
+    switch (state_) {
+        case LobbyState::Idle:
+            return stateIdle();
+        case LobbyState::Creating:
+            return stateCreating();
+
+        default:
+            X_NO_SWITCH_DEFAULT_ASSERT;
+    }
+
+    return false;
+}
+
+void Lobby::setState(LobbyState::Enum state)
+{
+    state_ = state;
+}
+
+void Lobby::startHosting(const MatchParameters& parms)
+{
+    params_ = params_;
+
+    shutdown();
+
+    {
+        // if we have a remote lobby, like on a server.
+        // would need to dispatch request and wait.
+        // for now just instant readdy.
+
+    }
+
+    setState(LobbyState::Creating);
+}
+
+void Lobby::finishedLoading(void)
+{
+    finishedLoading_ = true;
+}
+
+void Lobby::shutdown(void)
+{
+
+
+}
+
+bool Lobby::stateIdle(void)
+{
+    // anything..?
+
+    return true;
+}
+
+bool Lobby::stateCreating(void)
+{
+    // here we check if lobby has failed to create / finished.
+    // for now it's instantly ready.
+
+    // setup state of the new lobby.
+    initStateLobbyHost();
+
+    setState(LobbyState::Idle);
+    return true;
+}
+
+void Lobby::initStateLobbyHost(void)
+{
+    // we are hosting
+    isHost_ = true;
+
+
+    // need to add myself to peer list.
+    clearUsers();
+
+    auto localGuid = pPeer_->getMyGUID();
+    SystemAddress address;
+
+    LobbyUser user;
+    user.guid = localGuid;
+    user.address = address;
+    users_.emplace_back(user);
+
+    hostAddress_ = address;
+
+    // done?
+}
+
+void Lobby::clearUsers(void)
+{
+    users_.clear();
+}
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+
 Session::Session(IPeer* pPeer, core::MemoryArenaBase* arena) :
     pPeer_(X_ASSERT_NOT_NULL(pPeer)),
-    arena_(X_ASSERT_NOT_NULL(arena))
+    arena_(X_ASSERT_NOT_NULL(arena)),
+    partyLobby_(pPeer_, LobbyType::Party, arena),
+    gameLobby_(pPeer_, LobbyType::Game, arena)
 {
 
 }
 
+void Session::finishedLoading(void)
+{
+    // we finished loading.
+    // noice.
+
+    gameLobby_.finishedLoading();
+
+    auto flags = gameLobby_.getMatchFlags();
+    if (flags.IsSet(MatchFlag::Online))
+    {
+        if (gameLobby_.isHost())
+        {
+            setState(SessionState::InGame);
+        }
+        else
+        {
+            // TODO: send a packet to the host telling them we loaded.
+            // also eat a pickle, while we wait. yum.
+            X_ASSERT_NOT_IMPLEMENTED();
+        }
+    }
+    else
+    {
+        setState(SessionState::InGame);
+    }
+}
+
+
+void Session::quitToMenu(void)
+{
+    // TODO: leave any lobby.
+
+    setState(SessionState::Idle);
+}
+
+void Session::createPartyLobby(const MatchParameters& parms)
+{
+    // host a new 'party' lobby.
+    partyLobby_.startHosting(parms);
+
+    setState(SessionState::CreateAndMoveToPartyLobby);
+}
+
+void Session::createMatch(const MatchParameters& parms)
+{
+    // host a new 'game'
+    gameLobby_.startHosting(parms);
+
+    setState(SessionState::CreateAndMoveToGameLobby);
+}
+
+void Session::startMatch(void)
+{
+    X_ASSERT(state_ == SessionState::GameLobbyHost, "Can't start match without been a game host")(state_);
+
+    startLoading();
+}
+
 void Session::runUpdate(void)
 {
+
+    handleState();
+}
+
+
+bool Session::handleState(void)
+{
+    // so this is like the state machine for the game.
+    // we have diffrent logic depending on your current state.
     pPeer_->runUpdate();
 
+    switch (state_)
+    {
+        case SessionState::Idle:
+            return stateIdle();
+        case SessionState::CreateAndMoveToPartyLobby:
+            return stateCreateAndMoveToPartyLobby();
+        case SessionState::CreateAndMoveToGameLobby:
+            return stateCreateAndMoveToGameLobby();
+        case SessionState::PartyLobbyHost:
+            return statePartyLobbyHost();
+        case SessionState::PartyLobbyPeer:
+            return statePartyLobbyPeer();
+        case SessionState::GameLobbyHost:
+            return stateGameLobbyHost();
+        case SessionState::GameLobbyPeer:
+            return stateGameLobbyPeer();
+        case SessionState::Connecting:
+            return stateConnecting();
+        case SessionState::Loading:
+            return stateLoading();
+        case SessionState::InGame:
+            return stateInGame();
+
+        default:
+            X_NO_SWITCH_DEFAULT_ASSERT;
+    }
+
+    return false;
+}
+
+
+bool Session::stateIdle(void)
+{
+
+    return true;
+}
+
+bool Session::stateConnecting(void)
+{
+
+    return true;
+}
+
+bool Session::stateCreateAndMoveToPartyLobby(void)
+{
+    // wait for the lobby to create.
+    if (hasLobbyCreateCompleted(partyLobby_))
+    {
+        // Success
+        setState(SessionState::PartyLobbyHost);
+
+        return true;
+    }
+
+    return readPackets();
+}
+
+bool Session::stateCreateAndMoveToGameLobby(void)
+{
+    if (hasLobbyCreateCompleted(gameLobby_))
+    {
+     //   partyLobby_.sendMembersToLobby(gameLobby_);
+
+        // Success
+        setState(SessionState::GameLobbyHost);
+        return true;
+    }
+
+    return true;
+}
+
+bool Session::statePartyLobbyHost(void)
+{
+    return readPackets();
+}
+
+bool Session::statePartyLobbyPeer(void)
+{
+    return readPackets();
+}
+
+bool Session::stateGameLobbyHost(void)
+{
+    return readPackets();
+}
+
+bool Session::stateGameLobbyPeer(void)
+{
+    return readPackets();
+}
+
+bool Session::stateLoading(void)
+{
+    // hurry the fuck up!
+    if (!gameLobby_.hasFinishedLoading()) {
+        return false;
+    }
+
+    // if not online, we should of switched out of this state if hasFinishedLoading().
+    if (gameLobby_.getMatchFlags().IsSet(MatchFlag::Online)) {
+        X_ASSERT_UNREACHABLE();
+    }
+
+    if (gameLobby_.isHost())
+    {
+        // TODO: wait for the plebs to finish loading.
+        X_ASSERT_NOT_IMPLEMENTED();
+        return false;
+    }
+    else
+    {
+        // we are a dirty pleb.
+        // check the host is still around?
+        // if not exit to menu with error.
+        X_ASSERT_NOT_IMPLEMENTED();
+        return false;
+    }
+
+    setState(SessionState::InGame);
+    return true;
+}
+
+bool Session::stateInGame(void)
+{
+    // packets, packets, packets!
+    return readPackets();
+}
+
+
+bool Session::hasLobbyCreateCompleted(Lobby& lobby)
+{
+    if (lobby.getState() == LobbyState::Idle) {
+        return true;
+    }
+
+    // check for failed state.
+
+    return false;
+}
+
+void Session::startLoading(void)
+{
+    // should only be called if we are the host.
+    {
+        // TODO: tell the connected plebs to start loading.
+
+    }
+
+    // Weeeeeeeeeeee!!
+    setState(SessionState::Loading);
+}
+
+bool Session::readPackets(void)
+{
     Packet* pPacket = nullptr;
-    for (pPacket = pPeer_->receive(); pPacket; pPeer_->freePacket(pPacket), pPacket = pPeer_->receive()) 
+    for (pPacket = pPeer_->receive(); pPacket; pPeer_->freePacket(pPacket), pPacket = pPeer_->receive())
     {
         X_LOG0("Session", "Recived packet: bitLength: %" PRIu32, pPacket->bitLength);
-        
+
         core::FixedBitStreamNoneOwning bs(pPacket->begin(), pPacket->end(), true);
 
-        switch(pPacket->getID()) 
+        switch (pPacket->getID())
         {
             case MessageID::ConnectionRequestAccepted:
             {
@@ -51,11 +384,11 @@ void Session::runUpdate(void)
                 con.col = Col_Red;
                 con.pFont = gEnv->pFontSys->GetDefault();
 
-                Matrix33f ang = Matrix33f::createRotation(Vec3f(1.f,0.f,0.f), ::toRadians(-90.f));
-                
+                Matrix33f ang = Matrix33f::createRotation(Vec3f(1.f, 0.f, 0.f), ::toRadians(-90.f));
+
                 static int32_t num = 0;
 
-                pPRim->drawText(Vec3f(0.f,0.f,80.f - (num * 18.f)), ang, con, buf);
+                pPRim->drawText(Vec3f(0.f, 0.f, 80.f - (num * 18.f)), ang, con, buf);
 
                 num++;
                 break;
@@ -67,7 +400,9 @@ void Session::runUpdate(void)
 
     }
 
+    return true;
 }
+
 
 void Session::sendChatMsg(const char* pMsg)
 {
@@ -82,6 +417,40 @@ void Session::sendChatMsg(const char* pMsg)
 
     pPeer_->send(bs.data(), bs.sizeInBytes(), PacketPriority::High, PacketReliability::Reliable, pleb_);
 
+}
+
+SessionStatus::Enum Session::getStatus(void) const
+{
+    static_assert(SessionState::ENUM_COUNT == 10, "Enums changed?");
+    static_assert(SessionStatus::ENUM_COUNT == 6, "Enums changed?");
+
+    switch(state_)
+    {
+        case SessionState::Idle:
+            return SessionStatus::Idle;
+        case SessionState::CreateAndMoveToPartyLobby:
+            return SessionStatus::Connecting;
+        case SessionState::CreateAndMoveToGameLobby:
+            return SessionStatus::Connecting;
+        case SessionState::PartyLobbyHost:
+            return SessionStatus::PartyLobby;
+        case SessionState::PartyLobbyPeer:
+            return SessionStatus::PartyLobby;
+        case SessionState::GameLobbyHost:
+            return SessionStatus::GameLobby;
+        case SessionState::GameLobbyPeer:
+            return SessionStatus::GameLobby;
+        case SessionState::Connecting:
+            return SessionStatus::Connecting;
+        case SessionState::Loading:
+            return SessionStatus::Loading;
+        case SessionState::InGame:
+            return SessionStatus::InGame;
+        default:
+            X_NO_SWITCH_DEFAULT_ASSERT;
+    }
+
+    return SessionStatus::Idle;
 }
 
 X_NAMESPACE_END
--- Pulp/Network/Session/Session.h
+++ Pulp/Network/Session/Session.h
@@ -1,14 +1,144 @@
 #pragma once
 
 
+#include <Containers\Array.h>
+
 X_NAMESPACE_BEGIN(net)
 
-X_DECLARE_ENUM(SessionState)(
-    
+/*
+
+I want this to be like a game session.
+The reason it's in network lib, is no matter what game you make you should use this session.
+and single player works like a loopback server.
+
+So this session is for handling current game state like in menu / in game / loading etc..
+You submit useCmd's to the session and get snapshots back will all the ent info.
+
+The session has no idea what's in a snap shot, it's just a blob of data, it's unto the gameDll to interperate the data.
+
+
+*/
+
+
+// Status is merged state.
+X_DECLARE_ENUM(SessionStatus)(
     Idle,
+    PartyLobby,
+    GameLobby,
+    Connecting,
     Loading,
     InGame
 );
+
+X_DECLARE_ENUM(SessionState)(
+    Idle,
+    CreateAndMoveToPartyLobby,
+    CreateAndMoveToGameLobby,
+    PartyLobbyHost,
+    PartyLobbyPeer, // pleb
+    GameLobbyHost,
+    GameLobbyPeer, // pleb
+    Connecting,
+    Loading,
+    InGame
+);
+
+X_DECLARE_ENUM(LobbyType)(
+    Party,
+    Game
+);
+
+X_DECLARE_ENUM(LobbyState)(
+    Idle,
+    Creating
+);
+
+X_DECLARE_FLAGS(MatchFlag)(
+    Online    
+);
+
+typedef Flags<MatchFlag> MatchFlags;
+
+struct MatchParameters
+{
+    int32_t numSlots;
+    MatchFlags flags;
+
+    core::string mapName;
+};
+
+
+struct LobbyUser
+{
+    NetGUID guid;
+    SystemAddress address;
+
+};
+
+class Lobby
+{
+    typedef core::Array<LobbyUser> LobbyUserArr;
+
+public:
+    Lobby(IPeer* pPeer, LobbyType::Enum type, core::MemoryArenaBase* arena);
+
+    bool handleState(void);
+
+    void startHosting(const MatchParameters& parms);
+  //  void sendMembersToLobby(Lobby& destLobby);
+    void finishedLoading(void);
+
+    void shutdown(void);
+
+    X_INLINE LobbyState::Enum getState(void) const;
+    X_INLINE bool isHost(void) const;
+    X_INLINE bool hasFinishedLoading(void) const;
+    X_INLINE MatchFlags getMatchFlags(void) const;
+
+private:
+    void setState(LobbyState::Enum state);
+
+private:
+    bool stateIdle(void);
+    bool stateCreating(void);
+
+private:
+    void initStateLobbyHost(void);
+    void clearUsers(void);
+
+private:
+    IPeer* pPeer_;
+    LobbyType::Enum type_;
+    LobbyState::Enum state_;
+
+    MatchParameters params_;
+
+    bool isHost_;
+    bool finishedLoading_; // loaded the map yet slut?
+    SystemAddress hostAddress_;
+
+    LobbyUserArr users_;
+};
+
+X_INLINE LobbyState::Enum Lobby::getState(void) const
+{
+    return state_;
+}
+
+X_INLINE bool Lobby::isHost(void) const
+{
+    return isHost_;
+}
+
+X_INLINE bool Lobby::hasFinishedLoading(void) const
+{
+    return finishedLoading_;
+}
+
+X_INLINE MatchFlags Lobby::getMatchFlags(void) const
+{
+    return params_.flags;
+}
 
 
 class Session : public ISession
@@ -16,24 +146,72 @@ class Session : public ISession
 public:
     Session(IPeer* pPeer, core::MemoryArenaBase* arena);
 
+
+    void finishedLoading(void); // tell the session we finished loading the map.
+
+
+    void quitToMenu(void);
+    void createPartyLobby(const MatchParameters& parms);
+    void createMatch(const MatchParameters& parms);
+    void startMatch(void);
+
+
     void runUpdate(void) X_FINAL;
+
+    bool handleState(void);
 
     void sendChatMsg(const char* pMsg);
 
-
     X_INLINE IPeer* getPeer(void) const;
+
+    X_INLINE SessionState::Enum getState(void) const;
+    SessionStatus::Enum getStatus(void) const;
+
+private:
+    void setState(SessionState::Enum state);
+
+private:
+
+    bool stateIdle(void);
+    bool stateConnecting(void);
+    bool stateCreateAndMoveToPartyLobby(void);
+    bool stateCreateAndMoveToGameLobby(void);
+    bool statePartyLobbyHost(void);
+    bool statePartyLobbyPeer(void);
+    bool stateGameLobbyHost(void);
+    bool stateGameLobbyPeer(void);
+    bool stateLoading(void);
+    bool stateInGame(void);
+
+    bool hasLobbyCreateCompleted(Lobby& lobby);
+
+    void startLoading(void);
+
+    bool readPackets(void);
+
 
 private:
     IPeer* pPeer_;
     core::MemoryArenaBase* arena_;
 
+    SessionState::Enum state_;
+
     SystemHandle pleb_;
+
+
+    Lobby partyLobby_;
+    Lobby gameLobby_;
 };
 
 
 X_INLINE IPeer* Session::getPeer(void) const
 {
     return pPeer_;
+}
+
+X_INLINE SessionState::Enum Session::getState(void) const
+{
+    return state_;
 }
 
 X_NAMESPACE_END
