222e22b67bbaf8f574e6d1c7513a6047a76ca62e
Tom Crowley <tom@tom-crowley.co.uk>
2018-05-07 15:00:46 +0100

    Add some basic FixedRingBuffer tests

---
+++ Pulp/UnitTests/UT_C_FixedRingBuffer.cpp
@@ -0,0 +1,108 @@
+#include "stdafx.h"
+#include "ComplexTypes.h"
+
+#include <Containers\FixedRingBuffer.h>
+
+X_USING_NAMESPACE;
+
+using namespace core;
+
+
+typedef ::testing::Types<short, int, float, testTypes::CustomType> MyTypes;
+TYPED_TEST_CASE(FixedRingBufferTest, MyTypes);
+
+template<typename T>
+class FixedRingBufferTest : public ::testing::Test
+{
+public:
+};
+
+
+TYPED_TEST(FixedRingBufferTest, Types)
+{
+    testTypes::resetConConters();
+
+    FixedRingBuffer<TypeParam, 3> ring;
+
+    EXPECT_EQ(0, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    ring.append(16);
+    EXPECT_EQ(1, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    ring.append(32);
+    EXPECT_EQ(2, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    ring.append(48);
+    EXPECT_EQ(3, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    EXPECT_EQ(16, ring[0]);
+    EXPECT_EQ(32, ring[1]);
+    EXPECT_EQ(48, ring[2]);
+
+    ring.append(96);
+    EXPECT_EQ(3, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    EXPECT_EQ(32, ring[0]);
+    EXPECT_EQ(48, ring[1]);
+    EXPECT_EQ(96, ring[2]);
+}
+
+TEST(FixedRingBufferTest, ComplexConstruction)
+{
+    testTypes::resetConConters();
+
+    FixedRingBuffer<testTypes::CustomType, 3> ring;
+
+    EXPECT_EQ(0, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    ring.append(16);
+    ring.append(32);
+    ring.append(48);
+
+    EXPECT_EQ(0, testTypes::MOVE_COUNT);
+    EXPECT_EQ(3 + 3, testTypes::CONSRUCTION_COUNT); // 3 rvalues
+    EXPECT_EQ(3, testTypes::DECONSRUCTION_COUNT);
+
+    ring.append(96);
+    EXPECT_EQ(3, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    // 1 rvalue + assignment
+    EXPECT_EQ(0, testTypes::MOVE_COUNT);
+    EXPECT_EQ(6 + 1, testTypes::CONSRUCTION_COUNT);
+    EXPECT_EQ(3 + 1, testTypes::DECONSRUCTION_COUNT);
+}
+
+TEST(FixedRingBufferTest, ComplexEmplace)
+{
+    testTypes::resetConConters();
+
+    FixedRingBuffer<testTypes::CustomType, 3> ring;
+
+    EXPECT_EQ(0, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    ring.emplace_back(16);
+    ring.emplace_back(32);
+    ring.emplace_back(48);
+
+    EXPECT_EQ(0, testTypes::MOVE_COUNT);
+    EXPECT_EQ(3, testTypes::CONSRUCTION_COUNT); // 3 rvalues
+    EXPECT_EQ(0, testTypes::DECONSRUCTION_COUNT);
+
+    ring.emplace_back(96);
+    EXPECT_EQ(3, ring.size());
+    ASSERT_EQ(3, ring.capacity());
+
+    // 1 rvalue + assignment
+    EXPECT_EQ(0, testTypes::MOVE_COUNT);
+    EXPECT_EQ(4, testTypes::CONSRUCTION_COUNT);
+    EXPECT_EQ(1, testTypes::DECONSRUCTION_COUNT);
+}
+
