1559b1b2ec5cbb14570abfb006b4a73774306d47
Tom Crowley <tom@tom-crowley.co.uk>
2018-05-24 19:54:14 +0100

    Add a test that has a peer connect and disconnect multiple times to the same lobby.

--- tools/NetWorkTests/tests/SessionTest.cpp
+++ tools/NetWorkTests/tests/SessionTest.cpp
@@ -218,6 +218,134 @@ TEST_F(SessionTest, ConnectToPartyLobby)
     }
 }
 
+TEST_F(SessionTest, ReConnectToPartyLobby)
+{
+    EXPECT_EQ(SessionStatus::Idle, pSeverSes_->getStatus());
+    EXPECT_EQ(SessionStatus::Idle, pClientSes_->getStatus());
+
+    MatchParameters params;
+    pSeverSes_->createPartyLobby(params);
+
+    EXPECT_EQ(SessionStatus::Connecting, pSeverSes_->getStatus());
+
+    pump();
+
+    for (int32_t loop = 0; loop < 5; loop++)
+    {
+        EXPECT_EQ(SessionStatus::PartyLobby, pSeverSes_->getStatus());
+
+        net::SystemAddress sa;
+        sa.setToLoopback();
+        sa.setPortFromHostByteOrder(SERVER_PORT_BASE);
+        pClientSes_->connect(sa);
+
+        auto status = pClientSes_->getStatus();
+
+        EXPECT_NE(SessionStatus::Idle, status);
+
+        int32_t i = 0;
+        for (; i < 300; i++)
+        {
+            pump();
+
+            status = pClientSes_->getStatus();
+
+            if (status == SessionStatus::Connecting)
+            {
+                core::Thread::sleep(5);
+            }
+            else
+            {
+                EXPECT_EQ(SessionStatus::PartyLobby, status);
+                break;
+            }
+        }
+
+        ASSERT_TRUE(i < 100) << "failed to connect";
+
+        // we have connected to the server.
+        // check some states.
+        EXPECT_EQ(SessionStatus::PartyLobby, pSeverSes_->getStatus());
+        EXPECT_EQ(SessionStatus::PartyLobby, pClientSes_->getStatus());
+
+
+        // now have the client leave.
+        pClientSes_->disconnect();
+
+        // the client should send disconnect msg to host, but it might not arrive.
+        // in this test it should awlays arrive.
+        // it would just timeout in the real world.
+        // what i want to test is that we can re join after.
+        // so wait for the peer to leave hosts list.
+        pump();
+
+        EXPECT_EQ(SessionStatus::PartyLobby, pSeverSes_->getStatus());
+        EXPECT_EQ(SessionStatus::Idle, pClientSes_->getStatus());
+
+        for (i = 0; i < 300; i++)
+        {
+            pump();
+
+            auto* pLobby = pSeverSes_->getLobby(LobbyType::Party);
+
+            if (pLobby->getNumUsers() == 2)
+            {
+                core::Thread::sleep(100);
+            }
+            else
+            {
+                EXPECT_EQ(1, pLobby->getNumUsers());
+                break;
+            }
+        }
+
+        ASSERT_TRUE(i < 300) << "peer failed to disconnect from host";
+
+        // check we disconnected.
+        EXPECT_EQ(SessionStatus::PartyLobby, pSeverSes_->getStatus());
+        EXPECT_EQ(SessionStatus::Idle, pClientSes_->getStatus());
+
+        // server
+        {
+            auto* pLobby = pSeverSes_->getLobby(LobbyType::Party);
+
+            EXPECT_EQ(0, pLobby->getNumConnectedPeers());
+            EXPECT_EQ(1, pLobby->getNumUsers());
+            EXPECT_TRUE(pLobby->isHost());
+            EXPECT_FALSE(pLobby->isPeer());
+            EXPECT_FALSE(pLobby->allPeersLoaded());
+        }
+
+        {
+            auto* pLobby = pClientSes_->getLobby(LobbyType::Party);
+
+            EXPECT_EQ(0, pLobby->getNumConnectedPeers());
+            EXPECT_EQ(0, pLobby->getNumUsers());
+            EXPECT_FALSE(pLobby->isHost());
+            EXPECT_FALSE(pLobby->isPeer());
+            EXPECT_FALSE(pLobby->allPeersLoaded());
+        }
+
+    }
+
+    EXPECT_EQ(SessionStatus::PartyLobby, pSeverSes_->getStatus());
+    EXPECT_EQ(SessionStatus::Idle, pClientSes_->getStatus());
+
+    pSeverSes_->quitToMenu();
+
+    EXPECT_EQ(SessionStatus::Idle, pSeverSes_->getStatus());
+
+    {
+        auto* pLobby = pSeverSes_->getLobby(LobbyType::Party);
+
+        EXPECT_EQ(0, pLobby->getNumConnectedPeers());
+        EXPECT_EQ(0, pLobby->getNumUsers());
+        EXPECT_FALSE(pLobby->isHost());
+        EXPECT_FALSE(pLobby->isPeer());
+        EXPECT_FALSE(pLobby->allPeersLoaded());
+    }
+}
+
 
 
 X_NAMESPACE_END
