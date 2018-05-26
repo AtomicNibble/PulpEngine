204e65534e9bb65ccd234eb6bc364df4ed1b06a3
Tom Crowley <tom@tom-crowley.co.uk>
2018-05-26 12:13:02 +0100

    Add a msg to tell peers to connect and move to specified lobby.

--- Pulp/Common/NetMsgIds.h
+++ Pulp/Common/NetMsgIds.h
@@ -187,6 +187,8 @@ X_DECLARE_ENUM8(MessageID)
 
     LobbyGameParams,        // Game meta.
 
+    LobbyConnectAndMove,    // The host of a patry told us to join this party.
+
     // --- Session ---
 
     LoadingStart,   // tell peer to start loading
