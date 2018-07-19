813bf7e1cab9abd4b20d7e96000eba5dc5eea7fc
Tom Crowley <tom@tom-crowley.co.uk>
2018-07-19 23:08:18 +0100

    Pass empty mod for editor converter host.

--- tools/AssetManager/ConverterHost.cpp
+++ tools/AssetManager/ConverterHost.cpp
@@ -22,7 +22,7 @@ ConverterHost::~ConverterHost()
 
 void ConverterHost::init()
 {
-	if (!con_.Init()) {
+	if (!con_.Init(core::string(""))) {
 		// shit.
 		QMessageBox::critical(ICore::mainWindow(), "Converter", "Failed to init converter");
 	}
