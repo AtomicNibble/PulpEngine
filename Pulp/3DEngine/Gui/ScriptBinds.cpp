b99583466c3ba6d4dbe087a48228262cd0b4da7e
Tom Crowley <tom@tom-crowley.co.uk>
2018-07-29 13:45:43 +0100

    Implement fill color param parsing.

--- Pulp/3DEngine/Gui/ScriptBinds.cpp
+++ Pulp/3DEngine/Gui/ScriptBinds.cpp
@@ -32,7 +32,45 @@ namespace gui
 
     int32_t ScriptBinds_Menu::fill(script::IFunctionHandler* pH)
     {
-        pCtx_->fill(Col_Salmon);
+        SCRIPT_CHECK_PARAMETERS_MIN(1);
+
+        Color8u col;
+        Vec4i v;
+
+        auto type = pH->getParamType(1);
+        if (type == script::Type::Table)
+        {
+            script::ScriptValue any;
+            pH->getParamAny(1, any);
+
+            X_ASSERT(any.getType() == script::Type::Table, "Should be table")(any.getType());
+
+            any.pTable_->getValue(1, v.x);
+            any.pTable_->getValue(2, v.y);
+            any.pTable_->getValue(3, v.z);
+            any.pTable_->getValue(4, v.w);
+        }
+        else
+        {
+            // should have 4 ints,
+            pH->getParam(v.x, v.y, v.z);
+
+            if (pH->getParamCount() > 3)
+            {
+                pH->getParam(4, v.w);
+            }
+            else
+            {
+                v.w = 255;
+            }
+        }
+
+        col.r = static_cast<uint8_t>(v.x);
+        col.g = static_cast<uint8_t>(v.y);
+        col.b = static_cast<uint8_t>(v.z);
+        col.a = static_cast<uint8_t>(v.w);
+
+        ctx_.fill(col);
 
         return pH->endFunction();
     }
