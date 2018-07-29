b483dc0860d3a7bfc93c620c213fec6be9c1f976
Tom Crowley <tom@tom-crowley.co.uk>
2018-07-29 16:45:29 +0100

    Implement basic pause menu

--- game_folder/core_assets/menus/pause.lua
+++ game_folder/core_assets/menus/pause.lua
@@ -1,14 +1,30 @@
 
 
-
-function LoadAssets()
-
-
-end
-
-function Update()
-
-    gui.Text("Hello, world!");
+local function LoadAssets()
 
 
 end
+
+local function Update()
+
+    ui.fill(20,20,20,220)
+
+    if ui.button("RESUME") then
+        ui.close()
+    end
+    if ui.button("SETTINGS") then
+        ui.open("settings")
+    end
+    if ui.button("QUIT") then
+        core.exec("quit")
+    end
+
+    ui.sliderVar("VOLUME", "snd_vol_master");
+
+end
+
+
+return {
+  LoadAssets = LoadAssets,
+  Update = Update,
+}
