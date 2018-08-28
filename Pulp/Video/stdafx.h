8c91002e6315720a26284a36c785641d5e5c0dca
Tom Crowley <tom@tom-crowley.co.uk>
2018-08-28 22:31:02 +0100

    Add libvorbis source / head

--- Pulp/Video/stdafx.h
+++ Pulp/Video/stdafx.h
@@ -12,10 +12,18 @@
 
 #include <libyuv.h>
 
+// vorbis / ogg
+#include <ogg\ogg.h>
+#include <vorbis\codec.h>
+
 #if X_DEBUG
 X_LINK_LIB("vpxmdd");
+X_LINK_LIB("libvorbis_staticd");
+X_LINK_LIB("libogg_staticd");
 #else
 X_LINK_LIB("vpxmd");
+X_LINK_LIB("libvorbis_static");
+X_LINK_LIB("libogg_static");
 #endif // !X_DEBUG
 
 extern core::MemoryArenaBase* g_VideoArena;
