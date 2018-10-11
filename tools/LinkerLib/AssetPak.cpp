91b28cb543bf3700e4ede3eb026d724fa5766e46
Tom Crowley <tom@tom-crowley.co.uk>
2018-10-11 19:20:48 +0100

    Allow assets with invalid id's since some assets we want to have currently are not part of the db.

--- tools/LinkerLib/AssetPak.cpp
+++ tools/LinkerLib/AssetPak.cpp
@@ -625,7 +625,6 @@ bool AssetPakBuilder::save(const core::Path<char>& path)
 
 void AssetPakBuilder::addAsset(AssetId id, const core::string& name, core::string&& relativePath, AssetType::Enum type, DataVec&& data)
 {
-    X_ASSERT(id != assetDb::INVALID_ASSET_ID, "Invalid id")();
     X_ASSERT(name.isNotEmpty() && data.isNotEmpty(), "Empty name or data")(name.length(), data.size());
 
     assets_.emplace_back(id, name, std::move(relativePath), type, std::move(data), arena_);
