d943dfb22eefe7eb30431a4c16718d84cd155673
Tom Crowley <tom@tom-crowley.co.uk>
2018-10-14 15:44:27 +0100

    Tidy up

--- Pulp/Common/String/StackString.h
+++ Pulp/Common/String/StackString.h
@@ -12,23 +12,13 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup String
-/// \class FixedSizeString
-/// \brief A class representing a string containing a fixed number of characters.
-/// \details This class offers functionality similar to other string implementations, such as std::string.
-/// However, as the name implies, a FixedSizeString will never grow, shrink, or allocate any memory dynamically. It can
-/// often be used in situations where the maximum length of a string can be limited, such as when dealing with filenames,
-/// in the logging system, when parsing files, etc. A FixedSizeString should be preferred to other implementations
-/// in such cases.
-///
-/// In order to keep template instantiations to a minimum, certain typedefs for common usage scenarios are provided,
-/// which should be used whenever possible.
-/// \sa stringUtil StringRange FixedSizeString512 Pathname
+
 template<size_t N, typename TChar = char>
 class StackString
 {
 public:
     typedef TChar char_type;
+    typedef StackString<N, TChar> MyT;
 
     static const size_t BUF_SIZE = N;
 
@@ -185,18 +175,12 @@ public:
     explicit StackString(const unsigned __int64 f);
     explicit StackString(const __int64 f);
 
-    /// Constructs a string by copying the given range.
     StackString(const wchar_t* const beginInclusive, const wchar_t* const endExclusive);
 
-    /// Appends a character a certain amount of times.
     void append(wchar_t ch, size_t count);
-    /// Appends a string.
     inline void append(const wchar_t* str);
-    /// Appends part of another string.
     void append(const wchar_t* str, size_t count);
-    /// Appends part of another string.
     inline void append(const wchar_t* str, const wchar_t* end);
-    /// Appends a formatted string.
     void appendFmt(const wchar_t* format, ...);
     void appendFmt(const wchar_t* format, va_list args);
 
@@ -207,22 +191,14 @@ public:
     void setFmt(const wchar_t* format, ...);
     void setFmt(const wchar_t* format, va_list args);
 
-    /// \brief Replaces part of the string.
-    /// \remark Returns whether the string was found and replaced.
     bool replace(const wchar_t* original, const wchar_t* replacement);
     bool replace(const wchar_t* start, const wchar_t* original, const wchar_t* replacement);
-    /// \brief Replaces first occurenct of character
-    /// \remark Returns whether the string was found and replaced.
     bool replace(const wchar_t original, const wchar_t replacement);
 
-    /// Replaces all occurrences of a string, and returns the number of occurrences replaced.
     size_t replaceAll(const wchar_t* original, const wchar_t* replacement);
-    /// Replaces all occurrences of a character, and returns the number of occurrences replaced.
     size_t replaceAll(const wchar_t original, const wchar_t replacement);
 
-    /// Trims all whitespace to the left and right of the string.
     void trimWhitespace(void);
-    /// Trims all occurrences of the given character to the left and right of the string.
     void trim(wchar_t character);
 
     void trimLeft(wchar_t ch);
@@ -232,32 +208,17 @@ public:
     void trimRight(const wchar_t* pos);
 
     StackString<N, wchar_t>& trim(void);
-    /// removes all white space chars in front of string.
     StackString<N, wchar_t>& trimLeft(void);
-    // removes any leading white space chars.
     StackString<N, wchar_t>& trimRight(void);
 
-    /// \brief strip char from end as many times as the char occurs
     inline void stripTrailing(const wchar_t c);
-
-    /// removes the color codes ^1 ..
     inline void stripColorCodes(void);
-
-    /// \brief Clears the string such that GetLength() yields 0.
-    /// \remark No assumptions about the characters stored in the internal array should be made.
     inline void clear(void);
 
-    /// Returns whether the string equals a given string.
     inline bool isEqual(const wchar_t* other) const;
 
-    /// \brief Finds a character in the string, and returns a pointer to the last occurrence of the character.
-    /// \remark Returns a \c nullptr if the character could not be found.
     inline const wchar_t* findLast(wchar_t ch) const;
-    /// \brief Finds a character in the string, and returns a pointer to the first occurrence of the character.
-    /// \remark Returns a \c nullptr if the character could not be found.
     inline const wchar_t* find(wchar_t ch) const;
-    /// \brief Finds a string inside the string, and returns a pointer to it.
-    /// \remark Returns a \c nullptr if the string could not be found.
     inline const wchar_t* find(const wchar_t* string) const;
 
     inline const wchar_t* findCaseInsen(wchar_t ch) const;
