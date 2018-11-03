98e2ad7ca7221b8974c6b94c229beaa1998d9e36
Tom Crowley <tom@tom-crowley.co.uk>
2018-11-03 19:16:39 +0000

    Text needs to be null terminated.

--- Pulp/Common/Platform/win32/ClipBoard.cpp
+++ Pulp/Common/Platform/win32/ClipBoard.cpp
@@ -12,10 +12,11 @@ namespace clipboard
         X_ASSERT(pBegin <= pEnd, "Invalid range")(pBegin, pEnd);
 
         const size_t strLen = union_cast<size_t>(pEnd - pBegin);
-        HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strLen);
+        HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strLen + 1);
         LPSTR pDst = reinterpret_cast<LPSTR>(GlobalLock(hMem));
 
         std::memcpy(pDst, pBegin, strLen);
+        pDst[strLen + 1] = '\0';
 
         if (GlobalUnlock(hMem) != 0 && core::lastError::Get() != ERROR_NOT_LOCKED) {
             X_WARNING("Clipboard", "failed to unlock clipboard memory. Error: %s", core::lastError::ToString(Dsc));
