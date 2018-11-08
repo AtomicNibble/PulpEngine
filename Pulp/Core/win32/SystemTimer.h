2ee8f60a18071568a1eb573374d87fb02b331e96
Tom Crowley <tom@tom-crowley.co.uk>
2018-11-08 20:47:07 +0000

    Remove pointless comments

--- Pulp/Core/win32/SystemTimer.h
+++ Pulp/Core/win32/SystemTimer.h
@@ -7,7 +7,6 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup Platform
 /// \brief high-res timer for genral game use.
 /// \details Returns the value as a 64bit int for the following reasons: http://www.altdevblogaday.com/2012/02/05/dont-store-that-in-a-float/
 /// ( basically float percision sucks after game running for long time )
@@ -15,22 +14,13 @@ namespace SysTimer
 {
     typedef core::traits::Function<int64(void)> TimeUpdateFunc;
 
-    /// \brief Starts the time stamp mechanism.
-    /// \remark This is called automatically when starting the Core module.
     void Startup(void);
-
-    /// \brief Shuts down the time stamp mechanism.
-    /// \remark This is called automatically when starting the Core module.
     void Shutdown(void);
 
-    /// \brief Returns the current value of the high-resolution performance counter.
-    /// \remark This is not the same as actual CPU cycles.
+
     int64_t Get(void);
 
-    /// Converts counts returned by Get() into seconds.
     X_INLINE float ToSeconds(int64_t count);
-
-    /// Converts counts returned by Get() into milliseconds.
     X_INLINE float ToMilliSeconds(int64_t count);
 
     X_INLINE int64_t fromSeconds(float value);
