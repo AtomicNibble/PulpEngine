78732a20241ae423b8863ecd18a3aabccd6d0f2e
Tom Crowley <tom@tom-crowley.co.uk>
2018-12-23 22:14:39 +0000

    Template some of the tests with types.

--- Pulp/UnitTests/UT_C_FixedHashTable.cpp
+++ Pulp/UnitTests/UT_C_FixedHashTable.cpp
@@ -4,8 +4,10 @@
 
 X_USING_NAMESPACE;
 
+
 namespace
 {
+
     // These are used for checking we can use string key for int hash table.
     struct HashIntStr
     {
@@ -27,6 +29,8 @@ namespace
         }
     };
     
+    template<typename T>
+    using HashTableT16 = core::FixedHashTableStack<16, T, T>;
 
     template<size_t N>
     using HashTableIntIntN = core::FixedHashTableStack<N, int, int>;
@@ -37,22 +41,32 @@ namespace
 
 } // namespace
 
+typedef ::testing::Types<short, int, float> MyTypes;
+TYPED_TEST_CASE(FixedHashTable, MyTypes);
 
-TEST(FixedHashTable, MoveConstruct)
+template<typename T>
+class FixedHashTable : public ::testing::Test
 {
-    HashTableIntInt16 ht0;
-    ht0[1] = 1;
+public:
+};
 
-    HashTableIntInt16 ht1(std::move(ht0));
+TYPED_TEST(FixedHashTable, MoveConstruct)
+{
+    HashTableT16<TypeParam> ht0;
+    ht0[(TypeParam)1] = (TypeParam)1;
+
+    HashTableT16<TypeParam> ht1(std::move(ht0));
 
     EXPECT_TRUE(ht1.isNotEmpty());
     EXPECT_EQ(1, ht1.size());
     EXPECT_EQ(1, ht1[1]);
 }
 
-TEST(FixedHashTable, Iterator)
+TYPED_TEST(FixedHashTable, Iterator)
 {
-    HashTableIntIntN<128> ht;
+    using HashTable = core::FixedHashTableStack<128, TypeParam, TypeParam>;
+
+    HashTable ht;
     const auto& cht = ht;
 
     EXPECT_TRUE(ht.begin() == ht.end());
@@ -72,12 +86,12 @@ TEST(FixedHashTable, Iterator)
     EXPECT_FALSE(cit != ht.end());
 
     for (int i = 1; i < 100; ++i) {
-        ht[i] = i;
+        ht[(TypeParam)i] = (TypeParam)i;
     }
 
     std::array<bool, 100> visited = {};
     for (auto it = ht.begin(); it != ht.end(); ++it) {
-        visited[it->first] = true;
+        visited[(int)it->first] = true;
     }
 
     for (int i = 1; i < 100; ++i) {
@@ -91,25 +105,24 @@ TEST(FixedHashTable, Iterator)
     ));
 }
 
-
-TEST(FixedHashTable, Capacity)
+TYPED_TEST(FixedHashTable, Capacity)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
     const auto &cht = ht;
     EXPECT_FALSE(cht.isNotEmpty());
     EXPECT_TRUE(cht.isEmpty());
     EXPECT_EQ(0, cht.size());
-    ht[1] = 1;
+    ht[(TypeParam)1] = (TypeParam)1;
     EXPECT_TRUE(cht.isNotEmpty());
     EXPECT_FALSE(cht.isEmpty());
 }
 
-TEST(FixedHashTable, Clear)
+TYPED_TEST(FixedHashTable, Clear)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
-    ht[1] = 1;
+    ht[(TypeParam)1] = (TypeParam)1;
     ht.clear();
     EXPECT_TRUE(ht.isEmpty());
     EXPECT_TRUE(ht.size() == 0);
@@ -117,11 +130,11 @@ TEST(FixedHashTable, Clear)
     EXPECT_TRUE(ht.cbegin() == ht.cend());
 }
 
-TEST(FixedHashTable, Insert)
+TYPED_TEST(FixedHashTable, Insert)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
-    auto res = ht.insert({ 1, 1 }); // xvalue
+    auto res = ht.insert({ (TypeParam)1, (TypeParam)1 }); // xvalue
 
     EXPECT_FALSE(ht.isEmpty());
 
@@ -133,7 +146,7 @@ TEST(FixedHashTable, Insert)
     EXPECT_EQ(1, res.first->second);
     EXPECT_TRUE(res.second);
 
-    const auto v = std::make_pair(1, 2);
+    const auto v = std::make_pair((TypeParam)1, (TypeParam)2);
     auto res2 = ht.insert(v); // rvalue
 
     EXPECT_EQ(1, ht.size());
@@ -143,11 +156,11 @@ TEST(FixedHashTable, Insert)
     EXPECT_FALSE(res2.second);
 }
 
-TEST(FixedHashTable, Emplace)
+TYPED_TEST(FixedHashTable, Emplace)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
-    auto res = ht.emplace(1, 1); // xvalue
+    auto res = ht.emplace((TypeParam)1, (TypeParam)1); // xvalue
     EXPECT_FALSE(ht.isEmpty());
     EXPECT_EQ(1, ht.size());
     EXPECT_TRUE(ht.begin() != ht.end());
@@ -157,7 +170,7 @@ TEST(FixedHashTable, Emplace)
     EXPECT_EQ(1, res.first->second);
     EXPECT_TRUE(res.second);
 
-    auto res2 = ht.emplace(1, 2);
+    auto res2 = ht.emplace((TypeParam)1, (TypeParam)2);
     EXPECT_EQ(1, ht.size());
     EXPECT_TRUE(res2.first == res.first);
     EXPECT_EQ(1, res2.first->first);
@@ -165,11 +178,11 @@ TEST(FixedHashTable, Emplace)
     EXPECT_FALSE(res2.second);
 }
 
-TEST(FixedHashTable, EraseIterator)
+TYPED_TEST(FixedHashTable, EraseIterator)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
-    auto res = ht.emplace(1, 1);
+    auto res = ht.emplace((TypeParam)1, (TypeParam)1);
     ht.erase(res.first);
     EXPECT_TRUE(ht.isEmpty());
     EXPECT_FALSE(ht.isNotEmpty());
@@ -178,12 +191,12 @@ TEST(FixedHashTable, EraseIterator)
     EXPECT_TRUE(ht.cbegin() == ht.cend());
 }
 
-TEST(FixedHashTable, EraseKey)
+TYPED_TEST(FixedHashTable, EraseKey)
 {
-    HashTableIntInt16 ht;
-    EXPECT_EQ(0, ht.erase(1));
-    ht[1] = 1;
-    EXPECT_EQ(1, ht.erase(1));
+    HashTableT16<TypeParam> ht;
+    EXPECT_EQ(0, ht.erase((TypeParam)1));
+    ht[(TypeParam)1] = (TypeParam)1;
+    EXPECT_EQ(1, ht.erase((TypeParam)1));
 
     EXPECT_TRUE(ht.isEmpty());
     EXPECT_FALSE(ht.isNotEmpty());
@@ -206,10 +219,10 @@ TEST(FixedHashTable, EraseKeyT)
     EXPECT_TRUE(ht.cbegin() == ht.cend());
 }
 
-TEST(FixedHashTable, OperatorIndex)
+TYPED_TEST(FixedHashTable, OperatorIndex)
 {
-    HashTableIntInt16 ht;
-    ht[1] = 1;
+    HashTableT16<TypeParam> ht;
+    ht[(TypeParam)1] = (TypeParam)1;
     EXPECT_TRUE(ht.isNotEmpty());
 
     EXPECT_EQ(1, ht.size());
@@ -218,26 +231,26 @@ TEST(FixedHashTable, OperatorIndex)
     EXPECT_EQ(1, ht[1]);
 }
 
-TEST(FixedHashTable, Find)
+TYPED_TEST(FixedHashTable, Find)
 {
-    HashTableIntInt16 ht;
+    HashTableT16<TypeParam> ht;
 
     const auto &cht = ht;
-    ht[1] = 1;
+    ht[(TypeParam)1] = (TypeParam)1;
     {
-        auto it = ht.find(1);
+        auto it = ht.find((TypeParam)1);
         EXPECT_TRUE(it != ht.end());
         EXPECT_EQ(1, it->first);
         EXPECT_EQ(1, it->second);
-        it = ht.find(2);
+        it = ht.find((TypeParam)2);
         EXPECT_TRUE(it == ht.end());
     }
     {
-        auto it = cht.find(1);
+        auto it = cht.find((TypeParam)1);
         EXPECT_TRUE(it != cht.end());
         EXPECT_EQ(1, it->first);
         EXPECT_EQ(1, it->second);
-        it = cht.find(2);
+        it = cht.find((TypeParam)2);
         EXPECT_TRUE(it == cht.end());
     }
 }
