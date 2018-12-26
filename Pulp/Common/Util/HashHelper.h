fc4e10de7a2c9e19e88fed1a197c1cbbc120cac0
Tom Crowley <tom@tom-crowley.co.uk>
2018-12-26 11:47:07 +0000

    Style update

--- Pulp/Common/Util/HashHelper.h
+++ Pulp/Common/Util/HashHelper.h
@@ -28,70 +28,70 @@ struct hash : public _Bitwise_hash<_Ktype>
 template<>
 struct hash<core::string>
 {
-    size_t operator()(const core::string& __s) const
+    size_t operator()(const core::string& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.data(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.data(), str.length());
     }
 
-    size_t operator()(const core::Path<char>& __s) const
+    size_t operator()(const core::Path<char>& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.data(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.data(), str.length());
     }
 
-    size_t operator()(const char* const __s) const
+    size_t operator()(const char* const str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s, strlen(__s));
+        return (size_t)core::Hash::Fnv1aHash(str, strlen(str));
     }
 };
 
 template<size_t N>
 struct hash<core::StackString<N>>
 {
-    size_t operator()(const core::StackString<N>& __s) const
+    size_t operator()(const core::StackString<N>& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.c_str(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.c_str(), str.length());
     }
 
-    size_t operator()(const core::Path<char>& __s) const
+    size_t operator()(const core::Path<char>& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.data(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.data(), str.length());
     }
 
-    size_t operator()(const core::string& __s) const
+    size_t operator()(const core::string& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.data(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.data(), str.length());
     }
 };
 
 template<typename CharT>
 struct hash<core::Path<CharT>>
 {
-    size_t operator()(const core::Path<CharT>& __s) const
+    size_t operator()(const core::Path<CharT>& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.c_str(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.c_str(), str.length());
     }
 };
 
 template<>
 struct hash<const char*>
 {
-    size_t operator()(const char* const __s) const
+    size_t operator()(const char* const str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s, strlen(__s));
+        return (size_t)core::Hash::Fnv1aHash(str, strlen(str));
     }
 
-    size_t operator()(const core::string& __s) const
+    size_t operator()(const core::string& str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s.data(), __s.length());
+        return (size_t)core::Hash::Fnv1aHash(str.data(), str.length());
     }
 };
 
 template<>
 struct hash<const char* const>
 {
-    size_t operator()(const char* const __s) const
+    size_t operator()(const char* const str) const
     {
-        return (size_t)core::Hash::Fnv1aHash(__s, strlen(__s));
+        return (size_t)core::Hash::Fnv1aHash(str, strlen(str));
     }
 };
 
@@ -100,28 +100,28 @@ struct hash<const char* const>
 template<class _Type = void>
 struct equal_to
 {
-    bool operator()(const _Type& _Left, const _Type& _Right) const
+    bool operator()(const _Type& lhs, const _Type& rhs) const
     {
-        return (_Left == _Right);
+        return (lhs == rhs);
     }
 };
 
 template<>
 struct equal_to<core::string>
 {
-    bool operator()(const core::string& _Left, const core::string& _Right) const
+    bool operator()(const core::string& lhs, const core::string& rhs) const
     {
-        return (_Left == _Right);
+        return (lhs == rhs);
     }
 
-    bool operator()(const core::string& _Left, const core::Path<char>& _Right) const
+    bool operator()(const core::string& lhs, const core::Path<char>& rhs) const
     {
-        return core::strUtil::IsEqual(_Left.begin(), _Left.end(), _Right.begin(), _Right.end());
+        return core::strUtil::IsEqual(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
     }
 
-    bool operator()(const core::string& _Left, const char* const _Right) const
+    bool operator()(const core::string& lhs, const char* const rhs) const
     {
-        return _Left.compare(_Right);
+        return lhs.compare(rhs);
     }
 };
 
@@ -129,56 +129,56 @@ struct equal_to<core::string>
 template<size_t N>
 struct equal_to<core::StackString<N>>
 {
-    bool operator()(const core::StackString<N>& _Left, const core::StackString<N>& _Right) const
+    bool operator()(const core::StackString<N>& lhs, const core::StackString<N>& rhs) const
     {
-        return (_Left == _Right);
+        return (lhs == rhs);
     }
 
-    bool operator()(const core::StackString<N>& _Left, core::Path<char>& _Right) const
+    bool operator()(const core::StackString<N>& lhs, core::Path<char>& rhs) const
     {
-        return core::strUtil::IsEqual(_Left.begin(), _Left.end(), _Right.begin(), _Right.end());
+        return core::strUtil::IsEqual(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
     }
 
-    bool operator()(const core::StackString<N>& _Left, const core::string& _Right) const
+    bool operator()(const core::StackString<N>& lhs, const core::string& rhs) const
     {
-        return core::strUtil::IsEqual(_Left.begin(), _Left.end(), _Right.begin(), _Right.end());
+        return core::strUtil::IsEqual(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
     }
 };
 
 template<typename CharT>
 struct equal_to<core::Path<CharT>>
 {
-    bool operator()(const core::Path<CharT>& _Left, const core::Path<CharT>& _Right) const
+    bool operator()(const core::Path<CharT>& lhs, const core::Path<CharT>& rhs) const
     {
-        return (_Left == _Right);
+        return (lhs == rhs);
     }
 };
 
 template<>
 struct equal_to<const char*>
 {
-    bool operator()(const char* const _Left, const char* const _Right) const
+    bool operator()(const char* const lhs, const char* const rhs) const
     {
-        return core::strUtil::IsEqual(_Left, _Right);
+        return core::strUtil::IsEqual(lhs, rhs);
     }
 
-    bool operator()(const char* const _Left, const core::string& _Right) const
+    bool operator()(const char* const lhs, const core::string& rhs) const
     {
-        return core::strUtil::IsEqual(_Right.begin(), _Right.end(), _Left);
+        return core::strUtil::IsEqual(rhs.begin(), rhs.end(), lhs);
     }
 };
 
 template<>
 struct equal_to<const char* const>
 {
-    bool operator()(const char* const _Left, const char* const _Right) const
+    bool operator()(const char* const lhs, const char* const rhs) const
     {
-        return core::strUtil::IsEqual(_Left, _Right);
+        return core::strUtil::IsEqual(lhs, rhs);
     }
 
-    bool operator()(const char* const _Left, const core::string& _Right) const
+    bool operator()(const char* const lhs, const core::string& rhs) const
     {
-        return core::strUtil::IsEqual(_Right.begin(), _Right.end(), _Left);
+        return core::strUtil::IsEqual(rhs.begin(), rhs.end(), lhs);
     }
 };
 
@@ -191,47 +191,47 @@ struct equal_to_case_insen
 template<>
 struct equal_to_case_insen<core::string>
 {
-    bool operator()(const core::string& _Left, const core::string& _Right) const
+    bool operator()(const core::string& lhs, const core::string& rhs) const
     {
-        return _Left.compareCaseInsen(_Right);
+        return lhs.compareCaseInsen(rhs);
     }
 
-    bool operator()(const core::string& _Left, const core::Path<char>& _Right) const
+    bool operator()(const core::string& lhs, const core::Path<char>& rhs) const
     {
-        return core::strUtil::IsEqualCaseInsen(_Left.begin(), _Left.end(), _Right.begin(), _Right.end());
+        return core::strUtil::IsEqualCaseInsen(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
     }
 
-    bool operator()(const core::string& _Left, const char* const _Right) const
+    bool operator()(const core::string& lhs, const char* const rhs) const
     {
-        return _Left.compareCaseInsen(_Right);
+        return lhs.compareCaseInsen(rhs);
     }
 };
 
 template<>
 struct equal_to_case_insen<const char*>
 {
-    bool operator()(const char* const _Left, const char* const _Right) const
+    bool operator()(const char* const lhs, const char* const rhs) const
     {
-        return core::strUtil::IsEqualCaseInsen(_Left, _Right);
+        return core::strUtil::IsEqualCaseInsen(lhs, rhs);
     }
 
-    bool operator()(const char* const _Left, const core::string& _Right) const
+    bool operator()(const char* const lhs, const core::string& rhs) const
     {
-        return core::strUtil::IsEqualCaseInsen(_Right.begin(), _Right.end(), _Left);
+        return core::strUtil::IsEqualCaseInsen(rhs.begin(), rhs.end(), lhs);
     }
 };
 
 template<>
 struct equal_to_case_insen<const char* const>
 {
-    bool operator()(const char* const _Left, const char* const _Right) const
+    bool operator()(const char* const lhs, const char* const rhs) const
     {
-        return core::strUtil::IsEqualCaseInsen(_Left, _Right);
+        return core::strUtil::IsEqualCaseInsen(lhs, rhs);
     }
 
-    bool operator()(const char* const _Left, const core::string& _Right) const
+    bool operator()(const char* const lhs, const core::string& rhs) const
     {
-        return core::strUtil::IsEqualCaseInsen(_Right.begin(), _Right.end(), _Left);
+        return core::strUtil::IsEqualCaseInsen(rhs.begin(), rhs.end(), lhs);
     }
 };
 
