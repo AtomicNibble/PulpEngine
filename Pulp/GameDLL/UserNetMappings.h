caec3e137a206a5acfb10c03d508cba1cb0b935b
Tom Crowley <tom@tom-crowley.co.uk>
2018-12-29 18:01:09 +0000

    Style updates.

--- Pulp/GameDLL/UserNetMappings.h
+++ Pulp/GameDLL/UserNetMappings.h
@@ -4,7 +4,6 @@
 
 X_NAMESPACE_BEGIN(game)
 
-
 struct UserNetMappings
 {
     using PlayerGuidArr = std::array<net::NetGUID, net::MAX_PLAYERS>;
@@ -16,13 +15,15 @@ public:
     {
     }
 
-    void reset() {
+    void reset()
+    {
         localPlayerIdx = -1;
         lobbyUserGuids.fill(net::NetGUID());
         sysHandles.fill(net::INVALID_SYSTEM_HANDLE);
     }
 
-    void resetIndex(int32_t idx) {
+    void resetIndex(int32_t idx)
+    {
         lobbyUserGuids[idx] = net::NetGUID();
         sysHandles[idx] = net::INVALID_SYSTEM_HANDLE;
 
@@ -32,7 +33,8 @@ public:
         }
     }
 
-    void addUser(int32_t idx, net::UserInfo& info) {
+    void addUser(int32_t idx, net::UserInfo& info)
+    {
         X_ASSERT(!lobbyUserGuids[idx].isValid(), "Slow taken")(idx);
 
         lobbyUserGuids[idx] = info.guid;
@@ -43,12 +45,10 @@ public:
         }
     }
 
-    int32_t findFreeSlot(void) const {
-
-        for (int32_t i = 0; i < static_cast<int32_t>(lobbyUserGuids.size()); i++)
-        {
-            if (!lobbyUserGuids[i].isValid())
-            {
+    int32_t findFreeSlot(void) const
+    {
+        for (int32_t i = 0; i < static_cast<int32_t>(lobbyUserGuids.size()); i++) {
+            if (!lobbyUserGuids[i].isValid()) {
                 return i;
             }
         }
@@ -56,13 +56,15 @@ public:
         return -1;
     }
 
-    int32_t getNumUsers(void) const {
+    int32_t getNumUsers(void) const
+    {
         return safe_static_cast<int32_t>(std::count_if(lobbyUserGuids.begin(), lobbyUserGuids.end(), [](const net::NetGUID& guid) -> bool {
             return guid.isValid();
         }));
     }
 
-    int32_t getPlayerIdxForGuid(net::NetGUID guid) const {
+    int32_t getPlayerIdxForGuid(net::NetGUID guid) const
+    {
         auto it = std::find(lobbyUserGuids.begin(), lobbyUserGuids.end(), guid);
         if (it != lobbyUserGuids.end()) {
             auto idx = std::distance(lobbyUserGuids.begin(), it);
@@ -72,7 +74,8 @@ public:
         return -1;
     }
 
-    bool guidPresent(net::NetGUID guid) const {
+    bool guidPresent(net::NetGUID guid) const
+    {
         return std::find(lobbyUserGuids.begin(), lobbyUserGuids.end(), guid) != lobbyUserGuids.end();
     }
 
@@ -88,6 +91,5 @@ public:
     PlayerGuidArr lobbyUserGuids;
     SystemHandleArr sysHandles;
 };
-
 
 X_NAMESPACE_END
