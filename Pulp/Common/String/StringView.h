ac996bb69a466f7e63ae1e57f35cd4cdcb40bb60
Tom Crowley <tom@tom-crowley.co.uk>
2019-01-12 23:41:49 +0000

    Remove std::string_view and replace it with a simple none templated type. Also don't make construction of string literal implicit. And add some helper constructors for some engine types.

--- Pulp/Common/String/StringView.h
+++ Pulp/Common/String/StringView.h
@@ -1,11 +1,136 @@
 #pragma once
 
-#include <string_view>
+#include <String/StringUtil.h>
 
 X_NAMESPACE_BEGIN(core)
 
-using string_view = std::string_view;
+template<size_t N, typename TChar>
+class StackString;
 
-namespace string_view_literals = std::string_view_literals;
+
+class string_view
+{
+public:
+    using value_type = char;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type &;
+    using const_reference = const value_type&;
+    using const_iterator = const_pointer;
+    using iterator = const_iterator;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+
+public:
+    constexpr string_view();
+    constexpr string_view(const string_view&) = default;
+    constexpr explicit string_view(const_pointer pStr);
+    constexpr string_view(const_pointer pStr, size_type length);
+    constexpr string_view(const_pointer pBegin, const_pointer pEnd);
+
+    template<size_t N>
+    constexpr string_view(const core::StackString<N, value_type>& str);
+
+    constexpr string_view& operator=(const string_view&) = default;
+
+    constexpr const_reference operator[](const size_type idx) const;
+    constexpr const_reference at(const size_type idx) const;
+
+    constexpr size_type size() const;
+    constexpr size_type length() const;
+    constexpr bool empty() const;
+    constexpr const_pointer data() const;
+
+    constexpr const_iterator begin() const;
+    constexpr const_iterator end() const;
+
+private:
+    const_pointer pBegin_;
+    size_type size_;
+};
+
+
+constexpr string_view::string_view() :
+    pBegin_(nullptr),
+    size_(0)
+{
+}
+
+constexpr string_view::string_view(const_pointer pStr) :
+    pBegin_(pStr),
+    size_(__builtin_strlen(pStr)) // Visual studio seams to have this?
+{
+}
+
+constexpr string_view::string_view(const_pointer pStr, size_type length) :
+    pBegin_(pStr),
+    size_(length)
+{
+}
+
+constexpr string_view::string_view(const_pointer pBegin, const_pointer pEnd) :
+    pBegin_(pBegin),
+    size_(pBegin - pEnd)
+{
+}
+
+
+template<size_t N>
+constexpr string_view::string_view(const core::StackString<N, value_type>& str) :
+    pBegin_(str.data()),
+    size_(str.length())
+{
+}
+
+
+constexpr typename string_view::const_reference string_view::operator[](const size_type idx) const
+{
+    return *(pBegin_ + idx);
+}
+
+constexpr typename string_view::const_reference string_view::at(const size_type idx) const
+{
+    return *(pBegin_ + idx);
+}
+
+constexpr string_view::size_type string_view::size() const
+{
+    return size_;
+}
+
+constexpr string_view::size_type string_view::length() const
+{
+    return size_;
+}
+
+constexpr bool string_view::empty() const
+{
+    return size_ == 0;
+}
+
+constexpr typename string_view::const_pointer string_view::data() const
+{
+    return pBegin_;
+}
+
+constexpr typename string_view::const_iterator string_view::begin() const
+{
+    return pBegin_;
+}
+
+constexpr typename string_view::const_iterator string_view::end() const
+{
+    return pBegin_ + size_;
+}
+
+
+namespace string_view_literals
+{
+    X_NO_DISCARD constexpr string_view operator"" _sv(const char* pStr, size_t length) noexcept
+    {
+        return string_view(pStr, length);
+    }
+
+} // namespace string_view_literals 
 
 X_NAMESPACE_END
