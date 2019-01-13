9f8e23fbc6c5daa049e1ad85c0ad314b6b238813
Tom Crowley <tom@tom-crowley.co.uk>
2019-01-13 18:20:48 +0000

    Remove some pointless comments.

--- Pulp/Common/Memory/ThreadPolicies/MultiThreadPolicy.h
+++ Pulp/Common/Memory/ThreadPolicies/MultiThreadPolicy.h
@@ -5,36 +5,15 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup Memory
-/// \brief A class that implements a threading policy for memory arenas.
-/// \details This class implements the concepts of a threading policy as expected by the MemoryArena class. It is
-/// intended to be used by multi-threaded arenas, and works with different synchronization primitives.
-///
-/// The concepts expected from the template class \c SynchronizationPrimitive are the following:
-/// - It must implement a method \verbatim void Enter(void);\endverbatim that enters/locks the primitive.
-/// - It must implement a method \verbatim void Leave(void);\endverbatim that leaves/unlocks the primitive.
-///
-/// As an example, a CriticalSection can be used as a synchronization primitive with the MultiThreadPolicy:
-/// \code
-///   // define a multi-threaded policy
-///   typedef core::MultiThreadPolicy<core::CriticalSection> ThreadSafePolicy;
-///
-///   // defines a multi-threaded arena that uses a pool allocator, and simple bounds checking, tracking, and tagging
-///   typedef core::MemoryArena<core::PoolAllocator, ThreadSafePolicy, core::SimpleBoundsChecking, core::SimpleMemoryTracking, core::SimpleMemoryTagging> PoolArena;
-/// \endcode
-/// \sa SingleThreadPolicy
+
 template<class SynchronizationPrimitive>
 class MultiThreadPolicy
 {
 public:
-    /// A human-readable string literal containing the policy's type.
     static const char* const TYPE_NAME;
     static const bool IS_THREAD_SAFE = true;
 
-    /// Enters the synchronization primitive.
     inline void Enter(void);
-
-    /// Leaves the synchronization primitive.
     inline void Leave(void);
 
 private:
--- Pulp/Common/Memory/ThreadPolicies/SingleThreadPolicy.h
+++ Pulp/Common/Memory/ThreadPolicies/SingleThreadPolicy.h
@@ -5,24 +5,17 @@
 
 X_NAMESPACE_BEGIN(core)
 
-/// \ingroup Memory
-/// \brief A class that implements a threading policy for memory arenas.
-/// \details This class implements the concepts of a threading policy as expected by the MemoryArena class. It is
-/// intended to be used by single-threaded arenas, and only contains empty implementations.
-/// \sa MultiThreadPolicy
+
 class SingleThreadPolicy
 {
 public:
-    /// A human-readable string literal containing the policy's type.
     static const char* const TYPE_NAME;
     static const bool IS_THREAD_SAFE = false;
 
-    /// Empty implementation.
     inline void Enter(void) const
     {
     }
 
-    /// Empty implementation.
     inline void Leave(void) const
     {
     }
