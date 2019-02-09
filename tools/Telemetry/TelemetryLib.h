32fa674ddf1aaa185f746fc431fa22721578c324
Tom Crowley <tom@tom-crowley.co.uk>
2019-02-09 18:28:40 +0000

    Add new lib for Telemetry.

---
+++ tools/Telemetry/TelemetryLib.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef X_TELEMETRY_LIB_H_
+#define X_TELEMETRY_LIB_H_
+
+#include <Core\Compiler.h>
+
+#ifndef TELEMETRYLIB_EXPORT
+#ifdef X_LIB
+#define TELEMETRYLIB_EXPORT
+#else
+#ifdef TELEMETRY_LIB_EXPORT
+#define TELEMETRYLIB_EXPORT X_EXPORT
+#else
+#define TELEMETRYLIB_EXPORT X_IMPORT
+#endif // !TELEMETRY_LIB_EXPORT
+#endif // X_LIB
+#endif // !TELEMETRYLIB_EXPORT
+
+
+#include "Telemetry.h"
+
+#endif // !X_TELEMETRY_LIB_H_
---
+++ tools/Telemetry/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <EngineCommon.h>
+#include <Extension\FactoryRegNode.h>
+
+
+#ifdef X_LIB
+#define LINKERLIB_EXPORT 
+#else
+#ifdef LINKER_LIB_EXPORT 
+#define LINKERLIB_EXPORT X_EXPORT
+#else
+#define LINKERLIB_EXPORT X_IMPORT
+#endif // !LINKERLIB_EXPORT 
+#endif // X_LIB
