5145e916bf8717e5b7f36dbc878b9502c4bdee2f
Tom Crowley <tom@tom-crowley.co.uk>
2019-02-10 14:50:00 +0000

    Remove old macros.

--- tools/Telemetry/stdafx.h
+++ tools/Telemetry/stdafx.h
@@ -4,14 +4,3 @@
 #include <../TelemetryCommon/TelemetryCommonLib.h>
 
 // --------------------------------------
-
-
-#ifdef X_LIB
-#define LINKERLIB_EXPORT 
-#else
-#ifdef LINKER_LIB_EXPORT 
-#define LINKERLIB_EXPORT X_EXPORT
-#else
-#define LINKERLIB_EXPORT X_IMPORT
-#endif // !LINKERLIB_EXPORT 
-#endif // X_LIB
