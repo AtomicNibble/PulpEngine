6303faaa42bbfd960ee1a43bcb4bd5e3efe6a834
Tom Crowley <tom@tom-crowley.co.uk>
2019-02-15 18:30:36 +0000

    Move some stuff to util.

--- tools/Telemetry/Util.h
+++ tools/Telemetry/Util.h
@@ -32,4 +32,51 @@ TELEM_INLINE constexpr T RoundDownToMultiple(T numToRound, T multipleOf)
     return numToRound & ~(multipleOf - 1);
 }
 
+TELEM_INLINE void* AlignTop(void* ptr, tt_size alignment)
+{
+    union
+    {
+        void* as_void;
+        tt_uintptr as_uintptr_t;
+    };
 
+    const tt_size mask = alignment - 1;
+    as_void = ptr;
+    as_uintptr_t += mask;
+    as_uintptr_t &= ~mask;
+    return as_void;
+}
+
+template<typename T>
+TELEM_INLINE constexpr bool IsAligned(T value, unsigned int alignment, unsigned int offset)
+{
+    return ((value + offset) % alignment) == 0;
+}
+
+TELEM_INLINE bool Convert(const wchar_t* pSrc, tt_int32 srcSize,
+    char* output, tt_int32 outputBytes, tt_int32& lengthOut)
+{
+    lengthOut = 0;
+    if (!outputBytes) {
+        return false;
+    }
+
+    const tt_int32 bytesWritten = WideCharToMultiByte(
+        CP_UTF8,
+        0,
+        pSrc,
+        srcSize,
+        output,
+        outputBytes,
+        nullptr,
+        nullptr
+    );
+
+    if (bytesWritten == 0) {
+        // error
+        return false;
+    }
+
+    lengthOut = bytesWritten;
+    return true;
+}
