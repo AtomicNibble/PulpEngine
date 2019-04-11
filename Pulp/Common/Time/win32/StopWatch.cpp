b2088746066b0eb4c6643d5503924231748d3169
Tom Crowley <tom@tom-crowley.co.uk>
2019-04-11 22:53:13 +0100

    Remove QueryPerformanceFrequency fallback.

--- Pulp/Common/Time/win32/StopWatch.cpp
+++ Pulp/Common/Time/win32/StopWatch.cpp
@@ -11,9 +11,6 @@
 #define FAR far
 #endif
 
-#include "Mmsystem.h"
-// Winmm.lib is delay loaded.
-
 X_NAMESPACE_BEGIN(core)
 
 namespace
@@ -26,19 +23,13 @@ namespace
         SysTimer();
 
     public:
-        void StartUp(void);
+        bool StartUp(void);
 
         int64_t Get(void);
         float ToSeconds(int64_t count);
         float ToMilliSeconds(int64_t count);
 
     private:
-        static int64_t PerformanceCounterTime(void);
-        static int64_t MMTimeGet(void);
-
-    private:
-        TimeUpdateFunc::Pointer pUpdateFunc_;
-
         float oneOverFrequency_;
         float thousandOverFrequency_;
         int64_t frequency_;
@@ -46,7 +37,6 @@ namespace
 
     SysTimer::SysTimer()
     {
-        pUpdateFunc_ = nullptr;
         oneOverFrequency_ = 0.f;
         thousandOverFrequency_ = 0.f;
 
@@ -55,28 +45,28 @@ namespace
         StartUp();
     }
 
-    void SysTimer::StartUp(void)
+    bool SysTimer::StartUp(void)
     {
         LARGE_INTEGER frequency;
 
-        if (QueryPerformanceFrequency(&frequency)) {
-            frequency_ = frequency.QuadPart;
-            pUpdateFunc_ = &PerformanceCounterTime;
+        if (!QueryPerformanceFrequency(&frequency)) {
+            return false;
         }
-        else {
-            frequency_ = 1000;
-            pUpdateFunc_ = &MMTimeGet;
-        }
+
+        frequency_ = frequency.QuadPart;
 
         double resolution = 1.0 / static_cast<double>(frequency.QuadPart);
 
         oneOverFrequency_ = static_cast<float>(resolution);
         thousandOverFrequency_ = static_cast<float>(resolution * 1000.0);
+        return true;
     }
 
     X_INLINE int64_t SysTimer::Get(void)
     {
-        return pUpdateFunc_();
+        LARGE_INTEGER now = {};
+        QueryPerformanceCounter(&now);
+        return static_cast<int64_t>(now.QuadPart);
     }
 
     X_INLINE float SysTimer::ToSeconds(int64_t count)
@@ -87,20 +77,6 @@ namespace
     X_INLINE float SysTimer::ToMilliSeconds(int64_t count)
     {
         return static_cast<float>(count * thousandOverFrequency_);
-    }
-
-    // ===================================================
-
-    X_INLINE int64_t SysTimer::PerformanceCounterTime(void)
-    {
-        LARGE_INTEGER now = {};
-        QueryPerformanceCounter(&now);
-        return static_cast<int64_t>(now.QuadPart);
-    }
-
-    X_INLINE int64_t SysTimer::MMTimeGet(void)
-    {
-        return static_cast<int64_t>(timeGetTime());
     }
 
     SysTimer g_sysTimer;
