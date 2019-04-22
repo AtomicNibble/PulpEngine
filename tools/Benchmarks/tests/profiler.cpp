68a872d941c4925d686750c140feb1de66e445dc
Tom Crowley <tom@tom-crowley.co.uk>
2019-04-22 15:13:44 +0100

    Add benchmark for printf args.

--- tools/Benchmarks/tests/profiler.cpp
+++ tools/Benchmarks/tests/profiler.cpp
@@ -12,8 +12,6 @@ void BM_telem_zone(benchmark::State& state)
 
     telem::ContexHandle ctx;
     ttInitializeContext(ctx, telemBuf, sizeof(telemBuf));
-
-
     ttSetThreadName(ctx, 0, "Main");
 
     ttTick(ctx);
@@ -30,4 +28,32 @@ void BM_telem_zone(benchmark::State& state)
     ttShutDown();
 }
 
+void BM_telem_zone_printf(benchmark::State& state)
+{
+    const size_t telemBufSize = 1024 * 1024 * 2;
+    static uint8_t telemBuf[telemBufSize];
+
+    // Setup telem.
+    X_ASSERT(ttLoadLibary(), "Failed to resolve telem")();
+    X_ASSERT(ttInit(), "Failed to init telem")();
+
+    telem::ContexHandle ctx;
+    ttInitializeContext(ctx, telemBuf, sizeof(telemBuf));
+    ttSetThreadName(ctx, 0, "Main");
+
+    ttTick(ctx);
+
+    while (state.KeepRunning())
+    {
+        ttEnter(ctx, "this is a string %s in the middle", "with format spec");
+        ttLeave(ctx);
+    }
+
+    ttClose(ctx);
+
+    ttShutdownContext(ctx);
+    ttShutDown();
+}
+
 BENCHMARK(BM_telem_zone);
+BENCHMARK(BM_telem_zone_printf);
