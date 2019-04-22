b09b82cd611ef294cdc7611dfac3c771c8d46de7
Tom Crowley <tom@tom-crowley.co.uk>
2019-04-22 15:45:10 +0100

    Use a fixture for the profiler testing and add some more tests.

--- tools/Benchmarks/tests/profiler.cpp
+++ tools/Benchmarks/tests/profiler.cpp
@@ -1,59 +1,75 @@
 #include "stdafx.h"
 
-void BM_telem_zone(benchmark::State& state)
+
+class TelemFixture : public benchmark::Fixture
 {
-    // i need to make this from something.
-    const size_t telemBufSize = 1024 * 1024 * 2;
-    static uint8_t telemBuf[telemBufSize];
-
-    // Setup telem.
-    X_ASSERT(ttLoadLibary(), "Failed to resolve telem")();
-    X_ASSERT(ttInit(), "Failed to init telem")();
-
-    telem::ContexHandle ctx;
-    ttInitializeContext(ctx, telemBuf, sizeof(telemBuf));
-    ttSetThreadName(ctx, 0, "Main");
-
-    ttTick(ctx);
-
-    while (state.KeepRunning())
+public:
+    void SetUp(const ::benchmark::State&)
     {
+        const size_t telemBufSize = 1024 * 1024 * 2;
+        static uint8_t telemBuf[telemBufSize];
+
+        X_ASSERT(ttLoadLibary(), "Failed to resolve telem")();
+        X_ASSERT(ttInit(), "Failed to init telem")();
+
+        ttInitializeContext(ctx, telemBuf, sizeof(telemBuf));
+        ttTick(ctx);
+    }
+
+    void TearDown(const ::benchmark::State&)
+    {
+        ttClose(ctx);
+        ttShutdownContext(ctx);
+        ttShutDown();
+    }
+
+protected:
+    telem::ContexHandle ctx;
+};
+
+BENCHMARK_F(TelemFixture, zone_paused)(benchmark::State& state) {
+    ttPause(ctx, true);
+
+    for (auto _ : state) {
         ttEnter(ctx, "meow");
         ttLeave(ctx);
     }
-
-    ttClose(ctx);
-
-    ttShutdownContext(ctx);
-    ttShutDown();
 }
 
-void BM_telem_zone_printf(benchmark::State& state)
-{
-    const size_t telemBufSize = 1024 * 1024 * 2;
-    static uint8_t telemBuf[telemBufSize];
+BENCHMARK_F(TelemFixture, zone_simple)(benchmark::State& state) {
+    for (auto _ : state) {
+        ttEnter(ctx, "meow");
+        ttLeave(ctx);
+    }
+}
 
-    // Setup telem.
-    X_ASSERT(ttLoadLibary(), "Failed to resolve telem")();
-    X_ASSERT(ttInit(), "Failed to init telem")();
 
-    telem::ContexHandle ctx;
-    ttInitializeContext(ctx, telemBuf, sizeof(telemBuf));
-    ttSetThreadName(ctx, 0, "Main");
-
-    ttTick(ctx);
-
-    while (state.KeepRunning())
-    {
+BENCHMARK_F(TelemFixture, zone_printf_str)(benchmark::State& state) {
+    for (auto _ : state) {
         ttEnter(ctx, "this is a string %s in the middle", "with format spec");
         ttLeave(ctx);
     }
-
-    ttClose(ctx);
-
-    ttShutdownContext(ctx);
-    ttShutDown();
 }
 
-BENCHMARK(BM_telem_zone);
-BENCHMARK(BM_telem_zone_printf);
+
+BENCHMARK_F(TelemFixture, zone_printf_int)(benchmark::State& state) {
+    for (auto _ : state) {
+        ttEnter(ctx, "this is a string %" PRIi32 ", %" PRIi64 " in the middle", 25251_i32, 123456_i64);
+        ttLeave(ctx);
+    }
+}
+
+BENCHMARK_F(TelemFixture, callstack_get)(benchmark::State& state) {
+    TtCallStack stack;
+    for (auto _ : state) {
+        ttGetCallStack(ctx, stack);
+    }
+}
+
+
+BENCHMARK_REGISTER_F(TelemFixture, zone_paused);
+BENCHMARK_REGISTER_F(TelemFixture, zone_simple);
+BENCHMARK_REGISTER_F(TelemFixture, zone_printf_str);
+BENCHMARK_REGISTER_F(TelemFixture, zone_printf_int);
+BENCHMARK_REGISTER_F(TelemFixture, callstack_get);
+
