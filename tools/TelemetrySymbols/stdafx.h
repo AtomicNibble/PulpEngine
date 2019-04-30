16b09f2e919d8a152f8633df747e1d27b2ad2cc3
Tom Crowley <tom@tom-crowley.co.uk>
2019-04-30 19:37:07 +0100

    Base project for TelemSymbols.

---
+++ tools/TelemetrySymbols/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <EngineCommon.h>
+
+
+#ifndef TELEMETRY_SYMLIB_EXPORT
+
+#ifdef TELEM_LIB
+#define TELEMETRY_SYMLIB_EXPORT
+#else
+#ifdef TELEMETRY_SYM_LIB_EXPORT
+#define TELEMETRY_SYMLIB_EXPORT X_EXPORT
+#else
+#define TELEMETRY_SYMLIB_EXPORT X_IMPORT
+#endif // !TELEMETRY_SYM_LIB_EXPORT
+#endif // TELEM_LIB
+
+#endif // !TELEMETRY_SYMLIB_EXPORT
